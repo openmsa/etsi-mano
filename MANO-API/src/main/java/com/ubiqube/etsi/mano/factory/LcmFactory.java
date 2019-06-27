51092d7e53b3a6c11723dbde39b529333604ca78
Vignaud Olivier <ovi@ubiqube.com>
2019-06-27 12:16:49 +0200

    Added: Factory for LCM.

---
+++ MANO-API/src/main/java/com/ubiqube/etsi/mano/factory/LcmFactory.java
@@ -0,0 +1,62 @@
+package com.ubiqube.etsi.mano.factory;
+
+import com.ubiqube.etsi.mano.model.nslcm.sol003.CreateVnfRequest;
+import com.ubiqube.etsi.mano.model.nslcm.sol003.Link;
+import com.ubiqube.etsi.mano.model.nslcm.sol003.VnfInstance;
+import com.ubiqube.etsi.mano.model.nslcm.sol003.VnfInstanceLinks;
+
+public class LcmFactory {
+	public LcmFactory() {
+		// Nothing.
+	}
+
+	public static VnfInstance createVnfInstance(CreateVnfRequest createVnfRequest) {
+		final VnfInstance vnfInstance = new VnfInstance();
+		vnfInstance.setVnfdId(createVnfRequest.getVnfdId());
+		vnfInstance.setVnfInstanceDescription(createVnfRequest.getVnfInstanceDescription());
+		vnfInstance.setVnfInstanceName(createVnfRequest.getVnfInstanceName());
+		return vnfInstance;
+	}
+
+	public static VnfInstanceLinks createVnfInstancesLink(String hrefSelf, String hrefChangeExtConn, String hrefChangeFlavor, String hrefHeal, String hrefIndicators, String hrefInstanciate, String hrefOperate, String hrefScale, String hrefScaleToLevel) {
+		final VnfInstanceLinks vnfInstanceLinks = new VnfInstanceLinks();
+		;
+		final Link self = new Link();
+		self.setHref(hrefSelf);
+		vnfInstanceLinks.self(self);
+
+		final Link changeExtConn = new Link();
+		changeExtConn.setHref(hrefChangeExtConn);
+		vnfInstanceLinks.setChangeExtConn(changeExtConn);
+
+		final Link changeFlavour = new Link();
+		changeFlavour.setHref(hrefChangeFlavor);
+		vnfInstanceLinks.setChangeFlavour(changeFlavour);
+
+		final Link heal = new Link();
+		heal.setHref(hrefHeal);
+		vnfInstanceLinks.setHeal(heal);
+
+		final Link indicators = new Link();
+		indicators.setHref(hrefIndicators);
+		vnfInstanceLinks.setIndicators(indicators);
+
+		final Link instantiate = new Link();
+		instantiate.setHref(hrefInstanciate);
+		vnfInstanceLinks.setInstantiate(instantiate);
+
+		final Link operate = new Link();
+		operate.setHref(hrefOperate);
+		vnfInstanceLinks.setOperate(operate);
+
+		final Link scale = new Link();
+		scale.setHref(hrefScale);
+		vnfInstanceLinks.setScale(scale);
+
+		final Link scaleToLevel = new Link();
+		scaleToLevel.setHref(hrefScaleToLevel);
+		vnfInstanceLinks.setScaleToLevel(scaleToLevel);
+		return vnfInstanceLinks;
+	}
+
+}
