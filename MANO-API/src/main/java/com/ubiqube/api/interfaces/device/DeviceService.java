741bd13f99dc92d9aa7e4700424e5a64d3f3ae36
ofa <ofa@ubiqube.com>
2019-07-08 20:47:11 +0200

    add getAvailableManufacturers() in DeviceService

--- MANO-API/src/main/java/com/ubiqube/api/interfaces/device/DeviceService.java
+++ MANO-API/src/main/java/com/ubiqube/api/interfaces/device/DeviceService.java
@@ -2,7 +2,10 @@ package com.ubiqube.api.interfaces.device;
 
 import com.ubiqube.api.commons.id.DeviceId;
 import com.ubiqube.api.entities.device.SimpleDevice;
+import com.ubiqube.api.entities.device.Manufacturer;
 import com.ubiqube.api.exception.ServiceException;
+
+import java.util.Map;
 
 
 public interface DeviceService {
@@ -10,4 +13,5 @@ public interface DeviceService {
 	public DeviceId getDeviceId(String nsInstanceId) throws ServiceException;
 	public void deleteDevice(DeviceId deviceId, String name) throws ServiceException;
 	public SimpleDevice getDeviceModeleAndManId(DeviceId deviceId) throws ServiceException;
+	public Map<Long, Manufacturer> getAvailableManufacturers() throws ServiceException;
 }
