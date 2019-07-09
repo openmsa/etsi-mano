e078a55922998bbdcef0ab4dbed79495627dec07
Vignaud Olivier <ovi@ubiqube.com>
2019-07-09 14:23:06 +0200

    Activate annotation scan in thos runner.

--- MANO-API/src/main/java/com/ubiqube/etsi/mano/config/ServletInitializer.java
+++ MANO-API/src/main/java/com/ubiqube/etsi/mano/config/ServletInitializer.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 import com.ubiqube.etsi.mano.Application;
 
 @SpringBootApplication
+@ComponentScan(value = { "com.ubiqube.etsi.mano" })
 public class ServletInitializer extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(ServletInitializer.class, args);
