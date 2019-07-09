ac60f5dc88d2ceed81e2cf3632a950c564c52fea
ofa <ofa@ubiqube.com>
2019-07-09 23:23:31 +0200

    add implem for OrchestrationServiceRest (WIP)

--- MANO-API/src/main/java/com/ubiqube/etsi/mano/service/OrchestrationServiceRest.java
+++ MANO-API/src/main/java/com/ubiqube/etsi/mano/service/OrchestrationServiceRest.java
@@ -1,5 +1,6 @@
 package com.ubiqube.etsi.mano.service;
 
+import java.net.URI;
 import java.util.Map;
 
 import org.springframework.stereotype.Service;
@@ -11,9 +12,21 @@ import com.ubiqube.api.interfaces.orchestration.OrchestrationService;
 @Service
 public class OrchestrationServiceRest implements OrchestrationService {
 
+	private final static UbiRest rest = new UbiRest();
+
 	@Override
 	public ProcessInstance scheduleServiceImmediateMode(String ubiqubeId, long serviceId, String serviceName, String processName, Map<String, String> varsMap) throws ServiceException {
-		return null;
+		final URI uri = rest.uriBuilder()
+				.pathSegment("orchestration/v1/scheduleImmediateMode")
+				.queryParam("ubiqubeId", ubiqubeId)
+				.queryParam("serviceId", serviceId)
+				.queryParam("serviceName", serviceName)
+				.queryParam("processName", processName)
+				.build()
+				.toUri();
+		return rest.post(uri, varsMap, ProcessInstanceModel.class);
 	}
 
+	static class ProcessInstanceModel implements ProcessInstance {
+	}
 }
