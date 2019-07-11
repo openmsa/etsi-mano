8a3b3c55815b78fa3a47765824ab7058f630a72f
ofa <ofa@ubiqube.com>
2019-07-12 00:56:04 +0200

    trim assert messages in RepositoryServiceRestTest

--- MANO-API/src/test/integration/com/ubiqube/service/RepositoryServiceRestTest.java
+++ MANO-API/src/test/integration/com/ubiqube/service/RepositoryServiceRestTest.java
@@ -19,7 +19,7 @@ public class RepositoryServiceRestTest {
 	@Test
 	public void testDoSearch() throws Exception {
 		final List<String> res = repositoryServiceRest.doSearch("Process/ETSI-MANO", null);
-		assertNotNull("The list must not be null.", res);
+		assertNotNull("The list must not be null", res);
 	}
 
 	@Test
@@ -31,7 +31,7 @@ public class RepositoryServiceRestTest {
 	@Test
 	public void testGetElement() throws Exception {
 		final RepositoryElement res = repositoryServiceRest.getElement("Process/ETSI-MANO");
-		assertNotNull("The element must not be null.", res);
+		assertNotNull("The element must not be null", res);
 		assertEquals("The name must be equal to ETSI-MANO", res.getName(), "ETSI-MANO");
 	}
 
@@ -40,7 +40,7 @@ public class RepositoryServiceRestTest {
 		final RepositoryElement element = repositoryServiceRest.getElement(
 			"Process/ETSI-MANO/NFV/Common/Tasks/Task_Synchronize_to_VIM_tenant.php");
 		final byte res[] = repositoryServiceRest.getRepositoryElementContent(element);
-		assertNotNull("The element must not be null.", res);
+		assertNotNull("The element must not be null", res);
 		assertTrue("The element must be PHP code", new String(res).startsWith("<?php"));
 	}
 }
