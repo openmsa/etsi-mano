243a5abfc268ed1fbf5d2c5507b1c07cd7230f8d
ovi <ovi@ubiqube.com>
2019-07-20 01:10:09 +0200

    mano/repository: add msa/ repository classes

---
+++ MANO-API/src/main/java/com/ubiqube/etsi/mano/repository/msa/AbstractGenericRepository.java
@@ -0,0 +1,81 @@
+package com.ubiqube.etsi.mano.repository.msa;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ubiqube.api.entities.repository.RepositoryElement;
+import com.ubiqube.api.exception.ServiceException;
+import com.ubiqube.api.interfaces.repository.RepositoryService;
+import com.ubiqube.etsi.mano.exception.GenericException;
+
+/**
+ * A Generic implementation of classical CRUD action around a repository.
+ *
+ * @author Olivier Vignaud <ovi@ubiqube.com>
+ *
+ * @param <T>
+ */
+public abstract class AbstractGenericRepository<T> extends AbstractRepository<T> {
+	private static final Logger LOG = LoggerFactory.getLogger(AbstractGenericRepository.class);
+	private final ObjectMapper mapper;
+
+	@Inject
+	public AbstractGenericRepository(ObjectMapper _mapper, RepositoryService _repositoryService) {
+		super(_repositoryService);
+		mapper = _mapper;
+	}
+
+	abstract String getUriForId(String _id);
+
+	abstract String setId(T _entity);
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public final T get(String _id) {
+		final String uri = getUriForId(_id);
+		LOG.debug("Loading ID: {}", _id);
+		verify(uri);
+		final RepositoryElement repositoryElement = repositoryService.getElement(uri);
+		final byte[] repositoryContent = repositoryService.getRepositoryElementContent(repositoryElement);
+
+		try {
+			final String content = new String(repositoryContent, StandardCharsets.UTF_8);
+			return (T) mapper.readValue(content, getClazz());
+		} catch (final IOException e) {
+			throw new GenericException(e);
+		}
+	}
+
+	abstract Class<?> getClazz();
+
+	@Override
+	public final void delete(String _id) {
+		final String uri = getUriForId(_id);
+		verify(_id);
+		final RepositoryElement repositoryElement = repositoryService.getElement(uri);
+		repositoryService.deleteRepositoryElement(repositoryElement, "ncroot");
+	}
+
+	@Override
+	public final T save(T _entity) {
+		final String saveId = setId(_entity);
+
+		final String uri = getUriForId(saveId);
+		try {
+			final String str = mapper.writeValueAsString(_entity);
+			LOG.info("Creating entity @ {}", uri);
+			repositoryService.addFile(uri, "SOL005", "", str, "ncroot");
+		} catch (IOException | ServiceException e) {
+			throw new GenericException(e);
+		}
+
+		return _entity;
+	}
+
+}
---
+++ MANO-API/src/main/java/com/ubiqube/etsi/mano/repository/msa/VnfPackageMsa.java
@@ -0,0 +1,49 @@
+package com.ubiqube.etsi.mano.repository.msa;
+
+import java.util.UUID;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Repository;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ubiqube.api.interfaces.repository.RepositoryService;
+import com.ubiqube.etsi.mano.model.vnf.sol005.VnfPkgInfo;
+import com.ubiqube.etsi.mano.repository.VnfPackageRepository;
+
+/**
+ * Implementation of a repository for a VNFPackage document.
+ *
+ * @author Olivier Vignaud <ovi@ubiqube.com>
+ *
+ */
+@Repository
+public class VnfPackageMsa extends AbstractGenericRepository<VnfPkgInfo> implements VnfPackageRepository {
+	@Inject
+	public VnfPackageMsa(ObjectMapper _mapper, RepositoryService _repositoryService) {
+		super(_mapper, _repositoryService);
+	}
+
+	private static final String REPOSITORY_NVFO_DATAFILE_BASE_PATH = "Datafiles/NFVO/vnf_packages";
+
+	@Override
+	String getUriForId(String _id) {
+		return REPOSITORY_NVFO_DATAFILE_BASE_PATH + "/" + _id + "/vnfPkgInfo.json";
+	}
+
+	@Override
+	String setId(VnfPkgInfo _entity) {
+		final String id = _entity.getId();
+		if (null == id) {
+			_entity.setId(UUID.randomUUID().toString());
+		}
+
+		return _entity.getId();
+	}
+
+	@Override
+	Class<?> getClazz() {
+		return VnfPkgInfo.class;
+	}
+
+}
