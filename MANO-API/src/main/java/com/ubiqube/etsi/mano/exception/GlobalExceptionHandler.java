e200a51aa97e5491b1a4c4a167c872779e0c7f0e
Olivier Vignaud <ovi@ubiqube.com>
2019-07-23 17:04:34 +0200

    Fixed: global exception for web ones.

--- MANO-API/src/main/java/com/ubiqube/etsi/mano/exception/GlobalExceptionHandler.java
+++ MANO-API/src/main/java/com/ubiqube/etsi/mano/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.ubiqube.etsi.mano.model.vnf.sol005.ProblemDetails;
@@ -20,7 +21,12 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 		LOG.error("Exception", e);
 		final ProblemDetails problemDetails = new ProblemDetails(500, e.getMessage());
 		return new ResponseEntity<>(problemDetails, HttpStatus.INTERNAL_SERVER_ERROR);
-
 	}
 
+	@ExceptionHandler({ ResponseStatusException.class })
+	public ResponseEntity<ProblemDetails> handleWebApplicationException(ResponseStatusException e) {
+		LOG.error("Exception", e);
+		final ProblemDetails problemDetails = new ProblemDetails(e.getStatus().value(), e.getReason());
+		return new ResponseEntity<>(problemDetails, e.getStatus());
+	}
 }
