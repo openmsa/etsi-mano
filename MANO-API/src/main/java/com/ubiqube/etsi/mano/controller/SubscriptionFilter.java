b0b96a45e0f7336c7067ed8396df039f51d67bf3
Olivier Vignaud <ovi@ubiqube.com>
2019-07-24 16:31:52 +0200

    Removed duplicated filter classes.

--- MANO-API/src/main/java/com/ubiqube/etsi/mano/controller/SubscriptionFilter.java
+++ MANO-API/src/main/java/com/ubiqube/etsi/mano/controller/SubscriptionFilter.java
@@ -1,5 +1,6 @@
 package com.ubiqube.etsi.mano.controller;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 import org.antlr.v4.runtime.ANTLRInputStream;
@@ -9,6 +10,9 @@ import org.apache.commons.beanutils.BeanUtils;
 import com.ubiqube.etsi.mano.exception.GenericException;
 import com.ubiqube.etsi.mano.grammar.EtsiLexer;
 import com.ubiqube.etsi.mano.grammar.Etsifilter;
+import com.ubiqube.etsi.mano.grammar.Node;
+import com.ubiqube.etsi.mano.grammar.Node.Operand;
+import com.ubiqube.etsi.mano.grammar.TreeBuilder;
 import com.ubiqube.etsi.mano.model.vnf.sol005.SubscriptionObject;
 import com.ubiqube.etsi.mano.model.vnf.sol005.SubscriptionsPkgmSubscription;
 
@@ -20,28 +24,28 @@ import com.ubiqube.etsi.mano.model.vnf.sol005.SubscriptionsPkgmSubscription;
  */
 public class SubscriptionFilter {
 
-	public List<Filter> getFilters(String query) {
-		final FilterListner filterListner = new FilterListner();
+	public List<Node> getFilters(String query) {
+		final TreeBuilder treeBuilder = new TreeBuilder();
 		final EtsiLexer el = new EtsiLexer(new ANTLRInputStream(query));
 		final CommonTokenStream tokens = new CommonTokenStream(el);
 		final Etsifilter parser = new Etsifilter(tokens);
-		parser.addParseListener(filterListner);
+		parser.addParseListener(treeBuilder);
 		parser.filterExpr();
 
-		return filterListner.getFilters();
+		return treeBuilder.getListNode();
 	}
 
 	/**
 	 * In fact this method could be more generic by taking an Object.
 	 *
 	 * @param subscriptionRepository
-	 * @param filters
+	 * @param nodes
 	 * @return
 	 */
-	public boolean apply(SubscriptionObject subscriptionRepository, List<Filter> filters) {
+	public boolean apply(SubscriptionObject subscriptionRepository, List<Node> nodes) {
 		final SubscriptionsPkgmSubscription pkgSub = subscriptionRepository.getSubscriptionsPkgmSubscription();
-		for (final Filter filter : filters) {
-			if (!apply(pkgSub, filter)) {
+		for (final Node node : nodes) {
+			if (!apply(pkgSub, node)) {
 				return false;
 			}
 		}
@@ -49,35 +53,35 @@ public class SubscriptionFilter {
 		return true;
 	}
 
-	private boolean apply(SubscriptionsPkgmSubscription _pkgSub, Filter filter) {
+	private static boolean apply(SubscriptionsPkgmSubscription _pkgSub, Node node) {
 		try {
-			final String objectValue = BeanUtils.getNestedProperty(_pkgSub, filter.getAttr());
-			return decide(objectValue, filter.getValue(), filter.getOp());
-		} catch (final Exception e) {
+			final String objectValue = BeanUtils.getNestedProperty(_pkgSub, node.getName());
+			return decide(objectValue, node.getValue(), node.getOp());
+		} catch (final InvocationTargetException | IllegalAccessException | NoSuchMethodException e) {
 			throw new GenericException(e);
 		}
 	}
 
-	private boolean decide(String _objectValue, String _value, String _op) {
-		if ("eq".equals(_value)) {
+	private static boolean decide(String _objectValue, String _value, Operand operand) {
+		if (Operand.EQ.equals(operand)) {
 			return _value.equals(_objectValue);
 		}
-		if ("neq".equals(_value)) {
+		if (Operand.NEQ.equals(operand)) {
 			return !_value.equals(_objectValue);
 		}
 		// GT LT GTE LTE are numerical so cast everything in integer
 		final int left = Integer.parseInt(_objectValue);
 		final int right = Integer.parseInt(_value);
-		if ("gt".equals(_value)) {
+		if (Operand.GT.equals(operand)) {
 			return left > right;
 		}
-		if ("lt".equals(_value)) {
+		if (Operand.LT.equals(operand)) {
 			return left < right;
 		}
-		if ("gte".equals(_value)) {
+		if (Operand.GTE.equals(operand)) {
 			return left >= right;
 		}
-		if ("lte".equals(_value)) {
+		if (Operand.LTE.equals(operand)) {
 			return left <= right;
 		}
 		return false;
