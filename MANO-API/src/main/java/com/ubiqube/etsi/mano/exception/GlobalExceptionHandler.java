a83cf1e71cd72e330fba017760a5f8162e7789f7
Olivier Vignaud <ovi@ubiqube.com>
2019-07-24 16:14:34 +0200

    More generic exception handler.

--- MANO-API/src/main/java/com/ubiqube/etsi/mano/exception/GlobalExceptionHandler.java
+++ MANO-API/src/main/java/com/ubiqube/etsi/mano/exception/GlobalExceptionHandler.java
@@ -2,22 +2,35 @@ package com.ubiqube.etsi.mano.exception;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.ubiqube.etsi.mano.model.vnf.sol005.ProblemDetails;
 
-@ControllerAdvice
+/**
+ * There is a problem in content negotiation, Spring will refuse to return
+ * something else than the accept from the client, leading to a
+ * NOT_ACCEPTABLE_CONTANT (406) error.
+ *
+ * @see https://github.com/spring-projects/spring-framework/issues/20865
+ * @author olivier
+ *
+ */
+@RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
 	private static final Logger LOG = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
-	@ExceptionHandler({ RuntimeException.class })
-	public ResponseEntity<ProblemDetails> handleRunTimeException(RuntimeException e) {
+	@ExceptionHandler({ Exception.class })
+	public ResponseEntity<ProblemDetails> handleRunTimeException(Exception e) {
 		LOG.error("Exception", e);
 		final ProblemDetails problemDetails = new ProblemDetails(500, e.getMessage());
 		return new ResponseEntity<>(problemDetails, HttpStatus.INTERNAL_SERVER_ERROR);
@@ -29,4 +42,17 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 		final ProblemDetails problemDetails = new ProblemDetails(e.getStatus().value(), e.getReason());
 		return new ResponseEntity<>(problemDetails, e.getStatus());
 	}
+
+	@Override
+	protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException e, HttpHeaders headers, HttpStatus status, WebRequest request) {
+		final ProblemDetails problemDetails = new ProblemDetails(status.value(), e.getMessage());
+		return new ResponseEntity<>(problemDetails, status);
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleHttpMediaTypeNotAcceptable(HttpMediaTypeNotAcceptableException e, HttpHeaders headers, HttpStatus status, WebRequest request) {
+		final ProblemDetails problemDetails = new ProblemDetails(status.value(), e.getMessage());
+		return new ResponseEntity<>(problemDetails, status);
+	}
+
 }
