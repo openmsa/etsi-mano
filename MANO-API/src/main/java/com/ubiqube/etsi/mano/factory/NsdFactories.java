7c26d048488de0cf7fca2e865d0dab5d42679232
Olivier Vignaud <ovi@ubiqube.com>
2019-07-30 13:55:06 +0200

    Minor fix in NSD factory.

--- MANO-API/src/main/java/com/ubiqube/etsi/mano/factory/NsdFactories.java
+++ MANO-API/src/main/java/com/ubiqube/etsi/mano/factory/NsdFactories.java
@@ -3,12 +3,12 @@ package com.ubiqube.etsi.mano.factory;
 import java.util.ArrayList;
 
 import com.ubiqube.etsi.mano.model.nsd.sol005.NsDescriptorsNsdInfo;
-import com.ubiqube.etsi.mano.model.nsd.sol005.NsDescriptorsNsdInfoIdGetResponse;
-import com.ubiqube.etsi.mano.model.nsd.sol005.NsDescriptorsNsdInfoLinks;
-import com.ubiqube.etsi.mano.model.nsd.sol005.NsDescriptorsNsdInfoLinksSelf;
 import com.ubiqube.etsi.mano.model.nsd.sol005.NsDescriptorsNsdInfo.NsdOnboardingStateEnum;
 import com.ubiqube.etsi.mano.model.nsd.sol005.NsDescriptorsNsdInfo.NsdOperationalStateEnum;
 import com.ubiqube.etsi.mano.model.nsd.sol005.NsDescriptorsNsdInfo.NsdUsageStateEnum;
+import com.ubiqube.etsi.mano.model.nsd.sol005.NsDescriptorsNsdInfoIdGetResponse;
+import com.ubiqube.etsi.mano.model.nsd.sol005.NsDescriptorsNsdInfoLinks;
+import com.ubiqube.etsi.mano.model.nsd.sol005.NsDescriptorsNsdInfoLinksSelf;
 
 public class NsdFactories {
 
@@ -18,15 +18,7 @@ public class NsdFactories {
 
 	public static NsDescriptorsNsdInfoIdGetResponse createNsDescriptorsNsdInfoIdGetResponse(String _id, String _self, String _nsdContent) {
 		final NsDescriptorsNsdInfoIdGetResponse ret = new NsDescriptorsNsdInfoIdGetResponse();
-		final NsDescriptorsNsdInfo nsdInfo = new NsDescriptorsNsdInfo();
-		nsdInfo.setLinks(createNsDescriptorsNsdInfoLinks(_self, _nsdContent));
-		nsdInfo.setNestedNsdInfoIds(new ArrayList<String>());
-		nsdInfo.setNsdOnboardingState(NsdOnboardingStateEnum.CREATED);
-		nsdInfo.setNsdOperationalState(NsdOperationalStateEnum.ENABLED);
-		nsdInfo.setNsdUsageState(NsdUsageStateEnum.NOT_IN_USE);
-		nsdInfo.setPnfdInfoIds(new ArrayList<String>());
-		nsdInfo.setVnfPkgIds(new ArrayList<String>());
-		nsdInfo.setId(_id);
+		final NsDescriptorsNsdInfo nsdInfo = createNsDescriptorsNsdInfo(_id, _self, _nsdContent);
 		ret.setNsdInfo(nsdInfo);
 		return ret;
 	}
@@ -48,7 +40,7 @@ public class NsdFactories {
 		nsdInfo.setLinks(createNsDescriptorsNsdInfoLinks(_self, _nsdContent));
 		nsdInfo.setNestedNsdInfoIds(new ArrayList<String>());
 		nsdInfo.setNsdOnboardingState(NsdOnboardingStateEnum.CREATED);
-		nsdInfo.setNsdOperationalState(NsdOperationalStateEnum.ENABLED);
+		nsdInfo.setNsdOperationalState(NsdOperationalStateEnum.DISABLED);
 		nsdInfo.setNsdUsageState(NsdUsageStateEnum.NOT_IN_USE);
 		nsdInfo.setPnfdInfoIds(new ArrayList<String>());
 		nsdInfo.setVnfPkgIds(new ArrayList<String>());
