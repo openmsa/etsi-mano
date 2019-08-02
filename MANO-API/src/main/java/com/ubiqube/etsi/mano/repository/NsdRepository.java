6f59057542701ea0eb17e810f3680db1ba118a6e
Olivier Vignaud <ovi@ubiqube.com>
2019-08-02 12:15:18 +0200

    Code clean up.

--- MANO-API/src/main/java/com/ubiqube/etsi/mano/repository/NsdRepository.java
+++ MANO-API/src/main/java/com/ubiqube/etsi/mano/repository/NsdRepository.java
@@ -3,5 +3,5 @@ package com.ubiqube.etsi.mano.repository;
 import com.ubiqube.etsi.mano.model.nsd.sol005.NsDescriptorsNsdInfo;
 
 public interface NsdRepository extends CrudRepository<NsDescriptorsNsdInfo> {
-
+	// Implementation of Crud repository.
 }
--- MANO-API/src/main/java/com/ubiqube/etsi/mano/repository/SubscriptionRepository.java
+++ MANO-API/src/main/java/com/ubiqube/etsi/mano/repository/SubscriptionRepository.java
@@ -3,5 +3,5 @@ package com.ubiqube.etsi.mano.repository;
 import com.ubiqube.etsi.mano.model.vnf.sol005.SubscriptionObject;
 
 public interface SubscriptionRepository extends CrudRepository<SubscriptionObject> {
-
+	// Implementation of Crud repository.
 }
