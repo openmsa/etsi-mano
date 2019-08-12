64fe2d596219d36c82493907839ae3e65df53d7b
Olivier Vignaud <ovi@ubiqube.com>
2019-08-12 13:30:24 +0200

    Clean up.

--- MANO-API/src/main/java/com/ubiqube/etsi/mano/factory/VnfPackageFactory.java
+++ MANO-API/src/main/java/com/ubiqube/etsi/mano/factory/VnfPackageFactory.java
@@ -1,5 +1,6 @@
 package com.ubiqube.etsi.mano.factory;
 
+import com.ubiqube.etsi.mano.Constants;
 import com.ubiqube.etsi.mano.model.vnf.sol005.VnfPackagesVnfPkgInfoAdditionalArtifacts;
 import com.ubiqube.etsi.mano.model.vnf.sol005.VnfPackagesVnfPkgInfoChecksum;
 import com.ubiqube.etsi.mano.model.vnf.sol005.VnfPkgInfo;
@@ -9,7 +10,7 @@ import com.ubiqube.etsi.mano.model.vnf.sol005.VnfPkgInfo.UsageStateEnum;
 
 public class VnfPackageFactory {
 
-	public static VnfPkgInfo createVnfPkgInfo(String vnfPkgId, Object userData) {
+	public static VnfPkgInfo createVnfPkgInfo(final String vnfPkgId, final Object userData) {
 		final VnfPkgInfo vnfPkgInfo = new VnfPkgInfo();
 		vnfPkgInfo.setId(vnfPkgId);
 		vnfPkgInfo.setOnboardingState(OnboardingStateEnum.CREATED);
@@ -20,17 +21,17 @@ public class VnfPackageFactory {
 		return vnfPkgInfo;
 	}
 
-	public static VnfPackagesVnfPkgInfoAdditionalArtifacts createArtefact(String _filename, String _checksum) {
+	public static VnfPackagesVnfPkgInfoAdditionalArtifacts createArtefact(final String _filename, final String _checksum) {
 		final VnfPackagesVnfPkgInfoAdditionalArtifacts artefact = new VnfPackagesVnfPkgInfoAdditionalArtifacts();
 		artefact.artifactPath(_filename);
 		final VnfPackagesVnfPkgInfoChecksum checksum = new VnfPackagesVnfPkgInfoChecksum();
-		checksum.algorithm("SHA-256");
+		checksum.algorithm(Constants.HASH_ALGORITHM);
 		checksum.setHash(_checksum);
 		artefact.setChecksum(checksum);
 		return artefact;
 	}
 
-	public static VnfPackagesVnfPkgInfoAdditionalArtifacts createArtefact(String _filename, VnfPackagesVnfPkgInfoChecksum _checksum) {
+	public static VnfPackagesVnfPkgInfoAdditionalArtifacts createArtefact(final String _filename, final VnfPackagesVnfPkgInfoChecksum _checksum) {
 		final VnfPackagesVnfPkgInfoAdditionalArtifacts artefact = new VnfPackagesVnfPkgInfoAdditionalArtifacts();
 		artefact.artifactPath(_filename);
 		artefact.setChecksum(_checksum);
