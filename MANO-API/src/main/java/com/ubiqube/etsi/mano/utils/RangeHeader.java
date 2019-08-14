237954d67f8375056e11302897777ba03597a9ae
Olivier Vignaud <ovi@ubiqube.com>
2019-08-14 14:53:06 +0200

    Fixed: EOF parsing in RangeHeader.

--- MANO-API/src/main/java/com/ubiqube/etsi/mano/utils/RangeHeader.java
+++ MANO-API/src/main/java/com/ubiqube/etsi/mano/utils/RangeHeader.java
@@ -2,6 +2,30 @@ package com.ubiqube.etsi.mano.utils;
 
 /**
  * This class allows to handle the range from the HTTP request header.
+ *
+ * @see https://tools.ietf.org/html/rfc7233 It's a lot more complex :
+ * 
+ *      <pre>
+ * Additional examples, assuming a representation of length 10000:
+
+   o  The final 500 bytes (byte offsets 9500-9999, inclusive):
+
+        bytes=-500
+
+   Or:
+
+        bytes=9500-
+
+   o  The first and last bytes only (bytes 0 and 9999):
+
+        bytes=0-0,-1
+
+   o  Other valid (but not canonical) specifications of the second 500
+      bytes (byte offsets 500-999, inclusive):
+
+        bytes=500-600,601-999
+        bytes=500-700,601-999
+ *      </pre>
  */
 public class RangeHeader {
 
@@ -85,7 +109,10 @@ public class RangeHeader {
 		final Unit unit = Unit.valueOf(tokens[0].toUpperCase());
 		final String[] fromTo = tokens[1].split("-");
 		final int from = Integer.parseInt(fromTo[0]);
-		final Integer to = Integer.decode(fromTo[1]);
+		Integer to = null;
+		if (fromTo.length > 1) {
+			to = Integer.decode(fromTo[1]);
+		}
 		return new RangeHeader(unit, from, to);
 	}
 
