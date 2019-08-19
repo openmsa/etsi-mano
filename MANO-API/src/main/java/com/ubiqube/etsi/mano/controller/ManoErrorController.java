719d0542a1595f0512179317ec429534db10dbd5
Olivier Vignaud <ovi@ubiqube.com>
2019-08-19 14:31:05 +0200

    Changed return type to MediaType.APPLICATION_PROBLEM_JSON

--- MANO-API/src/main/java/com/ubiqube/etsi/mano/controller/ManoErrorController.java
+++ MANO-API/src/main/java/com/ubiqube/etsi/mano/controller/ManoErrorController.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.web.servlet.error.AbstractErrorController;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,9 +37,14 @@ public class ManoErrorController extends AbstractErrorController {
 		LOG.info("Error controller: {} => {}", status, ea);
 		if (status != null) {
 			final ProblemDetails problemDetail = new ProblemDetails(status, (String) ea.get("message"));
-			return ResponseEntity.status(status).body(problemDetail);
+			return ResponseEntity.status(status)
+					.contentType(MediaType.APPLICATION_PROBLEM_JSON)
+					.body(problemDetail);
 		}
 		final ProblemDetails problemDetail = new ProblemDetails(500, "Some thing bad happend.");
-		return ResponseEntity.status(500).body(problemDetail);
+		return ResponseEntity
+				.status(500)
+				.contentType(MediaType.APPLICATION_PROBLEM_JSON)
+				.body(problemDetail);
 	}
 }
