49eaa360ba17dc8cf8fea68c75df2d2077a574fa
Olivier Vignaud <ovi@ubiqube.com>
2019-08-30 12:15:32 +0200

    Implementing vnfPackagesVnfPkgIdGet methods.

--- MANO-API/src/main/java/com/ubiqube/etsi/mano/controller/vnf/VnfmPackageManagement.java
+++ MANO-API/src/main/java/com/ubiqube/etsi/mano/controller/vnf/VnfmPackageManagement.java
@@ -1,6 +1,9 @@
 package com.ubiqube.etsi.mano.controller.vnf;
 
+import java.net.URI;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,8 +11,11 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import com.ubiqube.etsi.mano.model.vnf.sol005.VnfPackagesVnfPkgIdGetResponse;
 import com.ubiqube.etsi.mano.model.vnf.sol005.VnfPkgInfo;
+import com.ubiqube.etsi.mano.service.rest.NfvoRest;
 import com.ubiqube.etsi.mano.utils.RangeHeader;
 
 /**
@@ -24,21 +30,36 @@ import com.ubiqube.etsi.mano.utils.RangeHeader;
 public class VnfmPackageManagement implements VnfPackageManagement {
 
 	private static final Logger LOG = LoggerFactory.getLogger(VnfmPackageManagement.class);
+	private final NfvoRest nfvoRest;
 
-	public VnfmPackageManagement() {
+	public VnfmPackageManagement(final NfvoRest _nfvoRest) {
 		LOG.info("Starting VNF Package Management in a VNFM Only mode.");
+		nfvoRest = _nfvoRest;
 	}
 
 	@Override
 	public VnfPkgInfo vnfPackagesVnfPkgIdGet(final String vnfPkgId, final Linkable links) {
-		// TODO Auto-generated method stub
-		return null;
+		final Map<String, Object> uriVariables = new HashMap<>();
+		uriVariables.put("vnfPkgId", vnfPkgId);
+		final URI uri = nfvoRest.uriBuilder()
+				.pathSegment("vnfpkgm/v1/vnf_packages/{vnfPkgId}")
+				.buildAndExpand(uriVariables)
+				.toUri();
+		final VnfPackagesVnfPkgIdGetResponse resp = nfvoRest.get(uri, VnfPackagesVnfPkgIdGetResponse.class);
+		return resp.getVnfPkgInfo();
 	}
 
 	@Override
 	public String vnfPackagesGet(final Map<String, String> queryParameters, final Linkable links) {
-		// TODO Auto-generated method stub
-		return null;
+		final UriComponentsBuilder builder = nfvoRest.uriBuilder()
+				.pathSegment("vnfpkgm/v1/vnf_packages");
+		for (final Entry<String, String> entry : queryParameters.entrySet()) {
+			builder.queryParam(entry.getKey(), entry.getValue());
+		}
+		final URI uri = builder
+				.build()
+				.toUri();
+		return nfvoRest.get(uri, String.class);
 	}
 
 	@Override
