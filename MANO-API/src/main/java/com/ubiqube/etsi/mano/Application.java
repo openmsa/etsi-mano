cf78801883af96090a6ba591db9c42942fc1f0b3
Olivier Vignaud <ovi@ubiqube.com>
2019-09-19 09:36:43 +0200

    Remove unused logger.

--- MANO-API/src/main/java/com/ubiqube/etsi/mano/Application.java
+++ MANO-API/src/main/java/com/ubiqube/etsi/mano/Application.java
@@ -1,7 +1,5 @@
 package com.ubiqube.etsi.mano;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -11,7 +9,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSwagger2
 public class Application extends SpringBootServletInitializer {
-	private static final Logger LOG = LoggerFactory.getLogger(Application.class);
 
 	public static void main(final String[] args) {
 		SpringApplication.run(Application.class, args);
