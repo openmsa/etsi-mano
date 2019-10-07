3e9d85b56a0b6e2ed50d5dca8aab4023daaf7dae
Vignaud Olivier <ovi@ubiqube.com>
2019-10-07 14:16:11 +0200

    NfvoRest use Configuration service.

--- MANO-API/src/main/java/com/ubiqube/etsi/mano/service/rest/NfvoRest.java
+++ MANO-API/src/main/java/com/ubiqube/etsi/mano/service/rest/NfvoRest.java
@@ -4,20 +4,15 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
-import org.springframework.util.Assert;
-import org.springframework.beans.factory.annotation.Value;
-import javax.annotation.PostConstruct;
 
+import com.ubiqube.etsi.mano.service.Configuration;
 
 @Service
 public class NfvoRest extends AbstractRest {
+	private final String url;
 
-	@Value("${nfvo.url}")
-	private String url;
-
-	@PostConstruct
-	private void checkConfig() {
-		Assert.notNull(url, "nfvo.url is not declared in property file.");
+	public NfvoRest(final Configuration _conf) {
+		url = _conf.build("nfvo.url").notNull().build();
 	}
 
 	@Override
