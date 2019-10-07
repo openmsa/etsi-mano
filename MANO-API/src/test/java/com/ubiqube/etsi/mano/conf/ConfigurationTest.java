16a0f9951f1cfdd1683006f23002befcb0b20872
Vignaud Olivier <ovi@ubiqube.com>
2019-10-07 09:27:36 +0200

    ConfigurationTest more correct Test Unit.

--- MANO-API/src/test/java/com/ubiqube/etsi/mano/conf/ConfigurationTest.java
+++ MANO-API/src/test/java/com/ubiqube/etsi/mano/conf/ConfigurationTest.java
@@ -1,16 +1,20 @@
 package com.ubiqube.etsi.mano.conf;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
-import com.ubiqube.etsi.mano.service.Configuration;
+import com.ubiqube.etsi.mano.service.PropertiesConfiguration;
 
 public class ConfigurationTest {
 
 	@Test
 	void testName() throws Exception {
-		final Configuration conf = null;
-		final String val = conf.build("")
+		final PropertiesConfiguration conf = new PropertiesConfiguration();
+		conf.set("key", "value");
+		final String val = conf.build("key")
 				.notNull()
 				.build();
+		assertEquals("value", val);
 	}
 }
