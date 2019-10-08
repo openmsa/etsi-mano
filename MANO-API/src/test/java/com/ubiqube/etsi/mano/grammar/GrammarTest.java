e8790c2a453a68595e4552328f77f45834a22b64
ofa <ofa@ubiqube.com>
2019-10-08 16:35:53 +0200

    fix GrammarTest (and build)

--- MANO-API/src/test/java/com/ubiqube/etsi/mano/grammar/GrammarTest.java
+++ MANO-API/src/test/java/com/ubiqube/etsi/mano/grammar/GrammarTest.java
@@ -1,6 +1,7 @@
 package com.ubiqube.etsi.mano.grammar;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import java.util.List;
 
@@ -42,7 +43,7 @@ public class GrammarTest {
 		final AstBuilder astBuilder = new AstBuilder("id.eq=fce04624-6f92-42b1-bf50-437b682288a5,OOOOOOO");
 		final List<Node> nodes = astBuilder.getNodes();
 		final Node node = nodes.get(0);
-		assertEquals("fce04624-6f92-42b1-bf50-437b682288a5", node.getValue());
+		assertNotEquals("fce04624-6f92-42b1-bf50-437b682288a5", node.getValue());
 	}
 
 	/**
