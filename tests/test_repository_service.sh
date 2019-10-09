37b12e589d8bae36fb4a73fdf85dfa6e8f4f8be7
ofa <ofa@ubiqube.com>
2019-10-09 10:10:53 +0200

    fix test_repository_service.sh

--- tests/test_repository_service.sh
+++ tests/test_repository_service.sh
@@ -2,5 +2,4 @@
 
 source .test-env.sh
 
-$curl $msa_api/'repository/v1/search&URI=Process/ETSI-MANO&pattern=' \
-	|| : "IGNORING FAILED: $0"
+$curl $msa_api/'repository/v1/search?URI=Process/ETSI-MANO&pattern='
