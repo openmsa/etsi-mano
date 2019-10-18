bc0c99824f949f10c10b5867c83269d81392ae19
Olivier Vignaud <ovi@ubiqube.com>
2019-10-18 12:04:46 +0200

    Add pnf descriptor management.

---
+++ MANO-API/src/main/java/com/ubiqube/etsi/mano/factory/PnfFactory.java
@@ -0,0 +1,17 @@
+package com.ubiqube.etsi.mano.factory;
+
+import com.ubiqube.etsi.mano.model.nsd.sol005.PnfDescriptorsPnfdInfo;
+import com.ubiqube.etsi.mano.model.nsd.sol005.PnfDescriptorsPnfdInfo.PnfdOnboardingStateEnum;
+import com.ubiqube.etsi.mano.model.nsd.sol005.PnfDescriptorsPnfdInfo.PnfdUsageStateEnum;
+import com.ubiqube.etsi.mano.model.nsd.sol005.PnfDescriptorsPostQuery;
+
+public class PnfFactory {
+
+	public static PnfDescriptorsPnfdInfo createPnfDescriptorsPnfdInfo(final PnfDescriptorsPostQuery query) {
+		final PnfDescriptorsPnfdInfo pnfd = new PnfDescriptorsPnfdInfo();
+		pnfd.setPnfdOnboardingState(PnfdOnboardingStateEnum.CREATED);
+		pnfd.setPnfdUsageState(PnfdUsageStateEnum.NOT_IN_USE);
+		pnfd.setUserDefinedData(query.getCreatePnfdInfoRequest().getUserDefinedData());
+		return pnfd;
+	}
+}
---
+++ MANO-API/src/main/java/com/ubiqube/etsi/mano/repository/PnfdInfoRepository.java
@@ -0,0 +1,7 @@
+package com.ubiqube.etsi.mano.repository;
+
+import com.ubiqube.etsi.mano.model.nsd.sol005.PnfDescriptorsPnfdInfo;
+
+public interface PnfdInfoRepository extends CrudRepository<PnfDescriptorsPnfdInfo> {
+
+}
