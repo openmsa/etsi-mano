a268e7fd2910c4cccf1cffe4d1132b1dc31c5fee
Olivier Vignaud <ovi@ubiqube.com>
2019-10-21 14:30:00 +0200

    Add missing @Entity in NsdPackage.

--- MANO-API/src/main/java/com/ubiqube/etsi/mano/dao/mano/NsdPackage.java
+++ MANO-API/src/main/java/com/ubiqube/etsi/mano/dao/mano/NsdPackage.java
@@ -3,6 +3,7 @@ package com.ubiqube.etsi.mano.dao.mano;
 import java.util.Set;
 import java.util.UUID;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -13,6 +14,7 @@ import com.ubiqube.etsi.mano.model.vnf.sol005.VnfPkgInfo.OnboardingStateEnum;
 import com.ubiqube.etsi.mano.model.vnf.sol005.VnfPkgInfo.OperationalStateEnum;
 import com.ubiqube.etsi.mano.model.vnf.sol005.VnfPkgInfo.UsageStateEnum;
 
+@Entity
 public class NsdPackage {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
