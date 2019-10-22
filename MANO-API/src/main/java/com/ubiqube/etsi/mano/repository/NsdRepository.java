e191aeaac151de2df6febfd1e024ca54b821cd6f
Olivier Vignaud <ovi@ubiqube.com>
2019-10-22 15:24:56 +0200

    Add some @Nonnull.

--- MANO-API/src/main/java/com/ubiqube/etsi/mano/repository/NsdRepository.java
+++ MANO-API/src/main/java/com/ubiqube/etsi/mano/repository/NsdRepository.java
@@ -1,5 +1,6 @@
 package com.ubiqube.etsi.mano.repository;
 
+import javax.annotation.Nonnull;
 import javax.validation.constraints.NotNull;
 
 import com.ubiqube.etsi.mano.model.nsd.sol005.NsDescriptorsNsdInfo;
@@ -8,9 +9,9 @@ import com.ubiqube.etsi.mano.model.nslcm.sol005.NsLcmOpOccsNsLcmOpOcc;
 import com.ubiqube.etsi.mano.model.nslcm.sol005.NsLcmOpOccsNsLcmOpOcc.LcmOperationTypeEnum;
 
 public interface NsdRepository extends CrudRepository<NsDescriptorsNsdInfo>, BinaryRepository {
-	void changeNsdUpdateState(NsDescriptorsNsdInfo nsdInfo, NsdUsageStateEnum inUse);
+	void changeNsdUpdateState(NsDescriptorsNsdInfo nsdInfo, @Nonnull NsdUsageStateEnum inUse);
 
-	NsLcmOpOccsNsLcmOpOcc createLcmOpOccs(String nsInstanceId, LcmOperationTypeEnum instantiate);
+	NsLcmOpOccsNsLcmOpOcc createLcmOpOccs(String nsInstanceId, @Nonnull LcmOperationTypeEnum instantiate);
 
 	void attachProcessIdToLcmOpOccs(@NotNull String lcmOpOccsId, String processId);
 }
