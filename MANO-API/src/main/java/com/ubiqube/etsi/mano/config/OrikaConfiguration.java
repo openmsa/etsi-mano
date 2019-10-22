dad842b1a40bda52159de936198060c87bd0e2bd
Olivier Vignaud <ovi@ubiqube.com>
2019-10-22 15:20:47 +0200

    Fix Orika config for NSD Package.

--- MANO-API/src/main/java/com/ubiqube/etsi/mano/config/OrikaConfiguration.java
+++ MANO-API/src/main/java/com/ubiqube/etsi/mano/config/OrikaConfiguration.java
@@ -2,7 +2,9 @@ package com.ubiqube.etsi.mano.config;
 
 import org.springframework.stereotype.Component;
 
+import com.ubiqube.etsi.mano.dao.mano.NsdPackage;
 import com.ubiqube.etsi.mano.mapper.UuidConverter;
+import com.ubiqube.etsi.mano.model.nsd.sol005.NsDescriptorsNsdInfo;
 
 import ma.glasnost.orika.MapperFactory;
 import ma.glasnost.orika.converter.ConverterFactory;
@@ -13,6 +15,12 @@ public class OrikaConfiguration implements OrikaMapperFactoryConfigurer {
 
 	@Override
 	public void configure(final MapperFactory orikaMapperFactory) {
+		orikaMapperFactory.classMap(NsDescriptorsNsdInfo.class, NsdPackage.class)
+				.field("vnfPkgIds{}", "vnfPkgIds{id}")
+				.field("pnfdInfoIds{}", "pnfdInfoIds{id}")
+				.field("nestedNsdInfoIds{}", "nestedNsdInfoIds{id}")
+				.byDefault()
+				.register();
 		final ConverterFactory converterFactory = orikaMapperFactory.getConverterFactory();
 		converterFactory.registerConverter(new UuidConverter());
 	}
