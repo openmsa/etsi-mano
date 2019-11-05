a620a39d93f1b40ce7651c4923e36804538bdfc7
Olivier Vignaud <ovi@ubiqube.com>
2019-11-05 17:14:12 +0100

    Fix EntrySchema attributes.

--- package-parser/src/main/java/com/ubiqube/parser/tosca/EntrySchema.java
+++ package-parser/src/main/java/com/ubiqube/parser/tosca/EntrySchema.java
@@ -1,7 +1,11 @@
 package com.ubiqube.parser.tosca;
 
+import java.util.List;
+
 public class EntrySchema {
 	private String type;
+	private String description;
+	private List<Object> constraints;
 
 	public String getType() {
 		return type;
@@ -9,6 +13,22 @@ public class EntrySchema {
 
 	public void setType(final String type) {
 		this.type = type;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(final String description) {
+		this.description = description;
+	}
+
+	public List<Object> getConstraints() {
+		return constraints;
+	}
+
+	public void setConstraints(final List<Object> constraints) {
+		this.constraints = constraints;
 	}
 
 }
