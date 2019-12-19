f709bcb8287893f7c6218558e81261e1cb225877
Olivier Vignaud <ovi@ubiqube.com>
2019-12-19 16:09:33 +0100

    Add missing files.

---
+++ package-parser/src/main/java/com/ubiqube/parser/tosca/api/ToscaApi.java
@@ -0,0 +1,13 @@
+package com.ubiqube.parser.tosca.api;
+
+import java.util.List;
+
+import com.ubiqube.parser.tosca.ToscaContext;
+
+public class ToscaApi {
+
+	public <T extends Class> List<T> getObjects(final ToscaContext root, final T t) {
+		root.getNodeType();
+		return null;
+	}
+}
