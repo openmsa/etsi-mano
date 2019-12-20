6b3303966d5c2c42260edf0f1ab7816d3d3344e2
Olivier Vignaud <ovi@ubiqube.com>
2019-12-20 09:08:51 +0100

    Some work around API.

--- package-parser/src/main/java/com/ubiqube/parser/tosca/api/ToscaApi.java
+++ package-parser/src/main/java/com/ubiqube/parser/tosca/api/ToscaApi.java
@@ -1,13 +1,29 @@
 package com.ubiqube.parser.tosca.api;
 
 import java.util.List;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
+import com.ubiqube.parser.tosca.NodeTemplate;
 import com.ubiqube.parser.tosca.ToscaContext;
 
 public class ToscaApi {
 
-	public <T extends Class> List<T> getObjects(final ToscaContext root, final T t) {
-		root.getNodeType();
+	public <T> List<T> getObjects(final ToscaContext root, final Class<T> destination) {
+		final String clazzname = destination.getName();
+		final List<NodeTemplate> nodes = root.getTopologies()
+				.getNodeTemplate()
+				.entrySet()
+				.stream()
+				.filter(x -> root.isAssignableFor(x.getValue(), clazzname))
+				.map(Entry::getValue)
+				.collect(Collectors.toList());
+
+		return mapToscaToClass(nodes, destination);
+	}
+
+	private <T> List<T> mapToscaToClass(final List<NodeTemplate> nodes, final Class<T> destination) {
+		nodes.stream();
 		return null;
 	}
 }
