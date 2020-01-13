c373c113f06947ff9678aced42fd4da299828a50
Olivier Vignaud <ovi@ubiqube.com>
2020-01-13 15:29:44 +0100

    Fix json walker cannot emit a HashMap.

---
+++ MANO-API/src/test/java/com/ubiqube/etsi/mano/service/SpelParserTest.java
@@ -0,0 +1,38 @@
+package com.ubiqube.etsi.mano.service;
+
+import java.io.FileInputStream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.util.StreamUtils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ubiqube.etsi.mano.config.OrikaConfiguration;
+import com.ubiqube.etsi.mano.mapper.JsonWalker;
+import com.ubiqube.etsi.mano.mapper.SpelWriter;
+import com.ubiqube.etsi.mano.model.vnf.sol005.VnfPkgInfo;
+
+import ma.glasnost.orika.MapperFacade;
+import ma.glasnost.orika.impl.DefaultMapperFactory;
+
+public class SpelParserTest {
+	private final DefaultMapperFactory mapperFactory;
+
+	public SpelParserTest() {
+		final OrikaConfiguration orikaConfiguration = new OrikaConfiguration();
+		mapperFactory = new DefaultMapperFactory.Builder().build();
+		orikaConfiguration.configure(mapperFactory);
+	}
+
+	@Test
+	void testComplexPatch() throws Exception {
+		final ObjectMapper _mapper = new ObjectMapper();
+		final FileInputStream fi = new FileInputStream("src/test/resources/json-patch01.json");
+		final MapperFacade _mapperFacade = mapperFactory.getMapperFacade();
+		final SpelWriter _spelWriter = new SpelWriter(_mapperFacade);
+		final JsonWalker _jsonWalker = new JsonWalker(_mapper);
+		final SpelPatcher sp = new SpelPatcher(_mapper, _jsonWalker, _spelWriter);
+		final VnfPkgInfo vpi = new VnfPkgInfo();
+		final byte[] bytes = StreamUtils.copyToByteArray(fi);
+		sp.patch(new String(bytes), vpi);
+	}
+}
