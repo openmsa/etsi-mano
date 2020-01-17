3965118f9ae6e1418675e7f255f3b1a9dd665481
Olivier Vignaud <ovi@ubiqube.com>
2020-01-17 16:03:47 +0000

    WF: Rewriting Task launch to take into account stackid AND removing OBMF calls.

--- WORKFLOWS/ETSI-MANO/NFV/VNF_Mgmt_Based_On_Heat/Process_Execute_Heat_Stack/Tasks/Task_Launch_Stack.php
+++ WORKFLOWS/ETSI-MANO/NFV/VNF_Mgmt_Based_On_Heat/Process_Execute_Heat_Stack/Tasks/Task_Launch_Stack.php
@@ -12,45 +12,47 @@ function list_args()
 
 if (!empty($context['deviceid'])){
 	$devicelongid = substr($context['deviceid'], 3);
-	$create=_obmf_create('{"stack":{"'.$context['stackname'].'":{"object_id":"'.$context['stackname'].'","parameters":[],"template_url":"'.$context['template_url'].'"}}}',$devicelongid);
-	
-$status=substr($create,strpos($create, "<status>")+8);
-$status=substr($status,0,strpos($status,"</status>"));
-$message="";
-if ($status=="ERROR"){
-	//get reason
-	$reason=substr($create,strpos($create, "<message>")+9);
-	$reason=substr($reason,0,strpos($reason, "</message>"));
-	echo prepare_json_response(FAILED, $reason, '');
-}else{
-	//stack is launched
-	$message.="Stack is launched \n";
-	update_asynchronous_task_details($context, $message);
-	
-	//check result
-	$stat="CREATE_IN_PROGRESS";
-	$message.="Deployment status: ".$stat;
-    	while ($stat=="CREATE_IN_PROGRESS") {
-		//wait 3 seconds
-		sleep(3);
-		//launch import
-		$imp=_obmf_import('{"stacklist":"0"}',$devicelongid);
-		$st = substr($imp,strpos($imp, "stack_status&quot")+25);
-		$stat = substr($st,0,strpos($st, "&quot;"));
-		$id = substr($imp,strpos($imp, "id&quo")+15);
-		$id = substr($id,0,strpos($id, "&quot;"));
-		$context['stackid']=$id;
-		$message.=".";
-		update_asynchronous_task_details($context, $message);
-	}
-	$message.="\n";
-	if ($stat=="CREATE_COMPLETE"){
-		echo prepare_json_response(ENDED,"Stack is deployed with id ".$context['stackid'],$context, true);
+	$create = json_decode(_order_command_execute ($devicelongid, 'CREATE', '{"stack":{"'.$context['stackname'].'":{"object_id":"'.$context['stackname'].'","parameters":[],"template_url":"'.$context['template_url'].'"}}}'), 1);
+	$innerMsg = json_decode($create['wo_newparams']['message'], 1);
+	//logToFile(debug_dump($create, "RESPONSESSSS ====================\n "));
+	$status = $create['wo_newparams']['status'];
+	$stackId = $innerMsg['stack']['id'];
+	$context['stackid'] = $stackId;
+	$message="";
+	if ($status=="ERROR"){
+		//get reason
+		$reason = $create['wo_newparams']['message'];
+		echo prepare_json_response(FAILED, $reason, '');
 	}else{
-		echo prepare_json_response(FAILED, 'Cannot terminated the stack', '');
+		//stack is launched
+		$message.="Stack is launched \n";
+		update_asynchronous_task_details($context, $message);
+	
+		//check result
+		$stat="CREATE_IN_PROGRESS";
+		$message.="Deployment status: ".$stat;
+		while ($stat=="CREATE_IN_PROGRESS") {
+			//wait 3 seconds
+			sleep(3);
+			//launch import
+			$resp = json_decode(_order_command_execute ($devicelongid, 'IMPORT', '{"stacklist":"0"}'), 1);
+			$innerMsg = json_decode($resp['wo_newparams']['message'], 1);
+			//logToFile(debug_dump($innerMsg, "RESPONSESSSS ====================[$stackId]\n"));
+			$stack = $innerMsg['stacklist'];
+			$stack = $stack[$stackId];
+			$stat = $stack['stack_status'];
+			$message = $stack['stack_status_reason'];
+			update_asynchronous_task_details($context, $message);
+		}
+		$message.="\n";
+		if ($stat=="CREATE_COMPLETE"){
+			echo prepare_json_response(ENDED,"Stack is deployed with id ".$context['stackid'],$context, true);
+		}else{
+			echo prepare_json_response(FAILED, 'Cannot terminated the stack: '.$stat, '');
+		}
 	}
-}
-}else{
+} else {
     echo prepare_json_response(FAILED, 'Missing parameters', '');
 }
-?>
+
+
