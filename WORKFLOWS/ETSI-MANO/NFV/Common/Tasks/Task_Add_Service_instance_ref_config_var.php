bf34df264f998ac0bc4f8f2fca4a74dcadec1548
Vignaud Olivier <ovi@ubiqube.com>
2020-01-21 09:09:03 +0100

    WF: Reformating.

--- WORKFLOWS/ETSI-MANO/NFV/Common/Tasks/Task_Add_Service_instance_ref_config_var.php
+++ WORKFLOWS/ETSI-MANO/NFV/Common/Tasks/Task_Add_Service_instance_ref_config_var.php
@@ -1,23 +1,21 @@
 <?php
-
 require_once '/opt/fmc_repository/Process/Reference/Common/common.php';
 
-function list_args() {
-}
+function list_args()
+{}
 
 foreach ($context['servers'] as $server) {
 
-$device_id = substr($server['device_id'], 3);
-$response = _configuration_variable_create ($device_id, "VNFM_SERVICE_INSTANCE_REFERENCE", $context['SERVICEINSTANCEREFERENCE']);
-$response = json_decode($response, true);
-if ($response['wo_status'] !== ENDED) {
-	$response = json_encode($response);
-	echo $response;
-	exit;
-}
+	$device_id = substr($server['device_id'], 3);
+	$response = _configuration_variable_create($device_id, "VNFM_SERVICE_INSTANCE_REFERENCE", $context['SERVICEINSTANCEREFERENCE']);
+	$response = json_decode($response, true);
+	if ($response['wo_status'] !== ENDED) {
+		$response = json_encode($response);
+		echo $response;
+		exit();
+	}
 }
 
 $response = prepare_json_response(ENDED, "Configuration variable VNFM_SERVICE_INSTANCE_REFERENCE created successfully to the device.", $context, true);
 echo $response;
 
-?>
