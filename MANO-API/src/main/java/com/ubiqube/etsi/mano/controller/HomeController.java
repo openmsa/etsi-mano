cf7359eb1fe95b3d0a2b0e8395307741d0243b74
Olivier Vignaud <ovi@ubiqube.com>
2020-02-03 15:59:16 +0100

    Change git / info to actuator.

--- MANO-API/src/main/java/com/ubiqube/etsi/mano/controller/HomeController.java
+++ MANO-API/src/main/java/com/ubiqube/etsi/mano/controller/HomeController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import com.ubiqube.etsi.mano.config.Version;
 import com.ubiqube.etsi.mano.dao.mano.VnfPackage;
 
 import springfox.documentation.annotations.ApiIgnore;
@@ -23,84 +22,4 @@ public class HomeController {
 		return ResponseEntity.ok(vnfPackage);
 	}
 
-	@GetMapping(value = "/version")
-	public ResponseEntity<VersionBean> version() {
-		return ResponseEntity.ok(new VersionBean());
-	}
-
-	class VersionBean {
-		private String mavenGroup = Version.MAVEN_GROUP;
-		private String mavenName = Version.MAVEN_NAME;
-		private String version = Version.VERSION;
-		private int gitRevision = Version.GIT_REVISION;
-		private String gitSha = Version.GIT_SHA;
-		private String gitDate = Version.GIT_DATE;
-		private String buildDate = Version.BUILD_DATE;
-		private long buildUnixTime = Version.BUILD_UNIX_TIME;
-
-		public String getMavenGroup() {
-			return mavenGroup;
-		}
-
-		public void setMavenGroup(final String mavenGroup) {
-			this.mavenGroup = mavenGroup;
-		}
-
-		public String getMavenName() {
-			return mavenName;
-		}
-
-		public void setMavenName(final String mavenName) {
-			this.mavenName = mavenName;
-		}
-
-		public String getVersion() {
-			return version;
-		}
-
-		public void setVersion(final String version) {
-			this.version = version;
-		}
-
-		public int getGitRevision() {
-			return gitRevision;
-		}
-
-		public void setGitRevision(final int gitRevision) {
-			this.gitRevision = gitRevision;
-		}
-
-		public String getGitSha() {
-			return gitSha;
-		}
-
-		public void setGitSha(final String gitSha) {
-			this.gitSha = gitSha;
-		}
-
-		public String getGitDate() {
-			return gitDate;
-		}
-
-		public void setGitDate(final String gitDate) {
-			this.gitDate = gitDate;
-		}
-
-		public String getBuildDate() {
-			return buildDate;
-		}
-
-		public void setBuildDate(final String buildDate) {
-			this.buildDate = buildDate;
-		}
-
-		public long getBuildUnixTime() {
-			return buildUnixTime;
-		}
-
-		public void setBuildUnixTime(final long buildUnixTime) {
-			this.buildUnixTime = buildUnixTime;
-		}
-
-	}
 }
