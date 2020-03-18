0fb4d19d69b36469379ae3320b1b9888c63aedc2
Olivier Vignaud <ovi@ubiqube.com>
2020-03-18 13:55:47 +0100

    Make VimConnInfo a repository.

--- MANO-API/src/main/java/com/ubiqube/etsi/mano/jpa/VimConnectionInformationJpa.java
+++ MANO-API/src/main/java/com/ubiqube/etsi/mano/jpa/VimConnectionInformationJpa.java
@@ -3,9 +3,11 @@ package com.ubiqube.etsi.mano.jpa;
 import java.util.UUID;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ubiqube.etsi.mano.dao.mano.VimConnectionInformation;
 
+@Repository
 public interface VimConnectionInformationJpa extends CrudRepository<VimConnectionInformation, UUID> {
 
 }
