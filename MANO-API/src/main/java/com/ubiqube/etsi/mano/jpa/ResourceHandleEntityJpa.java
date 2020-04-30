004468d7efd495ea7ca204f6d165a6aae6af8f42
Olivier Vignaud <ovi@ubiqube.com>
2020-04-30 15:45:30 +0200

    Add resourceEntity.

---
+++ MANO-API/src/main/java/com/ubiqube/etsi/mano/jpa/ResourceHandleEntityJpa.java
@@ -0,0 +1,11 @@
+package com.ubiqube.etsi.mano.jpa;
+
+import java.util.UUID;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.ubiqube.etsi.mano.dao.mano.ResourceHandleEntity;
+
+public interface ResourceHandleEntityJpa extends CrudRepository<ResourceHandleEntity, UUID> {
+
+}
