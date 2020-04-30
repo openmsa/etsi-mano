5059051073ec68c22308aa1b4eccfe3650317b6f
Olivier Vignaud <ovi@ubiqube.com>
2020-04-30 09:22:12 +0200

    Add friendly name for noopUow.

--- MANO-API/src/main/java/com/ubiqube/etsi/mano/service/graph/NoopUow.java
+++ MANO-API/src/main/java/com/ubiqube/etsi/mano/service/graph/NoopUow.java
@@ -2,20 +2,25 @@ package com.ubiqube.etsi.mano.service.graph;
 
 import java.util.Map;
 
+import org.apache.commons.lang3.RandomStringUtils;
+
+import com.ubiqube.etsi.mano.dao.mano.ResourceHandleEntity;
 import com.ubiqube.etsi.mano.dao.mano.VimConnectionInformation;
 import com.ubiqube.etsi.mano.service.vim.Vim;
 
 public class NoopUow extends AbstractUnitOfWork {
 	/** Serial. */
 	private static final long serialVersionUID = 1L;
+	private final String name;
 
 	public NoopUow() {
-		super(null);
+		super(new ResourceHandleEntity());
+		name = RandomStringUtils.random(5, "abcdefghijklmnopqrstuvwxyz0123456789");
 	}
 
 	@Override
 	public String getName() {
-		return "Noop operation.";
+		return "Noop_operation_" + name;
 	}
 
 	@Override
