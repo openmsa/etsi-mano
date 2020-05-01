bee285c71fe3a244ba1130b2208639e2380b076c
Olivier Vignaud <ovi@ubiqube.com>
2020-05-01 10:55:18 +0200

    Fix LessOrEqual operation.

--- package-parser/tosca-core/src/main/java/com/ubiqube/parser/tosca/constraints/LessOrEqual.java
+++ package-parser/tosca-core/src/main/java/com/ubiqube/parser/tosca/constraints/LessOrEqual.java
@@ -1,12 +1,9 @@
 package com.ubiqube.parser.tosca.constraints;
 
-import com.ubiqube.parser.tosca.ParseException;
-
 public class LessOrEqual extends SimpleValue implements Constraint {
 
 	public LessOrEqual(final String _value) {
 		super(_value);
-		throw new ParseException("key=" + _value);
 	}
 
 }
