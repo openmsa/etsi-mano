0fd81dd6552ab46267299d4af7a6126e7a57f08e
Olivier Vignaud <ovi@ubiqube.com>
2020-05-01 16:41:28 +0200

    Add SOL003 Fault manageent.

---
+++ MANO-API/src/main/java/com/ubiqube/etsi/mano/model/faultmngt/sol003/FaultyResourceType.java
@@ -0,0 +1,45 @@
+package com.ubiqube.etsi.mano.model.faultmngt.sol003;
+
+import java.util.Objects;
+import io.swagger.annotations.ApiModel;
+import com.fasterxml.jackson.annotation.JsonValue;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+/**
+ * The enumeration FaultyResourceType represents those types of faulty resource. 
+ */
+public enum FaultyResourceType {
+  
+  COMPUTE("COMPUTE"),
+  
+  STORAGE("STORAGE"),
+  
+  NETWORK("NETWORK");
+
+  private String value;
+
+  FaultyResourceType(String value) {
+    this.value = value;
+  }
+
+  @Override
+  @JsonValue
+  public String toString() {
+    return String.valueOf(value);
+  }
+
+  @JsonCreator
+  public static FaultyResourceType fromValue(String text) {
+    for (FaultyResourceType b : FaultyResourceType.values()) {
+      if (String.valueOf(b.value).equals(text)) {
+        return b;
+      }
+    }
+    return null;
+  }
+}
+
---
+++ MANO-API/src/main/java/com/ubiqube/etsi/mano/model/faultmngt/sol003/FmSubscriptionLinks.java
@@ -0,0 +1,87 @@
+package com.ubiqube.etsi.mano.model.faultmngt.sol003;
+
+import java.util.Objects;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.validation.annotation.Validated;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ubiqube.etsi.mano.model.Link;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+/**
+ * Links for this resource.
+ */
+@ApiModel(description = "Links for this resource. ")
+@Validated
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2020-05-01T16:03:26.781+02:00")
+
+public class FmSubscriptionLinks {
+	@JsonProperty("self")
+	private Link self = null;
+
+	public FmSubscriptionLinks self(final Link self) {
+		this.self = self;
+		return this;
+	}
+
+	/**
+	 * URI of this resource.
+	 * 
+	 * @return self
+	 **/
+	@ApiModelProperty(required = true, value = "URI of this resource. ")
+	@NotNull
+
+	@Valid
+
+	public Link getSelf() {
+		return self;
+	}
+
+	public void setSelf(final Link self) {
+		this.self = self;
+	}
+
+	@Override
+	public boolean equals(final java.lang.Object o) {
+		if (this == o) {
+			return true;
+		}
+		if ((o == null) || (getClass() != o.getClass())) {
+			return false;
+		}
+		final FmSubscriptionLinks fmSubscriptionLinks = (FmSubscriptionLinks) o;
+		return Objects.equals(this.self, fmSubscriptionLinks.self);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(self);
+	}
+
+	@Override
+	public String toString() {
+		final StringBuilder sb = new StringBuilder();
+		sb.append("class FmSubscriptionLinks {\n");
+
+		sb.append("    self: ").append(toIndentedString(self)).append("\n");
+		sb.append("}");
+		return sb.toString();
+	}
+
+	/**
+	 * Convert the given object to string with each line indented by 4 spaces
+	 * (except the first line).
+	 */
+	private String toIndentedString(final java.lang.Object o) {
+		if (o == null) {
+			return "null";
+		}
+		return o.toString().replace("\n", "\n    ");
+	}
+}
