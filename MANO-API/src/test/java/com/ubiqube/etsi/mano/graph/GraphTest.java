7685f7b7d779329539e0ba4d3e954b7c53ae3cfa
Olivier Vignaud <ovi@ubiqube.com>
2020-05-01 17:10:13 +0200

    Add Graph test.

---
+++ MANO-API/src/test/java/com/ubiqube/etsi/mano/graph/GraphTest.java
@@ -0,0 +1,66 @@
+package com.ubiqube.etsi.mano.graph;
+
+import java.util.Set;
+
+import org.jgrapht.ListenableGraph;
+import org.jgrapht.graph.DefaultListenableGraph;
+import org.jgrapht.graph.DirectedAcyclicGraph;
+import org.junit.jupiter.api.Test;
+
+import com.ubiqube.etsi.mano.service.graph.ConnectivityEdge;
+import com.ubiqube.etsi.mano.service.graph.EdgeListener;
+import com.ubiqube.etsi.mano.service.graph.UnitOfWork;
+
+public class GraphTest {
+
+	@Test
+	void testName() throws Exception {
+		final ListenableGraph<UnitOfWork, ConnectivityEdge> g = new DefaultListenableGraph(new DirectedAcyclicGraph(ConnectivityEdge.class));
+		g.addGraphListener(new EdgeListener<UnitOfWork>());
+		final UnitOfWork vduA = new TestUnitOfWork("A");
+		final UnitOfWork vduB = new TestUnitOfWork("B");
+		final UnitOfWork vduC = new TestUnitOfWork("C");
+		final UnitOfWork vduD = new TestUnitOfWork("D");
+		final UnitOfWork vduE = new TestUnitOfWork("E");
+		final UnitOfWork vduF = new TestUnitOfWork("F");
+		g.addVertex(vduA);
+		g.addVertex(vduB);
+		g.addVertex(vduC);
+		g.addVertex(vduD);
+		g.addVertex(vduE);
+		g.addVertex(vduF);
+
+		g.addEdge(vduA, vduB);
+		g.addEdge(vduA, vduC);
+
+		g.addEdge(vduB, vduD);
+		g.addEdge(vduC, vduD);
+
+		g.addEdge(vduE, vduC);
+		g.addEdge(vduE, vduF);
+		final UnitOfWork root = new TestUnitOfWork("Start");
+		g.addVertex(root);
+		g.vertexSet().stream()
+				.filter(key -> g.incomingEdgesOf(key).isEmpty())
+				.forEach(key -> {
+					if (key != root) {
+						g.addEdge(root, key);
+					}
+				});
+		// And end Node
+		final UnitOfWork end = new TestUnitOfWork("End");
+		g.addVertex(end);
+		g.vertexSet().stream()
+				.filter(key -> g.outgoingEdgesOf(key).isEmpty())
+				.forEach(key -> {
+					if (key != end) {
+						g.addEdge(key, end);
+					}
+				});
+		System.out.println("==========================");
+		final Set<ConnectivityEdge> edg = g.edgesOf(vduB);
+		edg.forEach(x -> {
+			System.out.println("Edge: " + x.getSource().getName() + " -> " + x.getTarget().getName());
+		});
+	}
+}
---
+++ MANO-API/src/test/java/com/ubiqube/etsi/mano/graph/TestUnitOfWork.java
@@ -0,0 +1,58 @@
+package com.ubiqube.etsi.mano.graph;
+
+import java.util.Map;
+
+import com.ubiqube.etsi.mano.dao.mano.ResourceHandleEntity;
+import com.ubiqube.etsi.mano.dao.mano.VimConnectionInformation;
+import com.ubiqube.etsi.mano.service.graph.UnitOfWork;
+import com.ubiqube.etsi.mano.service.vim.Vim;
+
+public class TestUnitOfWork implements UnitOfWork {
+	/** Serial. */
+	private static final long serialVersionUID = 1L;
+
+	private final String name;
+
+	public TestUnitOfWork(final String _name) {
+		name = _name;
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String toString() {
+		return "TestUnitOfWork [name=" + name + "]";
+	}
+
+	@Override
+	public String exec(final VimConnectionInformation vimConnectionInformation, final Vim vim, final Map<String, String> context) {
+		try {
+			Thread.sleep((long) (Math.random() * 5000));
+		} catch (final InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "Ended";
+	}
+
+	@Override
+	public UowType getType() {
+		return UowType.COMPUTE;
+	}
+
+	@Override
+	public ResourceHandleEntity getResourceHandleEntity() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getToscaName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
