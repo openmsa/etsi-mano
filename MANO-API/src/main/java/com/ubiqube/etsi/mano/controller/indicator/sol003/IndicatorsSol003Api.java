7771a27c7fc92acf52d3e25e6488f2b10015f757
Olivier Vignaud <ovi@ubiqube.com>
2020-05-01 16:42:22 +0200

    Add fault indicator.

---
+++ MANO-API/src/main/java/com/ubiqube/etsi/mano/controller/indicator/sol003/IndicatorsSol003Api.java
@@ -0,0 +1,36 @@
+package com.ubiqube.etsi.mano.controller.indicator.sol003;
+
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2020-05-01T15:58:19.438+02:00")
+
+@Controller
+public class IndicatorsSol003Api implements IndicatorsSol003 {
+
+	private final ObjectMapper objectMapper;
+
+	private final HttpServletRequest request;
+
+	@org.springframework.beans.factory.annotation.Autowired
+	public IndicatorsSol003Api(final ObjectMapper objectMapper, final HttpServletRequest request) {
+		this.objectMapper = objectMapper;
+		this.request = request;
+	}
+
+	@Override
+	public Optional<ObjectMapper> getObjectMapper() {
+		return Optional.ofNullable(objectMapper);
+	}
+
+	@Override
+	public Optional<HttpServletRequest> getRequest() {
+		return Optional.ofNullable(request);
+	}
+
+}
