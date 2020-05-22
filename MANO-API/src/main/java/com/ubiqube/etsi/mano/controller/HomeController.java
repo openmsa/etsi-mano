625aaa8364326655ff9f98cd36ef00721d167803
Olivier Vignaud <ovi@ubiqube.com>
2020-05-22 17:34:22 +0200

    Add delete VNF Lcm & grant request.

--- MANO-API/src/main/java/com/ubiqube/etsi/mano/controller/HomeController.java
+++ MANO-API/src/main/java/com/ubiqube/etsi/mano/controller/HomeController.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,7 +14,9 @@ import com.ubiqube.etsi.mano.dao.mano.TemporaryDownload;
 import com.ubiqube.etsi.mano.dao.mano.TemporaryDownload.ObjectType;
 import com.ubiqube.etsi.mano.dao.mano.VimConnectionInformation;
 import com.ubiqube.etsi.mano.dao.mano.VnfPackage;
+import com.ubiqube.etsi.mano.jpa.GrantRequestJpa;
 import com.ubiqube.etsi.mano.jpa.VimConnectionInformationJpa;
+import com.ubiqube.etsi.mano.jpa.VnfLcmOpOccsJpa;
 import com.ubiqube.etsi.mano.model.VimConnectionInfo;
 import com.ubiqube.etsi.mano.service.TemporaryDownloadService;
 
@@ -26,11 +29,16 @@ public class HomeController {
 	private final VimConnectionInformationJpa vciJpa;
 	private final MapperFacade mapper;
 	private final TemporaryDownloadService temporaryDownloadService;
+	private final GrantRequestJpa grandRequestJpa;
+	private final VnfLcmOpOccsJpa vnfLcmOpOccsJpa;
 
-	public HomeController(final VimConnectionInformationJpa _vciJpa, final MapperFacade _mapper, final TemporaryDownloadService _temporaryDownloadService) {
+	public HomeController(final VimConnectionInformationJpa _vciJpa, final MapperFacade _mapper, final TemporaryDownloadService _temporaryDownloadService,
+			final GrantRequestJpa _grandRequestJpa, final VnfLcmOpOccsJpa _vnfLcmOpOccsJpa) {
 		vciJpa = _vciJpa;
 		mapper = _mapper;
 		temporaryDownloadService = _temporaryDownloadService;
+		grandRequestJpa = _grandRequestJpa;
+		vnfLcmOpOccsJpa = _vnfLcmOpOccsJpa;
 	}
 
 	@GetMapping(value = "/")
@@ -60,5 +68,17 @@ public class HomeController {
 	public ResponseEntity<TemporaryDownload> exposeAnonymous(@PathVariable("objectType") final ObjectType objectType, @PathVariable("id") final UUID id) {
 		final TemporaryDownload td = temporaryDownloadService.exposeDocument(objectType, id);
 		return ResponseEntity.ok(td);
+	}
+
+	@DeleteMapping(value = "/admin/vnf-lcm-op-occs/{id}")
+	public ResponseEntity<Object> deleteLcmOpOccs(@PathVariable("id") final UUID id) {
+		vnfLcmOpOccsJpa.deleteById(id);
+		return ResponseEntity.ok().build();
+	}
+
+	@DeleteMapping(value = "/admin/grants/{id}")
+	public ResponseEntity<Object> deletegrants(@PathVariable("id") final UUID id) {
+		grandRequestJpa.deleteById(id);
+		return ResponseEntity.ok().build();
 	}
 }
