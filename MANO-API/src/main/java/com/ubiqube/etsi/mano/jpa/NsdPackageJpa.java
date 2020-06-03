21f1adb1f2411fdbcb50e1c17835e9c8bce4dbf8
Olivier Vignaud <ovi@ubiqube.com>
2020-06-03 15:42:29 +0200

    Fix get Nested Nsd query.

--- MANO-API/src/main/java/com/ubiqube/etsi/mano/jpa/NsdPackageJpa.java
+++ MANO-API/src/main/java/com/ubiqube/etsi/mano/jpa/NsdPackageJpa.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.ubiqube.etsi.mano.dao.mano.NsdPackage;
@@ -11,5 +12,9 @@ import com.ubiqube.etsi.mano.dao.mano.NsdPackage;
 public interface NsdPackageJpa extends CrudRepository<NsdPackage, UUID> {
 	Optional<NsdPackage> findByNsdInvariantId(String nsdInvariantId);
 
+	@Query("select child \n" +
+			" 	from NsdPackageNsdPackage nsdpackage0_\n" +
+			" 	left outer join NsdPackage child on nsdpackage0_.child = child \n" +
+			" 	where parent_id = ?1 ")
 	Set<NsdPackage> findByNestedNsdInfoIds_Parent(NsdPackage nsdPackage);
 }
