eeac3a75801cd6edd5228cd6ae424e2cef835046
Olivier Vignaud <ovi@ubiqube.com>
2020-06-10 16:37:03 +0200

    Make OperateChange Serializable.

--- MANO-API/src/main/java/com/ubiqube/etsi/mano/dao/mano/OperateChanges.java
+++ MANO-API/src/main/java/com/ubiqube/etsi/mano/dao/mano/OperateChanges.java
@@ -1,11 +1,16 @@
 package com.ubiqube.etsi.mano.dao.mano;
 
+import java.io.Serializable;
+
 import javax.persistence.Embeddable;
 
 import com.ubiqube.etsi.mano.model.nslcm.VnfOperationalStateType;
 
 @Embeddable
-public class OperateChanges {
+public class OperateChanges implements Serializable {
+
+	/** Serial. */
+	private static final long serialVersionUID = 1L;
 
 	private VnfOperationalStateType terminationType;
 
