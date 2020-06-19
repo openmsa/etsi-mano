d3dd6a0111e93b4c186423777e89c9e86ac1251f
Olivier Vignaud <ovi@ubiqube.com>
2020-06-19 10:39:16 +0200

    Reduce number of thread used.

--- MANO-API/src/main/java/com/ubiqube/etsi/mano/service/event/jms/GrantController.java
+++ MANO-API/src/main/java/com/ubiqube/etsi/mano/service/event/jms/GrantController.java
@@ -18,7 +18,7 @@ public class GrantController {
 		grantAction = _grantAction;
 	}
 
-	@JmsListener(destination = "system.actions.grants", concurrency = "10-25")
+	@JmsListener(destination = "system.actions.grants", concurrency = "5-10")
 	@Transactional(TxType.NEVER)
 	@org.springframework.transaction.annotation.Transactional(propagation = Propagation.NEVER)
 	public void onEvent(final GrantMessage ev) {
