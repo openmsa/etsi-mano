6c671f39e7ab0aeaac69a83c3ecc1bca3fef500d
Olivier Vignaud <ovi@ubiqube.com>
2020-06-25 15:33:40 +0200

    Remove unused method in NsInstance repository.

--- MANO-API/src/main/java/com/ubiqube/etsi/mano/repository/NsInstanceRepository.java
+++ MANO-API/src/main/java/com/ubiqube/etsi/mano/repository/NsInstanceRepository.java
@@ -1,10 +1,7 @@
 package com.ubiqube.etsi.mano.repository;
 
 import com.ubiqube.etsi.mano.dao.mano.NsdInstance;
-import com.ubiqube.etsi.mano.model.nslcm.InstantiationStateEnum;
 
 public interface NsInstanceRepository extends CrudRepository<NsdInstance> {
-
-	void changeNsdUpdateState(NsdInstance nsInstance, InstantiationStateEnum notInstantiated);
-
+	// Nothing.
 }
