6fd184fb3f09d3ff701cf111d15428fbd2a408a4
Olivier Vignaud <ovi@ubiqube.com>
2020-08-05 10:16:47 +0200

    Remove NSD VNF instances repository binary nature.

--- MANO-API/src/main/java/com/ubiqube/etsi/mano/repository/jpa/NsInstanceDb.java
+++ MANO-API/src/main/java/com/ubiqube/etsi/mano/repository/jpa/NsInstanceDb.java
@@ -7,17 +7,14 @@ import javax.persistence.EntityManager;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ubiqube.etsi.mano.dao.mano.NsdInstance;
-import com.ubiqube.etsi.mano.repository.ContentManager;
-import com.ubiqube.etsi.mano.repository.NamingStrategy;
 import com.ubiqube.etsi.mano.repository.NsInstanceRepository;
 
 @Service
-public class NsInstanceDb extends AbstractDirectJpa<NsdInstance> implements NsInstanceRepository {
+public class NsInstanceDb extends AbstractJpaOnly<NsdInstance> implements NsInstanceRepository {
 
-	public NsInstanceDb(final EntityManager em, final CrudRepository<NsdInstance, UUID> repository, final ContentManager contentManager, final ObjectMapper jsonMapper, final NamingStrategy namingStrategy) {
-		super(em, repository, contentManager, jsonMapper, namingStrategy);
+	public NsInstanceDb(final EntityManager em, final CrudRepository<NsdInstance, UUID> repository) {
+		super(em, repository);
 	}
 
 	@Override
--- MANO-API/src/main/java/com/ubiqube/etsi/mano/repository/jpa/VnfInstanceDb.java
+++ MANO-API/src/main/java/com/ubiqube/etsi/mano/repository/jpa/VnfInstanceDb.java
@@ -7,20 +7,17 @@ import javax.validation.constraints.NotNull;
 
 import org.springframework.stereotype.Service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ubiqube.etsi.mano.dao.mano.VnfInstance;
 import com.ubiqube.etsi.mano.jpa.VnfInstanceJpa;
-import com.ubiqube.etsi.mano.repository.ContentManager;
-import com.ubiqube.etsi.mano.repository.NamingStrategy;
 import com.ubiqube.etsi.mano.repository.VnfInstancesRepository;
 
 @Service
-public class VnfInstanceDb extends AbstractDirectJpa<VnfInstance> implements VnfInstancesRepository {
+public class VnfInstanceDb extends AbstractJpaOnly<VnfInstance> implements VnfInstancesRepository {
 
 	private final VnfInstanceJpa repository;
 
-	public VnfInstanceDb(final EntityManager em, final VnfInstanceJpa _repository, final ContentManager contentManager, final ObjectMapper jsonMapper, final NamingStrategy namingStrategy) {
-		super(em, _repository, contentManager, jsonMapper, namingStrategy);
+	public VnfInstanceDb(final EntityManager em, final VnfInstanceJpa _repository) {
+		super(em, _repository);
 		repository = _repository;
 	}
 
