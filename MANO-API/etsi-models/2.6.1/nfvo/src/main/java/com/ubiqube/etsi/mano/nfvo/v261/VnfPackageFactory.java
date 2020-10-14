ab22378a760d0f597b29d62568585ada77f5199f
Olivier Vignaud <ovi@ubiqube.com>
2020-10-14 16:13:54 +0200

    Remove createVnfPkgInfo Factory.

--- MANO-API/etsi-models/2.6.1/nfvo/src/main/java/com/ubiqube/etsi/mano/nfvo/v261/VnfPackageFactory.java
+++ MANO-API/etsi-models/2.6.1/nfvo/src/main/java/com/ubiqube/etsi/mano/nfvo/v261/VnfPackageFactory.java
@@ -1,31 +1,14 @@
 package com.ubiqube.etsi.mano.nfvo.v261;
 
-import java.util.Map;
-
 import javax.annotation.Nonnull;
 
 import com.ubiqube.etsi.mano.Constants;
 import com.ubiqube.etsi.mano.common.v261.model.vnf.Checksum;
 import com.ubiqube.etsi.mano.common.v261.model.vnf.VnfPackageArtifactInfo;
-import com.ubiqube.etsi.mano.dao.mano.OnboardingStateType;
-import com.ubiqube.etsi.mano.dao.mano.PackageOperationalState;
-import com.ubiqube.etsi.mano.dao.mano.PackageUsageState;
-import com.ubiqube.etsi.mano.dao.mano.VnfPackage;
 
 public class VnfPackageFactory {
 	private VnfPackageFactory() {
 		// Nothing.
-	}
-
-	@Nonnull
-	public static VnfPackage createVnfPkgInfo(final Map<String, String> userData) {
-		final VnfPackage vnfPkgInfo = new VnfPackage();
-		vnfPkgInfo.setOnboardingState(OnboardingStateType.CREATED);
-		vnfPkgInfo.setUserDefinedData(userData);
-		vnfPkgInfo.setOperationalState(PackageOperationalState.DISABLED);
-		vnfPkgInfo.setUsageState(PackageUsageState.NOT_IN_USE);
-
-		return vnfPkgInfo;
 	}
 
 	@Nonnull
