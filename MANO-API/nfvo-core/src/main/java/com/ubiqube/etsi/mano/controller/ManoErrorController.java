18b55a9c4f2b4d8771697ff0a18276d769f414eb
Olivier Vignaud <ovi@ubiqube.com>
2020-11-19 14:20:57 +0100

    Fix error message in ErrorController.

--- MANO-API/nfvo-core/src/main/java/com/ubiqube/etsi/mano/controller/ManoErrorController.java
+++ MANO-API/nfvo-core/src/main/java/com/ubiqube/etsi/mano/controller/ManoErrorController.java
@@ -24,6 +24,8 @@ import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.web.servlet.error.AbstractErrorController;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.error.ErrorAttributeOptions.Include;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -52,8 +54,8 @@ public class ManoErrorController extends AbstractErrorController {
 	@RequestMapping("/error")
 	public ResponseEntity<ProblemDetails> handleError(final HttpServletRequest request) {
 		final Integer status = (Integer) request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-		final Map<String, Object> ea = getErrorAttributes(request, true);
-		LOG.info("Error controller: {} => {}", status, ea);
+		final Map<String, Object> ea = getErrorAttributes(request, ErrorAttributeOptions.of(Include.EXCEPTION, Include.MESSAGE, Include.BINDING_ERRORS));
+		LOG.trace("Error controller: {} => {}", status, ea);
 		if (status != null) {
 			final ProblemDetails problemDetail = new ProblemDetails(status, (String) ea.get("message"));
 			return ResponseEntity.status(status)
