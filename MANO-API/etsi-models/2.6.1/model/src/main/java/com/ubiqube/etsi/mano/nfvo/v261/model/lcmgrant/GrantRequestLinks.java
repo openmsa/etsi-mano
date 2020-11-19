d60df260f7a57057a52ef525c4ca1035ba495446
Olivier Vignaud <ovi@ubiqube.com>
2020-11-19 10:46:19 +0100

    Fix license in etsi-model/2.6.1.

--- MANO-API/etsi-models/2.6.1/model/src/main/java/com/ubiqube/etsi/mano/nfvo/v261/model/lcmgrant/GrantRequestLinks.java
+++ MANO-API/etsi-models/2.6.1/model/src/main/java/com/ubiqube/etsi/mano/nfvo/v261/model/lcmgrant/GrantRequestLinks.java
@@ -1,17 +1,20 @@
 /**
- * This copy of Woodstox XML processor is licensed under the
- * Apache (Software) License, version 2.0 ("the License").
- * See the License for details about distribution rights, and the
- * specific rights regarding derivate works.
+ *     Copyright (C) 2019-2020 Ubiqube.
  *
- * You may obtain a copy of the License at:
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
  *
- * http://www.apache.org/licenses/
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
  *
- * A copy is also included in the downloadable source code package
- * containing Woodstox, in file "ASL2.0", under the same directory
- * as this file.
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.ubiqube.etsi.mano.nfvo.v261.model.lcmgrant;
 
 import java.util.Objects;
