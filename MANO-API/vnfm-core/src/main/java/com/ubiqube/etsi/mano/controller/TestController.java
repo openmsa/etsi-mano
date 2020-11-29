6c7ce50708e720859ed0ad9c47a63ede504657a9
Olivier Vignaud <ovi@ubiqube.com>
2020-11-29 19:53:15 +0100

    Remove test controller.

--- MANO-API/vnfm-core/src/main/java/com/ubiqube/etsi/mano/controller/TestController.java
+++ MANO-API/vnfm-core/src/main/java/com/ubiqube/etsi/mano/controller/TestController.java
@@ -16,27 +16,10 @@
  */
 package com.ubiqube.etsi.mano.controller;
 
-import java.util.Set;
-import java.util.UUID;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.ubiqube.etsi.mano.dao.mano.ChangeType;
-import com.ubiqube.etsi.mano.dao.mano.ScaleInfo;
-import com.ubiqube.etsi.mano.dao.mano.VnfInstance;
-import com.ubiqube.etsi.mano.dao.mano.VnfPackage;
-import com.ubiqube.etsi.mano.dao.mano.v2.Blueprint;
-import com.ubiqube.etsi.mano.dao.mano.v2.VnfBlueprint;
-import com.ubiqube.etsi.mano.service.VimResourceService;
-import com.ubiqube.etsi.mano.service.VnfInstanceService;
-import com.ubiqube.etsi.mano.service.VnfPackageService;
-import com.ubiqube.etsi.mano.service.graph.VnfPlanExecutor;
-import com.ubiqube.etsi.mano.service.plan.VnfPlanner;
 
 @Controller
 @RequestMapping("/exec")
@@ -44,32 +27,4 @@ public class TestController {
 
 	private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
 
-	private final VnfPlanExecutor planExecutor;
-
-	private final VnfPlanner planner;
-	private final VnfPackageService vnfPackageService;
-	private final VnfInstanceService vnfInstanceService;
-	private final VimResourceService vimResourceService;
-
-	public TestController(final VnfPlanExecutor _planExecutor, final VnfPlanner _planner, final VnfPackageService _vnfPackageService, final VnfInstanceService _vnfInstanceService, final VimResourceService _vimResourceService) {
-		planExecutor = _planExecutor;
-		planner = _planner;
-		vnfPackageService = _vnfPackageService;
-		vnfInstanceService = _vnfInstanceService;
-		vimResourceService = _vimResourceService;
-	}
-
-	@GetMapping(value = "/plan", produces = { "application/json" }, consumes = { "application/json" })
-	public ResponseEntity<Blueprint> testPlan() {
-		final VnfPackage vnfPackage = vnfPackageService.findById(UUID.fromString("d1814a92-7b09-420b-b212-6f9cd078456b"));
-		final VnfInstance vnfInstance = vnfInstanceService.findById(UUID.fromString("e381cdbc-5993-4ae4-9525-89db3237b309"));
-		final VnfBlueprint plan = new VnfBlueprint();
-		plan.setId(UUID.randomUUID());
-		plan.setVnfInstance(vnfInstance);
-		final Set<ScaleInfo> scaling = null;
-		planner.doPlan(vnfPackage, plan, scaling);
-		planner.convertToExecution(plan, ChangeType.ADDED);
-		vimResourceService.allocate(plan);
-		return ResponseEntity.ok(null);
-	}
 }
