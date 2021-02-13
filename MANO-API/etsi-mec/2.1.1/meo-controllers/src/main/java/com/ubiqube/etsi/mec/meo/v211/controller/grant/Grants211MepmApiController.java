4d120a8ae019460d1edf4d5cda02efc83df14aec
Olivier Vignaud <ovi@ubiqube.com>
2021-02-13 16:01:52 +0100

    MEC Grant, check parameters.

--- MANO-API/etsi-mec/2.1.1/meo-controllers/src/main/java/com/ubiqube/etsi/mec/meo/v211/controller/grant/Grants211MepmApiController.java
+++ MANO-API/etsi-mec/2.1.1/meo-controllers/src/main/java/com/ubiqube/etsi/mec/meo/v211/controller/grant/Grants211MepmApiController.java
@@ -20,6 +20,7 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 import java.net.URI;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
@@ -27,6 +28,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ubiqube.etsi.mano.dao.mano.GrantResponse;
 import com.ubiqube.etsi.mano.dao.mano.dto.VnfGrantsRequest;
+import com.ubiqube.etsi.mano.exception.GenericException;
 import com.ubiqube.etsi.mec.controller.grant.AppGrantController;
 import com.ubiqube.etsi.mec.meo.v211.model.grant.Grant;
 import com.ubiqube.etsi.mec.meo.v211.model.grant.GrantRequest;
@@ -62,9 +64,16 @@ public class Grants211MepmApiController implements Grants211MepmApi {
 	@Override
 	public ResponseEntity<Grant> grantPOST(final GrantRequest grantRequest) {
 		final VnfGrantsRequest obj = mapper.map(grantRequest, VnfGrantsRequest.class);
+		checkGrant(obj);
 		final GrantResponse resp = appGrantController.post(obj);
 		final URI location = linkTo(methodOn(Grants211MepmApi.class).grantGET(resp.getId().toString())).withSelfRel().toUri();
 		return ResponseEntity.created(location).build();
+	}
+
+	private static void checkGrant(final VnfGrantsRequest obj) {
+		Optional.ofNullable(obj.getVnfInstance()).orElseThrow(() -> new GenericException("Unable to find App instance."));
+		Optional.ofNullable(obj.getVnfdId()).orElseThrow(() -> new GenericException("Unable to find Appd Id."));
+		Optional.ofNullable(obj.getVnfLcmOpOccs()).orElseThrow(() -> new GenericException("Unable to find App LCM OP OCC."));
 	}
 
 }
