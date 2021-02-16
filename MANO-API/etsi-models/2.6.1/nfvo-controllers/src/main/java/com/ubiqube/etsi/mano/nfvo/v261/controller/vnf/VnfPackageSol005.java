59eba1e0a1adca46da9788bb429824c7c6e404a3
Olivier Vignaud <ovi@ubiqube.com>
2021-02-16 10:06:32 +0100

    Allow consume of anything in VNF package.

--- MANO-API/etsi-models/2.6.1/nfvo-controllers/src/main/java/com/ubiqube/etsi/mano/nfvo/v261/controller/vnf/VnfPackageSol005.java
+++ MANO-API/etsi-models/2.6.1/nfvo-controllers/src/main/java/com/ubiqube/etsi/mano/nfvo/v261/controller/vnf/VnfPackageSol005.java
@@ -132,7 +132,7 @@ public interface VnfPackageSol005 {
 			@ApiResponse(code = 416, message = "Requested Range Not Satisfiable The byte range passed in the \"Range\" header did not match any available byte range in the VNF package file (e.g. \"access after end of file\"). The response body may contain a ProblemDetails structure. ", response = ProblemDetails.class),
 			@ApiResponse(code = 500, message = "Internal Server Error If there is an application error not related to the client's input that cannot be easily mapped to any other HTTP response code (\"catch all error\"), the API producer shall respond withthis response code. The ProblemDetails structure shall be provided, and shall include in the \"detail\" attribute more information about the source of the problem. ", response = ProblemDetails.class),
 			@ApiResponse(code = 503, message = "Service Unavailable If the API producer encounters an internal overload situation of itself or of a system it relies on, it should respond with this response code, following the provisions in IETF RFC 7231 [13] for the use of the Retry-After HTTP header and for the alternative to refuse the connection. The \"ProblemDetails\" structure may be omitted. ", response = ProblemDetails.class) })
-	@GetMapping(value = "/{vnfPkgId}/vnfd", produces = { "application/json", "application/zip", "application/text" }, consumes = { "application/json" })
+	@GetMapping(value = "/{vnfPkgId}/vnfd", produces = { "application/json", "application/zip", "application/text" })
 	ResponseEntity<Resource> vnfPackagesVnfPkgIdVnfdGet(@Nonnull @PathVariable("vnfPkgId") String vnfPkgId);
 
 	/**
