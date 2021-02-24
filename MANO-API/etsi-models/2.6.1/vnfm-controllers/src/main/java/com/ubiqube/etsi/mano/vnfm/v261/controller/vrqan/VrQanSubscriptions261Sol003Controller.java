44b6f7b9c1d9c13be87b7ce567eff6dbec394224
Olivier Vignaud <ovi@ubiqube.com>
2021-02-24 16:10:10 +0100

    Add VrQan 2.6.1.

---
+++ MANO-API/etsi-models/2.6.1/vnfm-controllers/src/main/java/com/ubiqube/etsi/mano/vnfm/v261/controller/vrqan/VrQanSubscriptions261Sol003Controller.java
@@ -0,0 +1,70 @@
+package com.ubiqube.etsi.mano.vnfm.v261.controller.vrqan;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.UUID;
+
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ubiqube.etsi.mano.common.v261.model.Link;
+import com.ubiqube.etsi.mano.dao.mano.subs.SubscriptionType;
+import com.ubiqube.etsi.mano.service.SubscriptionServiceV2;
+import com.ubiqube.etsi.mano.vnfm.v261.model.vrqan.VrQuotaAvailSubscription;
+import com.ubiqube.etsi.mano.vnfm.v261.model.vrqan.VrQuotaAvailSubscriptionLinks;
+import com.ubiqube.etsi.mano.vnfm.v261.model.vrqan.VrQuotaAvailSubscriptionRequest;
+
+/**
+ *
+ * @author Olivier Vignaud <ovi@ubiqube.com>
+ *
+ */
+@RestController
+public class VrQanSubscriptions261Sol003Controller implements VrQanSubscriptions261Sol003Api {
+	private final SubscriptionServiceV2 subscriptionService;
+
+	public VrQanSubscriptions261Sol003Controller(final SubscriptionServiceV2 subscriptionService) {
+		super();
+		this.subscriptionService = subscriptionService;
+	}
+
+	@Override
+	public ResponseEntity<List<VrQuotaAvailSubscription>> subscriptionsGet(final MultiValueMap<String, String> requestParams, final String nextpageOpaqueMarker) {
+		final List<VrQuotaAvailSubscription> ret = subscriptionService.query(requestParams, VrQuotaAvailSubscription.class, VrQanSubscriptions261Sol003Controller::makeLinks, SubscriptionType.VRQAN);
+		return ResponseEntity.ok(ret);
+	}
+
+	@Override
+	public ResponseEntity<VrQuotaAvailSubscription> subscriptionsPost(@Valid final VrQuotaAvailSubscriptionRequest vrQuotaAvailSubscriptionRequest) throws URISyntaxException {
+		final VrQuotaAvailSubscription res = subscriptionService.create(vrQuotaAvailSubscriptionRequest, VrQuotaAvailSubscription.class, VrQanSubscriptions261Sol003Controller::makeLinks, SubscriptionType.VRQAN);
+		final URI location = new URI(res.getLinks().getSelf().getHref());
+		return ResponseEntity.created(location).body(res);
+	}
+
+	@Override
+	public ResponseEntity<Void> subscriptionsSubscriptionIdDelete(final String subscriptionId) {
+		subscriptionService.deleteById(UUID.fromString(subscriptionId), SubscriptionType.VRQAN);
+		return ResponseEntity.noContent().build();
+	}
+
+	@Override
+	public ResponseEntity<VrQuotaAvailSubscription> subscriptionsSubscriptionIdGet(final String subscriptionId) {
+		final VrQuotaAvailSubscription res = subscriptionService.findById(UUID.fromString(subscriptionId), VrQuotaAvailSubscription.class, VrQanSubscriptions261Sol003Controller::makeLinks, SubscriptionType.VRQAN);
+		return ResponseEntity.ok(res);
+	}
+
+	private static void makeLinks(final VrQuotaAvailSubscription subscription) {
+		final VrQuotaAvailSubscriptionLinks links = new VrQuotaAvailSubscriptionLinks();
+		final Link link = new Link();
+		link.setHref(linkTo(methodOn(VrQanSubscriptions261Sol003Api.class).subscriptionsSubscriptionIdGet(subscription.getId())).withSelfRel().getHref());
+		links.setSelf(link);
+		subscription.setLinks(links);
+	}
+}
