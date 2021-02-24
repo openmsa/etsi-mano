d3c6a1e2abb5646c114521aab02d3f4849731e5a
Olivier Vignaud <ovi@ubiqube.com>
2021-02-24 16:42:19 +0100

    Move vnfind sol002.

--- MANO-API/etsi-models/2.6.1/vnfm-controllers/src/main/java/com/ubiqube/etsi/mano/vnfm/v261/controller/vnfind/sol002/Indicators261Sol002ApiController.java
+++ MANO-API/etsi-models/2.6.1/vnfm-controllers/src/main/java/com/ubiqube/etsi/mano/vnfm/v261/controller/vnfind/sol002/Indicators261Sol002ApiController.java
@@ -14,7 +14,7 @@
  *     You should have received a copy of the GNU General Public License
  *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-package com.ubiqube.etsi.mano.vnfm.v261.controller.vnfind;
+package com.ubiqube.etsi.mano.vnfm.v261.controller.vnfind.sol002;
 
 import java.util.Optional;
 
