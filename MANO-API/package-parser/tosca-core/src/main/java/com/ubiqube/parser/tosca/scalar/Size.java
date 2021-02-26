9dff05c1fffafb54b1aaa90f2cde357b6e137417
Olivier Vignaud <ovi@ubiqube.com>
2021-02-26 17:00:50 +0100

    Minor code fix in Size.

--- MANO-API/package-parser/tosca-core/src/main/java/com/ubiqube/parser/tosca/scalar/Size.java
+++ MANO-API/package-parser/tosca-core/src/main/java/com/ubiqube/parser/tosca/scalar/Size.java
@@ -27,32 +27,36 @@ import com.ubiqube.parser.tosca.ParseException;
 
 public class Size {
 	private static final Logger LOG = LoggerFactory.getLogger(Size.class);
-	private final long size;
+
+	private static final Pattern SIZE_PATTERN = Pattern.compile("(?<size>[0-9]+)\\s*(?<unit>[a-zA-Z]+)");
+
+	private final long lsize;
+
 	private final String unit;
 
 	public Size(final String value) {
 		LOG.debug("Size value: {}", value);
-		final Pattern p = Pattern.compile("(?<size>[0-9]+)\\s*(?<unit>[a-zA-Z]+)");
-		final Matcher m = p.matcher(value);
+
+		final Matcher m = SIZE_PATTERN.matcher(value);
 		if (!m.find()) {
 			throw new ParseException("Size scalr: Unable to find a match for: " + value);
 		}
-		size = Long.parseLong(m.group("size"));
+		lsize = Long.parseLong(m.group("size"));
 		unit = m.group("unit");
 	}
 
 	public Size(final Long value) {
-		size = value.longValue();
+		lsize = value.longValue();
 		unit = "b";
 	}
 
 	public String getToscaForm() {
-		return size + " " + unit;
+		return lsize + " " + unit;
 	}
 
 	public BigDecimal getValue() {
 		final BigDecimal mul = getMultiplier(unit);
-		final BigDecimal sizeBd = new BigDecimal(size);
+		final BigDecimal sizeBd = new BigDecimal(lsize);
 		return mul.multiply(sizeBd);
 	}
 
@@ -60,21 +64,29 @@ public class Size {
 		switch (unit2.toLowerCase()) {
 		case "b":
 			return BigDecimal.ONE;
-		case "kb": // (1000 bytes)
+		// (1000 bytes)
+		case "kb":
 			return new BigDecimal("1000");
-		case "kiB":// (1024 bytes)
+		// (1024 bytes)
+		case "kiB":
 			return new BigDecimal("1024");
-		case "mb": // (1000000 bytes)
+		// (1000000 bytes)
+		case "mb":
 			return new BigDecimal("1000000");
-		case "mib": // (1048576 bytes)
+		// (1048576 bytes)
+		case "mib":
 			return new BigDecimal("1048576");
-		case "gb": // (1000000000 bytes)
+		// (1000000000 bytes)
+		case "gb":
 			return new BigDecimal("1000000000");
-		case "gib": // (1073741824 bytes)
+		// (1073741824 bytes)
+		case "gib":
 			return new BigDecimal("1073741824");
-		case "tb": // 1000000000000 bytes)
+		// 1000000000000 bytes)
+		case "tb":
 			return new BigDecimal("1000000000000");
-		case "tib": // 1099511627776
+		// 1099511627776
+		case "tib":
 			return new BigDecimal("1099511627776");
 		default:
 			throw new ParseException("Unknown scalar unit " + unit2);
