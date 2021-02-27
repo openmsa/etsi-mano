6a7d27246908b247c256b63affb68c2424e3464c
Olivier Vignaud <ovi@ubiqube.com>
2021-02-27 16:23:08 +0100

    Prepare vnf lcm 3.3.1.

---
+++ MANO-API/etsi-models/3.3.1/vnfm-controllers/src/main/java/com/ubiqube/etsi/mano/em/v331/controller/vnflcm/VnfLcmSubscriptions331Sol003Controller.java
@@ -0,0 +1,44 @@
+package com.ubiqube.etsi.mano.em.v331.controller.vnflcm;
+
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ubiqube.etsi.mano.em.v331.model.vnflcm.LccnSubscription;
+import com.ubiqube.etsi.mano.em.v331.model.vnflcm.LccnSubscriptionRequest;
+
+/**
+ *
+ * @author Olivier Vignaud <ovi@ubiqube.com>
+ *
+ */
+@RestController
+public class VnfLcmSubscriptions331Sol003Controller implements VnfLcmSubscriptions331Sol003Api {
+
+	@Override
+	public ResponseEntity<LccnSubscription> subscriptionsGet(final MultiValueMap<String, String> requestParams, @Valid final String nextpageOpaqueMarker) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResponseEntity<LccnSubscription> subscriptionsPost(@Valid final LccnSubscriptionRequest body) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResponseEntity<Void> subscriptionsSubscriptionIdDelete(final String subscriptionId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResponseEntity<LccnSubscription> subscriptionsSubscriptionIdGet(final String subscriptionId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ MANO-API/etsi-models/3.3.1/vnfm-controllers/src/main/java/com/ubiqube/etsi/mano/em/v331/controller/vnflcm/VnfSnapshots331Sol003Controller.java
@@ -0,0 +1,46 @@
+package com.ubiqube.etsi.mano.em.v331.controller.vnflcm;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ubiqube.etsi.mano.em.v331.model.vnflcm.CreateVnfSnapshotInfoRequest;
+import com.ubiqube.etsi.mano.em.v331.model.vnflcm.VnfSnapshotInfo;
+
+/**
+ *
+ * @author Olivier Vignaud <ovi@ubiqube.com>
+ *
+ */
+@RestController
+public class VnfSnapshots331Sol003Controller implements VnfSnapshots331Sol003Api {
+
+	@Override
+	public ResponseEntity<List<VnfSnapshotInfo>> vnfSnapshotsGet(final MultiValueMap<String, String> requestParams, @Valid final String nextpageOpaqueMarker) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResponseEntity<VnfSnapshotInfo> vnfSnapshotsPost(@Valid final CreateVnfSnapshotInfoRequest body) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResponseEntity<Void> vnfSnapshotsVnfSnapshotInfoIdDelete(final String vnfSnapshotInfoId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResponseEntity<VnfSnapshotInfo> vnfSnapshotsVnfSnapshotInfoIdGet(final String vnfSnapshotInfoId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
