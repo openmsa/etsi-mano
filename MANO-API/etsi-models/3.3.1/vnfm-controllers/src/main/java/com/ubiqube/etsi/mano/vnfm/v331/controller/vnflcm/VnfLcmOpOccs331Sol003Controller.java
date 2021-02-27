7097ebcc1fd9e1dacb6c0b0d0b849246fed98d26
Olivier Vignaud <ovi@ubiqube.com>
2021-02-27 19:13:20 +0100

    Works on VNF Lcm sol003.

--- MANO-API/etsi-models/3.3.1/vnfm-controllers/src/main/java/com/ubiqube/etsi/mano/vnfm/v331/controller/vnflcm/VnfLcmOpOccs331Sol003Controller.java
+++ MANO-API/etsi-models/3.3.1/vnfm-controllers/src/main/java/com/ubiqube/etsi/mano/vnfm/v331/controller/vnflcm/VnfLcmOpOccs331Sol003Controller.java
@@ -1,13 +1,30 @@
 package com.ubiqube.etsi.mano.vnfm.v331.controller.vnflcm;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import java.util.UUID;
+
 import javax.annotation.security.RolesAllowed;
 import javax.validation.Valid;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ubiqube.etsi.mano.controller.nslcm.VnfInstanceGenericFrontController;
+import com.ubiqube.etsi.mano.dao.mano.ResourceTypeEnum;
+import com.ubiqube.etsi.mano.dao.mano.v2.VnfBlueprint;
+import com.ubiqube.etsi.mano.vnfm.v331.model.vnflcm.AffectedVirtualLink;
+import com.ubiqube.etsi.mano.vnfm.v331.model.vnflcm.AffectedVirtualStorage;
+import com.ubiqube.etsi.mano.vnfm.v331.model.vnflcm.AffectedVnfc;
+import com.ubiqube.etsi.mano.vnfm.v331.model.vnflcm.Link;
 import com.ubiqube.etsi.mano.vnfm.v331.model.vnflcm.VnfLcmOpOcc;
+import com.ubiqube.etsi.mano.vnfm.v331.model.vnflcm.VnfLcmOpOccLinks;
+import com.ubiqube.etsi.mano.vnfm.v331.model.vnflcm.VnfLcmOpOccResourceChanges;
+
+import ma.glasnost.orika.MapperFacade;
 
 /**
  *
@@ -18,40 +35,93 @@ import com.ubiqube.etsi.mano.vnfm.v331.model.vnflcm.VnfLcmOpOcc;
 @RestController
 public class VnfLcmOpOccs331Sol003Controller implements VnfLcmOpOccs331Sol003Api {
 
+	private final VnfInstanceGenericFrontController frontController;
+
+	private final MapperFacade mapper;
+
+	public VnfLcmOpOccs331Sol003Controller(final VnfInstanceGenericFrontController frontController, final MapperFacade _mapper) {
+		super();
+		this.frontController = frontController;
+		mapper = _mapper;
+	}
+
 	@Override
-	public ResponseEntity<VnfLcmOpOcc> vnfLcmOpOccsGet(final MultiValueMap<String, String> requestParams, @Valid final String nextpageOpaqueMarker) {
-		// TODO Auto-generated method stub
-		return null;
+	public ResponseEntity<String> vnfLcmOpOccsGet(final MultiValueMap<String, String> requestParams, @Valid final String nextpageOpaqueMarker) {
+		return frontController.search(requestParams, VnfLcmOpOcc.class, VnfLcmOpOccs331Sol003Controller::makeLinks);
 	}
 
 	@Override
 	public ResponseEntity<Void> vnfLcmOpOccsVnfLcmOpOccIdCancelPost(final String vnfLcmOpOccId) {
-		// TODO Auto-generated method stub
-		return null;
+		return frontController.lcmOpOccCancel(UUID.fromString(vnfLcmOpOccId));
 	}
 
 	@Override
 	public ResponseEntity<VnfLcmOpOcc> vnfLcmOpOccsVnfLcmOpOccIdFailPost(final String vnfLcmOpOccId) {
-		// TODO Auto-generated method stub
-		return null;
+		return frontController.lcmOpOccFail(UUID.fromString(vnfLcmOpOccId));
 	}
 
 	@Override
 	public ResponseEntity<VnfLcmOpOcc> vnfLcmOpOccsVnfLcmOpOccIdGet(final String vnfLcmOpOccId) {
-		// TODO Auto-generated method stub
-		return null;
+		final VnfBlueprint resultDb = frontController.lcmOpOccFindById(UUID.fromString(vnfLcmOpOccId));
+		final VnfLcmOpOcc entity = mapper.map(resultDb, VnfLcmOpOcc.class);
+		final VnfLcmOpOccResourceChanges resourceChanged = new VnfLcmOpOccResourceChanges();
+		resultDb.getTasks().stream()
+				.filter(x -> x.getType() == ResourceTypeEnum.VL)
+				.map(x -> mapper.map(x, AffectedVirtualLink.class))
+				.forEach(resourceChanged::addAffectedVirtualLinksItem);
+		resultDb.getTasks().stream()
+				.filter(x -> x.getType() == ResourceTypeEnum.STORAGE)
+				.map(x -> mapper.map(x, AffectedVirtualStorage.class))
+				.forEach(resourceChanged::addAffectedVirtualStoragesItem);
+		resultDb.getTasks().stream()
+				.filter(x -> x.getType() == ResourceTypeEnum.COMPUTE)
+				.map(x -> mapper.map(x, AffectedVnfc.class))
+				.forEach(resourceChanged::addAffectedVnfcsItem);
+		entity.setResourceChanges(resourceChanged);
+		makeLinks(entity);
+		return new ResponseEntity<>(entity, HttpStatus.OK);
 	}
 
 	@Override
 	public ResponseEntity<Void> vnfLcmOpOccsVnfLcmOpOccIdRetryPost(final String vnfLcmOpOccId) {
-		// TODO Auto-generated method stub
-		return null;
+		return frontController.lcmOpOccRetry(UUID.fromString(vnfLcmOpOccId));
 	}
 
 	@Override
 	public ResponseEntity<Void> vnfLcmOpOccsVnfLcmOpOccIdRollbackPost(final String vnfLcmOpOccId) {
-		// TODO Auto-generated method stub
-		return null;
+		return frontController.lcmOpOccRollback(UUID.fromString(vnfLcmOpOccId));
 	}
 
+	private static void makeLinks(final VnfLcmOpOcc vnfLcmOpOcc) {
+		final String id = vnfLcmOpOcc.getId();
+		final VnfLcmOpOccLinks links = new VnfLcmOpOccLinks();
+		final Link cancel = new Link();
+		cancel.setHref(linkTo(methodOn(VnfLcmOpOccs331Sol003Api.class).vnfLcmOpOccsVnfLcmOpOccIdCancelPost(id)).withSelfRel().getHref());
+		links.setCancel(cancel);
+
+		final Link fail = new Link();
+		fail.setHref(linkTo(methodOn(VnfLcmOpOccs331Sol003Api.class).vnfLcmOpOccsVnfLcmOpOccIdFailPost(id)).withSelfRel().getHref());
+		links.setFail(fail);
+
+		// XXX We can't have this grant link directly, because of classpath on interface.
+		// grant.setHref(linkTo(methodOn(LcmGrants.class).grantsGrantIdGet(vnfLcmOpOcc.getGrantId(),"")).withSelfRel().getHref());
+
+		final Link retry = new Link();
+		retry.setHref(linkTo(methodOn(VnfLcmOpOccs331Sol003Api.class).vnfLcmOpOccsVnfLcmOpOccIdRetryPost(id)).withSelfRel().getHref());
+		links.setRetry(retry);
+
+		final Link rollback = new Link();
+		rollback.setHref(linkTo(methodOn(VnfLcmOpOccs331Sol003Api.class).vnfLcmOpOccsVnfLcmOpOccIdRollbackPost(id)).withSelfRel().getHref());
+		links.setRollback(rollback);
+
+		final Link self = new Link();
+		self.setHref(linkTo(methodOn(VnfLcmOpOccs331Sol003Api.class).vnfLcmOpOccsVnfLcmOpOccIdGet(id)).withSelfRel().getHref());
+		links.setSelf(self);
+
+		final Link vnfInstance = new Link();
+		vnfInstance.setHref(linkTo(methodOn(VnfInstances331Sol003Api.class).vnfInstancesVnfInstanceIdGet(vnfLcmOpOcc.getId())).withSelfRel().getHref());
+		links.setVnfInstance(vnfInstance);
+
+		vnfLcmOpOcc.setLinks(links);
+	}
 }
--- MANO-API/etsi-models/3.3.1/vnfm-controllers/src/main/java/com/ubiqube/etsi/mano/vnfm/v331/controller/vnflcm/VnfLcmSubscriptions331Sol003Controller.java
+++ MANO-API/etsi-models/3.3.1/vnfm-controllers/src/main/java/com/ubiqube/etsi/mano/vnfm/v331/controller/vnflcm/VnfLcmSubscriptions331Sol003Controller.java
@@ -1,5 +1,13 @@
 package com.ubiqube.etsi.mano.vnfm.v331.controller.vnflcm;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.UUID;
+
 import javax.annotation.security.RolesAllowed;
 import javax.validation.Valid;
 
@@ -7,8 +15,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ubiqube.etsi.mano.dao.mano.subs.SubscriptionType;
+import com.ubiqube.etsi.mano.service.SubscriptionServiceV2;
 import com.ubiqube.etsi.mano.vnfm.v331.model.vnflcm.LccnSubscription;
+import com.ubiqube.etsi.mano.vnfm.v331.model.vnflcm.LccnSubscriptionLinks;
 import com.ubiqube.etsi.mano.vnfm.v331.model.vnflcm.LccnSubscriptionRequest;
+import com.ubiqube.etsi.mano.vnfm.v331.model.vnflcm.Link;
 
 /**
  *
@@ -18,29 +30,45 @@ import com.ubiqube.etsi.mano.vnfm.v331.model.vnflcm.LccnSubscriptionRequest;
 @RolesAllowed({ "ROLE_NFVO" })
 @RestController
 public class VnfLcmSubscriptions331Sol003Controller implements VnfLcmSubscriptions331Sol003Api {
+	private final SubscriptionServiceV2 subscriptionService;
 
 	@Override
-	public ResponseEntity<LccnSubscription> subscriptionsGet(final MultiValueMap<String, String> requestParams, @Valid final String nextpageOpaqueMarker) {
-		// TODO Auto-generated method stub
-		return null;
+	public ResponseEntity<List<LccnSubscription>> subscriptionsGet(final MultiValueMap<String, String> requestParams, @Valid final String nextpageOpaqueMarker) {
+		final List<LccnSubscription> ret = subscriptionService.query(requestParams, LccnSubscription.class, VnfLcmSubscriptions331Sol003Controller::makeLinks, SubscriptionType.VNFPM);
+		return ResponseEntity.ok(ret);
+	}
+
+	public VnfLcmSubscriptions331Sol003Controller(final SubscriptionServiceV2 subscriptionService) {
+		super();
+		this.subscriptionService = subscriptionService;
 	}
 
 	@Override
-	public ResponseEntity<LccnSubscription> subscriptionsPost(@Valid final LccnSubscriptionRequest body) {
-		// TODO Auto-generated method stub
-		return null;
+	public ResponseEntity<LccnSubscription> subscriptionsPost(@Valid final LccnSubscriptionRequest body) throws URISyntaxException {
+		final LccnSubscription res = subscriptionService.create(body, LccnSubscription.class, VnfLcmSubscriptions331Sol003Controller::makeLinks, SubscriptionType.VNFPM);
+		final URI location = new URI(res.getLinks().getSelf().getHref());
+		return ResponseEntity.created(location).body(res);
 	}
 
 	@Override
 	public ResponseEntity<Void> subscriptionsSubscriptionIdDelete(final String subscriptionId) {
-		// TODO Auto-generated method stub
-		return null;
+		subscriptionService.deleteById(UUID.fromString(subscriptionId), SubscriptionType.VNFPM);
+		return ResponseEntity.noContent().build();
 	}
 
 	@Override
 	public ResponseEntity<LccnSubscription> subscriptionsSubscriptionIdGet(final String subscriptionId) {
-		// TODO Auto-generated method stub
-		return null;
+		final LccnSubscription res = subscriptionService.findById(UUID.fromString(subscriptionId), LccnSubscription.class, VnfLcmSubscriptions331Sol003Controller::makeLinks, SubscriptionType.ALARM);
+		return ResponseEntity.ok(res);
+	}
+
+	private static void makeLinks(final LccnSubscription subs) {
+		final LccnSubscriptionLinks links = new LccnSubscriptionLinks();
+		final Link self = new Link();
+		self.setHref(linkTo(methodOn(VnfLcmSubscriptions331Sol003Api.class).subscriptionsSubscriptionIdGet(subs.getId())).withSelfRel().getHref());
+		links.setSelf(self);
+
+		subs.setLinks(links);
 	}
 
 }
