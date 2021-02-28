2beb02908a9ca24821dabe0dc45f3ead8d414ade
Olivier Vignaud <ovi@ubiqube.com>
2021-02-28 18:35:18 +0100

    Add VNFLcmSubscription Front controller and deploy it to 3.3.1.

--- MANO-API/etsi-models/3.3.1/vnfm-controllers/src/main/java/com/ubiqube/etsi/mano/em/v331/controller/vnflcm/VnfLcmSubscriptions331Sol002Controller.java
+++ MANO-API/etsi-models/3.3.1/vnfm-controllers/src/main/java/com/ubiqube/etsi/mano/em/v331/controller/vnflcm/VnfLcmSubscriptions331Sol002Controller.java
@@ -1,13 +1,22 @@
 package com.ubiqube.etsi.mano.em.v331.controller.vnflcm;
 
+import static com.ubiqube.etsi.mano.Constants.getSafeUUID;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ubiqube.etsi.mano.controller.nslcm.VnfLcmSubscriptionFrontController;
 import com.ubiqube.etsi.mano.em.v331.model.vnflcm.LccnSubscription;
+import com.ubiqube.etsi.mano.em.v331.model.vnflcm.LccnSubscriptionLinks;
 import com.ubiqube.etsi.mano.em.v331.model.vnflcm.LccnSubscriptionRequest;
+import com.ubiqube.etsi.mano.em.v331.model.vnflcm.Link;
 
 /**
  *
@@ -16,29 +25,42 @@ import com.ubiqube.etsi.mano.em.v331.model.vnflcm.LccnSubscriptionRequest;
  */
 @RestController
 public class VnfLcmSubscriptions331Sol002Controller implements VnfLcmSubscriptions331Sol002Api {
+	private final VnfLcmSubscriptionFrontController frontController;
+
+	public VnfLcmSubscriptions331Sol002Controller(final VnfLcmSubscriptionFrontController frontController) {
+		super();
+		this.frontController = frontController;
+	}
 
 	@Override
-	public ResponseEntity<LccnSubscription> subscriptionsGet(final MultiValueMap<String, String> requestParams, @Valid final String nextpageOpaqueMarker) {
-		// TODO Auto-generated method stub
-		return null;
+	public ResponseEntity<List<LccnSubscription>> subscriptionsGet(final MultiValueMap<String, String> requestParams, @Valid final String nextpageOpaqueMarker) {
+		return frontController.search(requestParams, nextpageOpaqueMarker, LccnSubscription.class, VnfLcmSubscriptions331Sol002Controller::makeLinks);
 	}
 
 	@Override
 	public ResponseEntity<LccnSubscription> subscriptionsPost(@Valid final LccnSubscriptionRequest body) {
-		// TODO Auto-generated method stub
-		return null;
+		return frontController.create(body, LccnSubscription.class, VnfLcmSubscriptions331Sol002Controller::makeLinks, VnfLcmSubscriptions331Sol002Controller::getSelfLink);
 	}
 
 	@Override
 	public ResponseEntity<Void> subscriptionsSubscriptionIdDelete(final String subscriptionId) {
-		// TODO Auto-generated method stub
-		return null;
+		return frontController.deleteById(getSafeUUID(subscriptionId));
 	}
 
 	@Override
 	public ResponseEntity<LccnSubscription> subscriptionsSubscriptionIdGet(final String subscriptionId) {
-		// TODO Auto-generated method stub
-		return null;
+		return frontController.findById(getSafeUUID(subscriptionId), LccnSubscription.class, VnfLcmSubscriptions331Sol002Controller::makeLinks);
 	}
 
+	private static void makeLinks(final LccnSubscription subscription) {
+		final LccnSubscriptionLinks links = new LccnSubscriptionLinks();
+		final Link link = new Link();
+		link.setHref(linkTo(methodOn(VnfLcmSubscriptions331Sol002Api.class).subscriptionsSubscriptionIdGet(subscription.getId())).withSelfRel().getHref());
+		links.setSelf(link);
+		subscription.setLinks(links);
+	}
+
+	private static String getSelfLink(final LccnSubscription subscription) {
+		return linkTo(methodOn(VnfLcmSubscriptions331Sol002Api.class).subscriptionsSubscriptionIdGet(subscription.getId())).withSelfRel().getHref();
+	}
 }
--- MANO-API/etsi-models/3.3.1/vnfm-controllers/src/main/java/com/ubiqube/etsi/mano/vnfm/v331/controller/vnflcm/VnfLcmSubscriptions331Sol003Controller.java
+++ MANO-API/etsi-models/3.3.1/vnfm-controllers/src/main/java/com/ubiqube/etsi/mano/vnfm/v331/controller/vnflcm/VnfLcmSubscriptions331Sol003Controller.java
@@ -1,12 +1,10 @@
 package com.ubiqube.etsi.mano.vnfm.v331.controller.vnflcm;
 
+import static com.ubiqube.etsi.mano.Constants.getSafeUUID;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.List;
-import java.util.UUID;
 
 import javax.annotation.security.RolesAllowed;
 import javax.validation.Valid;
@@ -15,8 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ubiqube.etsi.mano.dao.mano.subs.SubscriptionType;
-import com.ubiqube.etsi.mano.service.SubscriptionServiceV2;
+import com.ubiqube.etsi.mano.controller.nslcm.VnfLcmSubscriptionFrontController;
 import com.ubiqube.etsi.mano.vnfm.v331.model.vnflcm.LccnSubscription;
 import com.ubiqube.etsi.mano.vnfm.v331.model.vnflcm.LccnSubscriptionLinks;
 import com.ubiqube.etsi.mano.vnfm.v331.model.vnflcm.LccnSubscriptionRequest;
@@ -30,36 +27,35 @@ import com.ubiqube.etsi.mano.vnfm.v331.model.vnflcm.Link;
 @RolesAllowed({ "ROLE_NFVO" })
 @RestController
 public class VnfLcmSubscriptions331Sol003Controller implements VnfLcmSubscriptions331Sol003Api {
-	private final SubscriptionServiceV2 subscriptionService;
+	private final VnfLcmSubscriptionFrontController frontController;
+
+	public VnfLcmSubscriptions331Sol003Controller(final VnfLcmSubscriptionFrontController frontController) {
+		super();
+		this.frontController = frontController;
+	}
 
 	@Override
 	public ResponseEntity<List<LccnSubscription>> subscriptionsGet(final MultiValueMap<String, String> requestParams, @Valid final String nextpageOpaqueMarker) {
-		final List<LccnSubscription> ret = subscriptionService.query(requestParams, LccnSubscription.class, VnfLcmSubscriptions331Sol003Controller::makeLinks, SubscriptionType.VNFPM);
-		return ResponseEntity.ok(ret);
-	}
-
-	public VnfLcmSubscriptions331Sol003Controller(final SubscriptionServiceV2 subscriptionService) {
-		super();
-		this.subscriptionService = subscriptionService;
+		return frontController.search(requestParams, nextpageOpaqueMarker, LccnSubscription.class, VnfLcmSubscriptions331Sol003Controller::makeLinks);
 	}
 
 	@Override
-	public ResponseEntity<LccnSubscription> subscriptionsPost(@Valid final LccnSubscriptionRequest body) throws URISyntaxException {
-		final LccnSubscription res = subscriptionService.create(body, LccnSubscription.class, VnfLcmSubscriptions331Sol003Controller::makeLinks, SubscriptionType.VNFPM);
-		final URI location = new URI(res.getLinks().getSelf().getHref());
-		return ResponseEntity.created(location).body(res);
+	public ResponseEntity<LccnSubscription> subscriptionsPost(@Valid final LccnSubscriptionRequest body) {
+		return frontController.create(body, LccnSubscription.class, VnfLcmSubscriptions331Sol003Controller::makeLinks, VnfLcmSubscriptions331Sol003Controller::getSelfLink);
 	}
 
 	@Override
 	public ResponseEntity<Void> subscriptionsSubscriptionIdDelete(final String subscriptionId) {
-		subscriptionService.deleteById(UUID.fromString(subscriptionId), SubscriptionType.VNFPM);
-		return ResponseEntity.noContent().build();
+		return frontController.deleteById(getSafeUUID(subscriptionId));
 	}
 
 	@Override
 	public ResponseEntity<LccnSubscription> subscriptionsSubscriptionIdGet(final String subscriptionId) {
-		final LccnSubscription res = subscriptionService.findById(UUID.fromString(subscriptionId), LccnSubscription.class, VnfLcmSubscriptions331Sol003Controller::makeLinks, SubscriptionType.ALARM);
-		return ResponseEntity.ok(res);
+		return frontController.findById(getSafeUUID(subscriptionId), LccnSubscription.class, VnfLcmSubscriptions331Sol003Controller::makeLinks);
+	}
+
+	private static String getSelfLink(final LccnSubscription subscription) {
+		return linkTo(methodOn(VnfLcmSubscriptions331Sol003Api.class).subscriptionsSubscriptionIdGet(subscription.getId())).withSelfRel().getHref();
 	}
 
 	private static void makeLinks(final LccnSubscription subs) {
