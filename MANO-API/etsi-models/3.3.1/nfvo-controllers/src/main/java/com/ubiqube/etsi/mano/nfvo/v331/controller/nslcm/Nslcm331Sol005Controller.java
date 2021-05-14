5802b0d69aef96b6cf3d1da82e16f64c6a64d59a
Olivier Vignaud <ovi@ubiqube.com>
2021-05-14 15:12:47 +0200

    Add NS lcm front controller verision 2.6.1 & 3.3.1.

--- MANO-API/etsi-models/3.3.1/nfvo-controllers/src/main/java/com/ubiqube/etsi/mano/nfvo/v331/controller/nslcm/Nslcm331Sol005Controller.java
+++ MANO-API/etsi-models/3.3.1/nfvo-controllers/src/main/java/com/ubiqube/etsi/mano/nfvo/v331/controller/nslcm/Nslcm331Sol005Controller.java
@@ -16,32 +16,65 @@
  */
 package com.ubiqube.etsi.mano.nfvo.v331.controller.nslcm;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
-import javax.servlet.http.HttpServletRequest;
-import java.util.Optional;
+
+import com.ubiqube.etsi.mano.controller.nslcm.NsLcmGenericFrontController;
+import com.ubiqube.etsi.mano.nfvo.v331.model.nslcm.CancelMode;
+import com.ubiqube.etsi.mano.nfvo.v331.model.nslcm.Link;
+import com.ubiqube.etsi.mano.nfvo.v331.model.nslcm.NsLcmOpOcc;
+import com.ubiqube.etsi.mano.nfvo.v331.model.nslcm.NsLcmOpOccLinks;
 
 @RestController
 public class Nslcm331Sol005Controller implements Nslcm331Sol005Api {
+	private final NsLcmGenericFrontController nsLcmGenericFrontController;
 
-    private final ObjectMapper objectMapper;
+	public Nslcm331Sol005Controller(final NsLcmGenericFrontController nsLcmGenericFrontController) {
+		super();
+		this.nsLcmGenericFrontController = nsLcmGenericFrontController;
+	}
 
-    private final HttpServletRequest request;
+	@Override
+	public ResponseEntity<Void> nslcmV1NsLcmOpOccsNsLcmOpOccIdCancelPost(final String nsLcmOpOccId, @Valid final CancelMode body) {
+		return nsLcmGenericFrontController.cancel(nsLcmOpOccId, body.getCancelMode().toString());
+	}
 
-    @org.springframework.beans.factory.annotation.Autowired
-    public Nslcm331Sol005Controller(ObjectMapper objectMapper, HttpServletRequest request) {
-        this.objectMapper = objectMapper;
-        this.request = request;
-    }
+	@Override
+	public ResponseEntity<NsLcmOpOcc> nslcmV1NsLcmOpOccsNsLcmOpOccIdFailPost(final String nsLcmOpOccId) {
+		return nsLcmGenericFrontController.fail(nsLcmOpOccId, NsLcmOpOcc.class, Nslcm331Sol005Controller::makeLinks);
+	}
 
-    @Override
-    public Optional<ObjectMapper> getObjectMapper() {
-        return Optional.ofNullable(objectMapper);
-    }
+	public static void makeLinks(@NotNull final NsLcmOpOcc nsLcmOpOccs) {
+		@NotNull
+		final String id = nsLcmOpOccs.getId().toString();
+		final NsLcmOpOccLinks nsLcmOpOccLinks = new NsLcmOpOccLinks();
 
-    @Override
-    public Optional<HttpServletRequest> getRequest() {
-        return Optional.ofNullable(request);
-    }
+		final Link _continue = new Link();
+		_continue.setHref(linkTo(methodOn(NsLcmOpOccs331Sol005Api.class).nsLcmOpOccsNsLcmOpOccIdContinuePost(id)).withSelfRel().getHref());
+		nsLcmOpOccLinks.setContinue(_continue);
+
+		final Link nsInstance = new Link();
+		nsInstance.setHref(linkTo(methodOn(NsInstances331Sol005Api.class).nsInstancesNsInstanceIdGet(nsLcmOpOccs.getNsInstanceId())).withSelfRel().getHref());
+		nsLcmOpOccLinks.setNsInstance(nsInstance);
+
+		final Link retry = new Link();
+		retry.setHref(linkTo(methodOn(NsLcmOpOccs331Sol005Api.class).nsLcmOpOccsNsLcmOpOccIdRetryPost(id)).withSelfRel().getHref());
+		nsLcmOpOccLinks.setRetry(retry);
+
+		final Link rollback = new Link();
+		rollback.setHref(linkTo(methodOn(NsLcmOpOccs331Sol005Api.class).nsLcmOpOccsNsLcmOpOccIdRollbackPost(id)).withSelfRel().getHref());
+		nsLcmOpOccLinks.setRollback(rollback);
+
+		final Link self = new Link();
+		self.setHref(linkTo(methodOn(NsLcmOpOccs331Sol005Api.class).nsLcmOpOccsNsLcmOpOccIdGet(id)).withSelfRel().getHref());
+		nsLcmOpOccLinks.setSelf(self);
+		nsLcmOpOccs.setLinks(nsLcmOpOccLinks);
+	}
 
 }
