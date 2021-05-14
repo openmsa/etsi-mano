a36929bbdd2240a15c74d8dfe8a008a697deac92
Olivier Vignaud <ovi@ubiqube.com>
2021-05-14 20:56:36 +0200

    Add 2.7.1 vnflcm Sol003.

---
+++ MANO-API/etsi-models/2.7.1/model/src/main/java/com/ubiqube/etsi/mano/vnfm/v271/model/vnflcm/VnfExtCpConfig.java
@@ -0,0 +1,160 @@
+/**
+ *     Copyright (C) 2019-2020 Ubiqube.
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.ubiqube.etsi.mano.vnfm.v271.model.vnflcm;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.ubiqube.etsi.mano.vnfm.v271.model.vnflcm.CpProtocolData;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * This type represents an externally provided link port or network address information per instance of an external connection point. In case a link port is provided, the VNFM shall use that link port when connecting the external CP to the external VL. In a link port is not provided, the VNFM shall create a link port on the external VL, and use that link port to connect the external CP to the external VL. 
+ */
+@ApiModel(description = "This type represents an externally provided link port or network address information per instance of an external connection point. In case a link port is provided, the VNFM shall use that link port when connecting the external CP to the external VL. In a link port is not provided, the VNFM shall create a link port on the external VL, and use that link port to connect the external CP to the external VL. ")
+@Validated
+
+public class VnfExtCpConfig   {
+  @JsonProperty("cpInstanceId")
+  private String cpInstanceId = null;
+
+  @JsonProperty("linkPortId")
+  private String linkPortId = null;
+
+  @JsonProperty("cpProtocolData")
+  @Valid
+  private List<CpProtocolData> cpProtocolData = null;
+
+  public VnfExtCpConfig cpInstanceId(String cpInstanceId) {
+    this.cpInstanceId = cpInstanceId;
+    return this;
+  }
+
+  /**
+   * Identifier of the external CP instance to which this set of configuration parameters is requested to be applied. Shall be present if this instance has already been created. 
+   * @return cpInstanceId
+  **/
+  @ApiModelProperty(value = "Identifier of the external CP instance to which this set of configuration parameters is requested to be applied. Shall be present if this instance has already been created. ")
+
+
+  public String getCpInstanceId() {
+    return cpInstanceId;
+  }
+
+  public void setCpInstanceId(String cpInstanceId) {
+    this.cpInstanceId = cpInstanceId;
+  }
+
+  public VnfExtCpConfig linkPortId(String linkPortId) {
+    this.linkPortId = linkPortId;
+    return this;
+  }
+
+  /**
+   * Identifier of a pre-configured link port to which the external CP will be associated. The following conditions apply to the attributes \"linkPortId\" and \"cpProtocolData\": 1) The \"linkPortId\" and \"cpProtocolData\" attributes shall both be    absent for the deletion of an existing external CP instance    addressed by cpInstanceId. 2) At least one of these attributes shall be present for a    to-be-created external CP instance or an existing external    CP instance. 3) If the \"linkPortId\" attribute is absent, the VNFM shall create a    link port. 4) If the \"cpProtocolData\" attribute is absent, the \"linkPortId\"    attribute shall be provided referencing a pre-created link port,    and the VNFM can use means outside the scope of the present    document to obtain the pre-configured address information for the    connection point from the resource representing the link port. 5) If both \"cpProtocolData\" and \"linkportId\" are provided, the API    consumer shall ensure that the cpProtocolData can be used with the    pre-created link port referenced by \"linkPortId\". 
+   * @return linkPortId
+  **/
+  @ApiModelProperty(value = "Identifier of a pre-configured link port to which the external CP will be associated. The following conditions apply to the attributes \"linkPortId\" and \"cpProtocolData\": 1) The \"linkPortId\" and \"cpProtocolData\" attributes shall both be    absent for the deletion of an existing external CP instance    addressed by cpInstanceId. 2) At least one of these attributes shall be present for a    to-be-created external CP instance or an existing external    CP instance. 3) If the \"linkPortId\" attribute is absent, the VNFM shall create a    link port. 4) If the \"cpProtocolData\" attribute is absent, the \"linkPortId\"    attribute shall be provided referencing a pre-created link port,    and the VNFM can use means outside the scope of the present    document to obtain the pre-configured address information for the    connection point from the resource representing the link port. 5) If both \"cpProtocolData\" and \"linkportId\" are provided, the API    consumer shall ensure that the cpProtocolData can be used with the    pre-created link port referenced by \"linkPortId\". ")
+
+
+  public String getLinkPortId() {
+    return linkPortId;
+  }
+
+  public void setLinkPortId(String linkPortId) {
+    this.linkPortId = linkPortId;
+  }
+
+  public VnfExtCpConfig cpProtocolData(List<CpProtocolData> cpProtocolData) {
+    this.cpProtocolData = cpProtocolData;
+    return this;
+  }
+
+  public VnfExtCpConfig addCpProtocolDataItem(CpProtocolData cpProtocolDataItem) {
+    if (this.cpProtocolData == null) {
+      this.cpProtocolData = new ArrayList<>();
+    }
+    this.cpProtocolData.add(cpProtocolDataItem);
+    return this;
+  }
+
+  /**
+   * Parameters for configuring the network protocols on the link port that connects the CP to a VL. The following conditions apply to the attributes \"linkPortId\" and \"cpProtocolData\": 1) The \"linkPortId\" and \"cpProtocolData\" attributes shall both be    absent for the deletion of an existing external CP instance    addressed by cpInstanceId. 2) At least one of these attributes shall be present for a    to-be-created external CP instance or an existing external    CP instance. 3) If the \"linkPortId\" attribute is absent, the VNFM shall create a    link port. 4) If the \"cpProtocolData\" attribute is absent, the \"linkPortId\"    attribute shall be provided referencing a pre-created link port,    and the VNFM can use means outside the scope of the present    document to obtain the pre-configured address information for the    connection point from the resource representing the link port. 5) If both \"cpProtocolData\" and \"linkportId\" are provided, the API    consumer shall ensure that the cpProtocolData can be used with the    pre-created link port referenced by \"linkPortId\". 
+   * @return cpProtocolData
+  **/
+  @ApiModelProperty(value = "Parameters for configuring the network protocols on the link port that connects the CP to a VL. The following conditions apply to the attributes \"linkPortId\" and \"cpProtocolData\": 1) The \"linkPortId\" and \"cpProtocolData\" attributes shall both be    absent for the deletion of an existing external CP instance    addressed by cpInstanceId. 2) At least one of these attributes shall be present for a    to-be-created external CP instance or an existing external    CP instance. 3) If the \"linkPortId\" attribute is absent, the VNFM shall create a    link port. 4) If the \"cpProtocolData\" attribute is absent, the \"linkPortId\"    attribute shall be provided referencing a pre-created link port,    and the VNFM can use means outside the scope of the present    document to obtain the pre-configured address information for the    connection point from the resource representing the link port. 5) If both \"cpProtocolData\" and \"linkportId\" are provided, the API    consumer shall ensure that the cpProtocolData can be used with the    pre-created link port referenced by \"linkPortId\". ")
+
+  @Valid
+
+  public List<CpProtocolData> getCpProtocolData() {
+    return cpProtocolData;
+  }
+
+  public void setCpProtocolData(List<CpProtocolData> cpProtocolData) {
+    this.cpProtocolData = cpProtocolData;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    VnfExtCpConfig vnfExtCpConfig = (VnfExtCpConfig) o;
+    return Objects.equals(this.cpInstanceId, vnfExtCpConfig.cpInstanceId) &&
+        Objects.equals(this.linkPortId, vnfExtCpConfig.linkPortId) &&
+        Objects.equals(this.cpProtocolData, vnfExtCpConfig.cpProtocolData);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(cpInstanceId, linkPortId, cpProtocolData);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class VnfExtCpConfig {\n");
+    
+    sb.append("    cpInstanceId: ").append(toIndentedString(cpInstanceId)).append("\n");
+    sb.append("    linkPortId: ").append(toIndentedString(linkPortId)).append("\n");
+    sb.append("    cpProtocolData: ").append(toIndentedString(cpProtocolData)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
---
+++ etsi-models/2.7.1/vnfm-controllers/src/main/java/com/ubiqube/etsi/mano/vnfm/v271/controller/vnflcm/VnfLcmOpOccs271Sol0à3Controller.java
@@ -0,0 +1,141 @@
+/**
+ *     Copyright (C) 2019-2020 Ubiqube.
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.ubiqube.etsi.mano.vnfm.v271.controller.vnflcm;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import java.util.UUID;
+
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ubiqube.etsi.mano.controller.nslcm.VnfLcmOpOccGenericFrontController;
+import com.ubiqube.etsi.mano.dao.mano.ResourceTypeEnum;
+import com.ubiqube.etsi.mano.dao.mano.v2.VnfBlueprint;
+import com.ubiqube.etsi.mano.vnfm.v271.model.vnflcm.AffectedVirtualLink;
+import com.ubiqube.etsi.mano.vnfm.v271.model.vnflcm.AffectedVirtualStorage;
+import com.ubiqube.etsi.mano.vnfm.v271.model.vnflcm.AffectedVnfc;
+import com.ubiqube.etsi.mano.vnfm.v271.model.vnflcm.Link;
+import com.ubiqube.etsi.mano.vnfm.v271.model.vnflcm.VnfLcmOpOcc;
+import com.ubiqube.etsi.mano.vnfm.v271.model.vnflcm.VnfLcmOpOccLinks;
+import com.ubiqube.etsi.mano.vnfm.v271.model.vnflcm.VnfLcmOpOccResourceChanges;
+
+import ma.glasnost.orika.MapperFacade;
+
+/**
+ *
+ * @author Olivier Vignaud <ovi@ubiqube.com>
+ *
+ */
+@RestController
+public class VnfLcmOpOccs271Sol0à3Controller implements VnfLcmOpOccs271Sol003Api {
+	private final VnfLcmOpOccGenericFrontController frontController;
+
+	private final MapperFacade mapper;
+
+	public VnfLcmOpOccs271Sol0à3Controller(final VnfLcmOpOccGenericFrontController frontController, final MapperFacade _mapper) {
+		super();
+		this.frontController = frontController;
+		mapper = _mapper;
+	}
+
+	@Override
+	public ResponseEntity<String> vnfLcmOpOccsGet(final MultiValueMap<String, String> requestParams, @Valid final String nextpageOpaqueMarker) {
+		return frontController.search(requestParams, VnfLcmOpOcc.class, VnfLcmOpOccs271Sol0à3Controller::makeLinks);
+	}
+
+	@Override
+	public ResponseEntity<Void> vnfLcmOpOccsVnfLcmOpOccIdCancelPost(final String vnfLcmOpOccId) {
+		return frontController.lcmOpOccCancel(UUID.fromString(vnfLcmOpOccId));
+	}
+
+	@Override
+	public ResponseEntity<VnfLcmOpOcc> vnfLcmOpOccsVnfLcmOpOccIdFailPost(final String vnfLcmOpOccId) {
+		return frontController.lcmOpOccFail(UUID.fromString(vnfLcmOpOccId));
+	}
+
+	@Override
+	public ResponseEntity<VnfLcmOpOcc> vnfLcmOpOccsVnfLcmOpOccIdGet(final String vnfLcmOpOccId) {
+		final VnfBlueprint resultDb = frontController.lcmOpOccFindById(UUID.fromString(vnfLcmOpOccId));
+		final VnfLcmOpOcc entity = mapper.map(resultDb, VnfLcmOpOcc.class);
+		final VnfLcmOpOccResourceChanges resourceChanged = new VnfLcmOpOccResourceChanges();
+		resultDb.getTasks().stream()
+				.filter(x -> x.getType() == ResourceTypeEnum.VL)
+				.map(x -> mapper.map(x, AffectedVirtualLink.class))
+				.forEach(resourceChanged::addAffectedVirtualLinksItem);
+		resultDb.getTasks().stream()
+				.filter(x -> x.getType() == ResourceTypeEnum.STORAGE)
+				.map(x -> mapper.map(x, AffectedVirtualStorage.class))
+				.forEach(resourceChanged::addAffectedVirtualStoragesItem);
+		resultDb.getTasks().stream()
+				.filter(x -> x.getType() == ResourceTypeEnum.COMPUTE)
+				.map(x -> mapper.map(x, AffectedVnfc.class))
+				.forEach(resourceChanged::addAffectedVnfcsItem);
+		entity.setResourceChanges(resourceChanged);
+		makeLinks(entity);
+		return new ResponseEntity<>(entity, HttpStatus.OK);
+	}
+
+	@Override
+	public ResponseEntity<Void> vnfLcmOpOccsVnfLcmOpOccIdRetryPost(final String vnfLcmOpOccId) {
+		return frontController.lcmOpOccRetry(UUID.fromString(vnfLcmOpOccId));
+	}
+
+	@Override
+	public ResponseEntity<Void> vnfLcmOpOccsVnfLcmOpOccIdRollbackPost(final String vnfLcmOpOccId) {
+		return frontController.lcmOpOccRollback(UUID.fromString(vnfLcmOpOccId));
+	}
+
+	private static void makeLinks(final VnfLcmOpOcc vnfLcmOpOcc) {
+		final String id = vnfLcmOpOcc.getId();
+		final VnfLcmOpOccLinks links = new VnfLcmOpOccLinks();
+		final Link cancel = new Link();
+		cancel.setHref(linkTo(methodOn(VnfLcmOpOccs271Sol003Api.class).vnfLcmOpOccsVnfLcmOpOccIdCancelPost(id)).withSelfRel().getHref());
+		links.setCancel(cancel);
+
+		final Link fail = new Link();
+		fail.setHref(linkTo(methodOn(VnfLcmOpOccs271Sol003Api.class).vnfLcmOpOccsVnfLcmOpOccIdFailPost(id)).withSelfRel().getHref());
+		links.setFail(fail);
+
+		// XXX We can't have this grant link directly, because of classpath on interface.
+		// grant.setHref(linkTo(methodOn(LcmGrants.class).grantsGrantIdGet(vnfLcmOpOcc.getGrantId(),"")).withSelfRel().getHref());
+
+		final Link retry = new Link();
+		retry.setHref(linkTo(methodOn(VnfLcmOpOccs271Sol003Api.class).vnfLcmOpOccsVnfLcmOpOccIdRetryPost(id)).withSelfRel().getHref());
+		links.setRetry(retry);
+
+		final Link rollback = new Link();
+		rollback.setHref(linkTo(methodOn(VnfLcmOpOccs271Sol003Api.class).vnfLcmOpOccsVnfLcmOpOccIdRollbackPost(id)).withSelfRel().getHref());
+		links.setRollback(rollback);
+
+		final Link self = new Link();
+		self.setHref(linkTo(methodOn(VnfLcmOpOccs271Sol003Api.class).vnfLcmOpOccsVnfLcmOpOccIdGet(id)).withSelfRel().getHref());
+		links.setSelf(self);
+
+		final Link vnfInstance = new Link();
+		vnfInstance.setHref(linkTo(methodOn(VnfInstances271Sol003Api.class).vnfInstancesVnfInstanceIdGet(vnfLcmOpOcc.getId())).withSelfRel().getHref());
+		links.setVnfInstance(vnfInstance);
+
+		vnfLcmOpOcc.setLinks(links);
+	}
+
+}
