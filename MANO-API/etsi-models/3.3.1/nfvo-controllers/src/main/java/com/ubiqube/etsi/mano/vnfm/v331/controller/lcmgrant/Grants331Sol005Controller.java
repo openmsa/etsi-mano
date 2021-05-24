f0cfb2d7637ba871eecf22b7d6925482cc0a1a67
Olivier Vignaud <ovi@ubiqube.com>
2021-05-24 16:41:59 +0200

    Add 3.3.1 lcmgrant front controller.

--- MANO-API/etsi-models/3.3.1/nfvo-controllers/src/main/java/com/ubiqube/etsi/mano/vnfm/v331/controller/lcmgrant/Grants331Sol005Controller.java
+++ MANO-API/etsi-models/3.3.1/nfvo-controllers/src/main/java/com/ubiqube/etsi/mano/vnfm/v331/controller/lcmgrant/Grants331Sol005Controller.java
@@ -16,13 +16,18 @@
  */
 package com.ubiqube.etsi.mano.vnfm.v331.controller.lcmgrant;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ubiqube.etsi.mano.controller.lcmgrant.LcmGrantsFrontController;
 import com.ubiqube.etsi.mano.vnfm.v331.model.grant.Grant;
 import com.ubiqube.etsi.mano.vnfm.v331.model.grant.GrantRequest;
+import com.ubiqube.etsi.mano.vnfm.v331.model.grant.Link;
 
 /**
  *
@@ -31,16 +36,30 @@ import com.ubiqube.etsi.mano.vnfm.v331.model.grant.GrantRequest;
  */
 @RestController
 public class Grants331Sol005Controller implements Grants331Sol005Api {
+	private final LcmGrantsFrontController lcmGrantsFrontController;
 
-	@Override
-	public ResponseEntity<Grant> grantsGrantIdGet(final String grantId) {
-		// TODO Auto-generated method stub
-		return null;
+	public Grants331Sol005Controller(final LcmGrantsFrontController lcmGrantsFrontController) {
+		super();
+		this.lcmGrantsFrontController = lcmGrantsFrontController;
 	}
 
 	@Override
-	public ResponseEntity<Grant> grantsPost(@Valid final GrantRequest body) {
-		// TODO Auto-generated method stub
-		return null;
+	public ResponseEntity<Grant> grantsGrantIdGet(final String grantId) {
+		return lcmGrantsFrontController.grantsGrantIdGet(grantId, Grant.class, Grants331Sol005Controller::makeSelfLinks);
+	}
+
+	@Override
+	public ResponseEntity<Grant> grantsPost(@Valid final GrantRequest grantRequest) {
+		return lcmGrantsFrontController.grantsPost(grantRequest, Grant.class, Grants331Sol005Controller::getSelfLink);
+	}
+
+	private static void makeSelfLinks(final Grant jsonGrant) {
+		final Link link = new Link();
+		link.setHref(linkTo(methodOn(Grants331Sol005Api.class).grantsGrantIdGet(jsonGrant.getId())).withSelfRel().getHref());
+		jsonGrant.getLinks().setSelf(link);
+	}
+
+	private static String getSelfLink(final Grant grant) {
+		return linkTo(methodOn(Grants331Sol005Api.class).grantsGrantIdGet(grant.getId())).withSelfRel().getHref();
 	}
 }
