0399a31895f593589b918f554466a86e4522dad3
Olivier Vignaud <ovi@ubiqube.com>
2021-05-24 11:11:39 +0200

    Fix userDefinedData in pnf.

--- MANO-API/etsi-models/2.8.1/model/src/main/java/com/ubiqube/etsi/mano/nfvo/v281/model/nsd/CreatePnfdInfoRequest.java
+++ MANO-API/etsi-models/2.8.1/model/src/main/java/com/ubiqube/etsi/mano/nfvo/v281/model/nsd/CreatePnfdInfoRequest.java
@@ -16,84 +16,84 @@
  */
 package com.ubiqube.etsi.mano.nfvo.v281.model.nsd;
 
+import java.util.Map;
 import java.util.Objects;
+
+import javax.validation.Valid;
+
+import org.springframework.validation.annotation.Validated;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.ubiqube.etsi.mano.nfvo.v281.model.nsd.KeyValuePairs;
+
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import org.springframework.validation.annotation.Validated;
-import javax.validation.Valid;
-import javax.validation.constraints.*;
 
 /**
- * User-defined data for the PNF descriptor resource to be created. It shall be present when the user defined data is set for the individual PNF descriptor resource to be created. 
+ * User-defined data for the PNF descriptor resource to be created. It shall be present when the user defined data is set for the individual PNF descriptor resource to be created.
  */
 @ApiModel(description = "User-defined data for the PNF descriptor resource to be created. It shall be present when the user defined data is set for the individual PNF descriptor resource to be created. ")
 @Validated
 
-public class CreatePnfdInfoRequest   {
-  @JsonProperty("userDefinedData")
-  private KeyValuePairs userDefinedData = null;
+public class CreatePnfdInfoRequest {
+	@JsonProperty("userDefinedData")
+	private Map<String, Object> userDefinedData = null;
 
-  public CreatePnfdInfoRequest userDefinedData(KeyValuePairs userDefinedData) {
-    this.userDefinedData = userDefinedData;
-    return this;
-  }
+	public CreatePnfdInfoRequest userDefinedData(final Map<String, Object> userDefinedData) {
+		this.userDefinedData = userDefinedData;
+		return this;
+	}
 
-  /**
-   * Get userDefinedData
-   * @return userDefinedData
-  **/
-  @ApiModelProperty(value = "")
+	/**
+	 * Get userDefinedData
+	 *
+	 * @return userDefinedData
+	 **/
+	@ApiModelProperty(value = "")
 
-  @Valid
+	@Valid
 
-  public KeyValuePairs getUserDefinedData() {
-    return userDefinedData;
-  }
+	public Map<String, Object> getUserDefinedData() {
+		return userDefinedData;
+	}
 
-  public void setUserDefinedData(KeyValuePairs userDefinedData) {
-    this.userDefinedData = userDefinedData;
-  }
+	public void setUserDefinedData(final Map<String, Object> userDefinedData) {
+		this.userDefinedData = userDefinedData;
+	}
 
+	@Override
+	public boolean equals(final java.lang.Object o) {
+		if (this == o) {
+			return true;
+		}
+		if ((o == null) || (getClass() != o.getClass())) {
+			return false;
+		}
+		final CreatePnfdInfoRequest createPnfdInfoRequest = (CreatePnfdInfoRequest) o;
+		return Objects.equals(this.userDefinedData, createPnfdInfoRequest.userDefinedData);
+	}
 
-  @Override
-  public boolean equals(java.lang.Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    CreatePnfdInfoRequest createPnfdInfoRequest = (CreatePnfdInfoRequest) o;
-    return Objects.equals(this.userDefinedData, createPnfdInfoRequest.userDefinedData);
-  }
+	@Override
+	public int hashCode() {
+		return Objects.hash(userDefinedData);
+	}
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(userDefinedData);
-  }
+	@Override
+	public String toString() {
+		final StringBuilder sb = new StringBuilder();
+		sb.append("class CreatePnfdInfoRequest {\n");
 
-  @Override
-  public String toString() {
-    StringBuilder sb = new StringBuilder();
-    sb.append("class CreatePnfdInfoRequest {\n");
-    
-    sb.append("    userDefinedData: ").append(toIndentedString(userDefinedData)).append("\n");
-    sb.append("}");
-    return sb.toString();
-  }
+		sb.append("    userDefinedData: ").append(toIndentedString(userDefinedData)).append("\n");
+		sb.append("}");
+		return sb.toString();
+	}
 
-  /**
-   * Convert the given object to string with each line indented by 4 spaces
-   * (except the first line).
-   */
-  private String toIndentedString(java.lang.Object o) {
-    if (o == null) {
-      return "null";
-    }
-    return o.toString().replace("\n", "\n    ");
-  }
+	/**
+	 * Convert the given object to string with each line indented by 4 spaces (except the first line).
+	 */
+	private String toIndentedString(final java.lang.Object o) {
+		if (o == null) {
+			return "null";
+		}
+		return o.toString().replace("\n", "\n    ");
+	}
 }
-
