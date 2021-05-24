963f4284e7f779404002cd42da148cf6981736ad
Olivier Vignaud <ovi@ubiqube.com>
2021-05-24 16:42:14 +0200

    Add 2.8.1 lcmgrant front controller.

--- MANO-API/etsi-models/2.8.1/nfvo-controllers/src/main/java/com/ubiqube/etsi/mano/vnfm/v281/controller/grant/Grants281Sol003Controller.java
+++ MANO-API/etsi-models/2.8.1/nfvo-controllers/src/main/java/com/ubiqube/etsi/mano/vnfm/v281/controller/grant/Grants281Sol003Controller.java
@@ -16,32 +16,50 @@
  */
 package com.ubiqube.etsi.mano.vnfm.v281.controller.grant;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.stereotype.Controller;
-import javax.servlet.http.HttpServletRequest;
-import java.util.Optional;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
-@Controller
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ubiqube.etsi.mano.controller.lcmgrant.LcmGrantsFrontController;
+import com.ubiqube.etsi.mano.vnfm.v281.model.grant.Grant;
+import com.ubiqube.etsi.mano.vnfm.v281.model.grant.GrantRequest;
+import com.ubiqube.etsi.mano.vnfm.v281.model.grant.Link;
+
+/**
+ *
+ * @author Olivier Vignaud <ovi@ubiqube.com>
+ *
+ */
+@RestController
 public class Grants281Sol003Controller implements Grants281Sol003Api {
+	private final LcmGrantsFrontController lcmGrantsFrontController;
 
-    private final ObjectMapper objectMapper;
+	public Grants281Sol003Controller(final LcmGrantsFrontController lcmGrantsFrontController) {
+		super();
+		this.lcmGrantsFrontController = lcmGrantsFrontController;
+	}
 
-    private final HttpServletRequest request;
+	@Override
+	public ResponseEntity<Grant> grantsGrantIdGet(final String grantId) {
+		return lcmGrantsFrontController.grantsGrantIdGet(grantId, Grant.class, Grants281Sol003Controller::makeSelfLinks);
+	}
 
-    @org.springframework.beans.factory.annotation.Autowired
-    public Grants281Sol003Controller(ObjectMapper objectMapper, HttpServletRequest request) {
-        this.objectMapper = objectMapper;
-        this.request = request;
-    }
+	@Override
+	public ResponseEntity<Grant> grantsPost(@Valid final GrantRequest grantRequest) {
+		return lcmGrantsFrontController.grantsPost(grantRequest, Grant.class, Grants281Sol003Controller::getSelfLink);
+	}
 
-    @Override
-    public Optional<ObjectMapper> getObjectMapper() {
-        return Optional.ofNullable(objectMapper);
-    }
+	private static void makeSelfLinks(final Grant jsonGrant) {
+		final Link link = new Link();
+		link.setHref(linkTo(methodOn(Grants281Sol003Api.class).grantsGrantIdGet(jsonGrant.getId())).withSelfRel().getHref());
+		jsonGrant.getLinks().setSelf(link);
+	}
 
-    @Override
-    public Optional<HttpServletRequest> getRequest() {
-        return Optional.ofNullable(request);
-    }
-
+	private static String getSelfLink(final Grant grant) {
+		return linkTo(methodOn(Grants281Sol003Api.class).grantsGrantIdGet(grant.getId())).withSelfRel().getHref();
+	}
 }
