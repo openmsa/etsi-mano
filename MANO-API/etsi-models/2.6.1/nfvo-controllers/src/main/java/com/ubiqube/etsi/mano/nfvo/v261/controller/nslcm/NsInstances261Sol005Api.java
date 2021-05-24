201a727dcee7bdbde34b820dd8c67a5a3fb62e8b
Olivier Vignaud <ovi@ubiqube.com>
2021-05-24 11:10:03 +0200

    Fix documentation in 2.6.1 nsInstance.

--- MANO-API/etsi-models/2.6.1/nfvo-controllers/src/main/java/com/ubiqube/etsi/mano/nfvo/v261/controller/nslcm/NsInstances261Sol005Api.java
+++ MANO-API/etsi-models/2.6.1/nfvo-controllers/src/main/java/com/ubiqube/etsi/mano/nfvo/v261/controller/nslcm/NsInstances261Sol005Api.java
@@ -40,6 +40,7 @@ import com.ubiqube.etsi.mano.nfvo.v261.model.nslcm.UpdateNsRequest;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
@@ -59,7 +60,7 @@ public interface NsInstances261Sol005Api {
 			@ApiResponse(code = 406, message = "Not Acceptable If the Accept HTTP header does not contain at least one name of a content type that is acceptable to the API producer, the API producer shall respond with this response code. The ProblemDetails structure may be omitted in that case.         ", response = ProblemDetails.class), @ApiResponse(code = 409, message = "Conflict Another request is in progress that prohibits the fulfilment of the current request, or the current resource state is inconsistent with the request. ", response = ProblemDetails.class), @ApiResponse(code = 416, message = "Requested Range Not Satisfiable This code is returned if the requested byte range in the Range HTTP header is not present in the requested resource. ", response = ProblemDetails.class),
 			@ApiResponse(code = 500, message = "Internal Server Error If there is an application error not related to the client's input that cannot be easily mapped to any other HTTP response code (\"catch all error\"), the API producer shall respond withthis response code. The ProblemDetails structure shall be provided, and shall include in the \"detail\" attribute more information about the source of the problem. ", response = ProblemDetails.class), @ApiResponse(code = 503, message = "Service Unavailable If the API producer encounters an internal overload situation of itself or of a system it relies on, it should respond with this response code, following the provisions in IETF RFC 7231 [13] for the use of the Retry-After HTTP header and for the alternative to refuse the connection. The \"ProblemDetails\" structure may be omitted. ", response = ProblemDetails.class) })
 	@GetMapping(consumes = { "application/json" }, produces = { "application/json" })
-	ResponseEntity<String> nsInstancesGet(@Nonnull @RequestParam MultiValueMap<String, String> requestParams);
+	ResponseEntity<String> nsInstancesGet(@ApiParam(value = "All query parameters. ", required = true) @Nonnull @RequestParam MultiValueMap<String, String> requestParams);
 
 	/**
 	 * Delete NS instance resource.
