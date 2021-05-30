dfaf9bc9cf1650b83de689b41007a3bc13257c94
Olivier Vignaud <ovi@ubiqube.com>
2021-05-30 11:39:06 +0200

    Add authentication modules.

---
+++ MANO-API/mano-auth/auth-api/src/main/java/com/ubiqube/etsi/mano/config/WebSecurityConfig.java
@@ -0,0 +1,54 @@
+/**
+ *     Copyright (C) 2019-2020 Ubiqube.
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.ubiqube.etsi.mano.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	private final SecutiryConfig secutiryConfig;
+
+	public WebSecurityConfig(SecutiryConfig secutiryConfig) {
+		super();
+		this.secutiryConfig = secutiryConfig;
+	}
+
+	@Override
+	protected void configure(final AuthenticationManagerBuilder _auth) throws Exception {
+		secutiryConfig.configure(_auth);
+	}
+
+	/**
+	 * All request must be authenticated, No login page.
+	 */
+	@Override
+	protected void configure(final HttpSecurity http) throws Exception {
+		ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry res = http.authorizeRequests()
+				.antMatchers("/openApi*").permitAll()
+				.antMatchers("/download/**").permitAll()
+				.antMatchers("/actuator/**").permitAll()
+				.anyRequest().authenticated();
+		secutiryConfig.configure(res);
+	}
+}
