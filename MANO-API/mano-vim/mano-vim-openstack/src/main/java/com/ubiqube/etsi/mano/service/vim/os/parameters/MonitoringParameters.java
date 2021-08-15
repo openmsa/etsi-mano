4be029db3f2200f4e3fd349a0efd75acb19a36a6
Olivier Vignaud <ovi@ubiqube.com>
2021-08-15 13:00:13 +0200

    Move Vims.

---
+++ MANO-API/mano-vim/mano-vim-openstack/src/main/java/com/ubiqube/etsi/mano/service/vim/os/parameters/MonitoringParameters.java
@@ -0,0 +1,45 @@
+/**
+ *     Copyright (C) 2019-2020 Ubiqube.
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.ubiqube.etsi.mano.service.vim.os.parameters;
+
+import com.ubiqube.etsi.mano.dao.mano.MonitoringParams;
+import com.ubiqube.etsi.mano.dao.mano.VnfCompute;
+import com.ubiqube.etsi.mano.dao.mano.v2.VnfBlueprint;
+
+public class MonitoringParameters {
+
+	public VnfCompute getVnfCompute() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public VnfBlueprint getBlueprint() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public MonitoringParams getMonitoringParameters() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
