b27734bf4322c9cc88c34825962e2612e9bf281d
Olivier Vignaud <ovi@ubiqube.com>
2021-08-16 10:19:40 +0200

    Partial fic for nfvo webapp.

--- MANO-API/nfvo-webapp/src/main/java/com/ubiqube/etsi/mano/Application.java
+++ MANO-API/nfvo-webapp/src/main/java/com/ubiqube/etsi/mano/Application.java
@@ -22,6 +22,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.jms.annotation.EnableJms;
 
+import ma.glasnost.orika.OrikaSystemProperties;
+import ma.glasnost.orika.impl.generator.EclipseJdtCompilerStrategy;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @SpringBootApplication
@@ -30,6 +32,9 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 public class Application extends SpringBootServletInitializer {
 
 	public static void main(final String[] args) {
+		System.setProperty(OrikaSystemProperties.COMPILER_STRATEGY, EclipseJdtCompilerStrategy.class.getName());
+		System.setProperty(OrikaSystemProperties.WRITE_SOURCE_FILES, "true");
+		System.setProperty(OrikaSystemProperties.WRITE_SOURCE_FILES_TO_PATH, "/tmp/okika");
 		SpringApplication.run(Application.class, args);
 	}
 
