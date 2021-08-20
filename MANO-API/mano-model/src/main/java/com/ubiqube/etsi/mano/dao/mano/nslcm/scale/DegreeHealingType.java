19fd0fe15dc18dd4e8bd61d6bcae2b8840699d40
Olivier Vignaud <ovi@ubiqube.com>
2021-08-20 15:13:45 +0200

    Add NsScale request(.

---
+++ MANO-API/mano-model/src/main/java/com/ubiqube/etsi/mano/dao/mano/nslcm/scale/DegreeHealingType.java
@@ -0,0 +1,52 @@
+/**
+ *     Copyright (C) 2019-2020 Ubiqube.
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.ubiqube.etsi.mano.dao.mano.nslcm.scale;
+
+/**
+ *
+ * @author Olivier Vignaud <ovi@ubiqube.com>
+ *
+ */
+public enum DegreeHealingType {
+	HEAL_RESTORE("HEAL_RESTORE"),
+
+	HEAL_QOS("HEAL_QOS"),
+
+	HEAL_RESET("HEAL_RESET"),
+
+	PARTIAL_HEALING("PARTIAL_HEALING");
+
+	private String value;
+
+	DegreeHealingType(final String value) {
+		this.value = value;
+	}
+
+	@Override
+	public String toString() {
+		return String.valueOf(value);
+	}
+
+	public static DegreeHealingType fromValue(final String text) {
+		for (final DegreeHealingType b : DegreeHealingType.values()) {
+			if (String.valueOf(b.value).equals(text)) {
+				return b;
+			}
+		}
+		return null;
+	}
+}
