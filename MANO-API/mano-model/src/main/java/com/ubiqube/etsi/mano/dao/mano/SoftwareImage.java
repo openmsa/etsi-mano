2cc17ec43e99c688afcf6ff8e6dff419390c4891
Olivier Vignaud <ovi@ubiqube.com>
2021-08-25 18:01:54 +0200

    Add file field in storageImage.

--- MANO-API/mano-model/src/main/java/com/ubiqube/etsi/mano/dao/mano/SoftwareImage.java
+++ MANO-API/mano-model/src/main/java/com/ubiqube/etsi/mano/dao/mano/SoftwareImage.java
@@ -27,6 +27,16 @@ import javax.persistence.Id;
 
 import com.ubiqube.etsi.mano.dao.mano.common.Checksum;
 
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author Olivier Vignaud <ovi@ubiqube.com>
+ *
+ */
+@Setter
+@Getter
 @Entity
 @EntityListeners(AuditListener.class)
 public class SoftwareImage implements Auditable {
@@ -64,110 +74,5 @@ public class SoftwareImage implements Auditable {
 
 	private Audit audit;
 
-	public UUID getId() {
-		return id;
-	}
-
-	public void setId(final UUID id) {
-		this.id = id;
-	}
-
-	public String getVimId() {
-		return vimId;
-	}
-
-	public void setVimId(final String vimId) {
-		this.vimId = vimId;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(final String name) {
-		this.name = name;
-	}
-
-	public String getProvider() {
-		return provider;
-	}
-
-	public void setProvider(final String provider) {
-		this.provider = provider;
-	}
-
-	public String getVersion() {
-		return version;
-	}
-
-	public void setVersion(final String version) {
-		this.version = version;
-	}
-
-	public Checksum getChecksum() {
-		return checksum;
-	}
-
-	public void setChecksum(final Checksum checksum) {
-		this.checksum = checksum;
-	}
-
-	public String getContainerFormat() {
-		return containerFormat;
-	}
-
-	public void setContainerFormat(final String containerFormat) {
-		this.containerFormat = containerFormat;
-	}
-
-	public String getDiskFormat() {
-		return diskFormat;
-	}
-
-	public void setDiskFormat(final String diskFormat) {
-		this.diskFormat = diskFormat;
-	}
-
-	public long getMinDisk() {
-		return minDisk;
-	}
-
-	public void setMinDisk(final long minDisk) {
-		this.minDisk = minDisk;
-	}
-
-	public long getMinRam() {
-		return minRam;
-	}
-
-	public void setMinRam(final long minRam) {
-		this.minRam = minRam;
-	}
-
-	public long getSize() {
-		return size;
-	}
-
-	public void setSize(final long size) {
-		this.size = size;
-	}
-
-	public String getImagePath() {
-		return imagePath;
-	}
-
-	public void setImagePath(final String imagePath) {
-		this.imagePath = imagePath;
-	}
-
-	@Override
-	public Audit getAudit() {
-		return audit;
-	}
-
-	@Override
-	public void setAudit(final Audit audit) {
-		this.audit = audit;
-	}
-
+	private String file;
 }
