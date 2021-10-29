d1236210cdfe49e2d0ca03afc40ce638b260f5af
Olivier Vignaud <ovi@ubiqube.com>
2021-10-29 12:15:01 +0200

    3.5.1 Add vrqan.

---
+++ MANO-API/etsi-models/3.5.1/model/src/main/java/com/ubiqube/etsi/mano/vnfm/v351/model/vrqan/VrQuotaAvailSubscription.java
@@ -0,0 +1,158 @@
+package com.ubiqube.etsi.mano.vnfm.v351.model.vrqan;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.ubiqube.etsi.mano.vnfm.v351.model.vrqan.VrQuotaAvailNotificationsFilter;
+import com.ubiqube.etsi.mano.vnfm.v351.model.vrqan.VrQuotaAvailSubscriptionLinks;
+import io.swagger.v3.oas.annotations.media.Schema;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * This type represents a subscription related to notifications related to the availability of the virtualised resources quotas. 
+ */
+@Schema(description = "This type represents a subscription related to notifications related to the availability of the virtualised resources quotas. ")
+@Validated
+
+
+public class VrQuotaAvailSubscription   {
+  @JsonProperty("id")
+  private String id = null;
+
+  @JsonProperty("filter")
+  private VrQuotaAvailNotificationsFilter filter = null;
+
+  @JsonProperty("callbackUri")
+  private String callbackUri = null;
+
+  @JsonProperty("_links")
+  private VrQuotaAvailSubscriptionLinks _links = null;
+
+  public VrQuotaAvailSubscription id(String id) {
+    this.id = id;
+    return this;
+  }
+
+  /**
+   * Get id
+   * @return id
+   **/
+  @Schema(required = true, description = "")
+      @NotNull
+
+    public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public VrQuotaAvailSubscription filter(VrQuotaAvailNotificationsFilter filter) {
+    this.filter = filter;
+    return this;
+  }
+
+  /**
+   * Get filter
+   * @return filter
+   **/
+  @Schema(description = "")
+  
+    @Valid
+    public VrQuotaAvailNotificationsFilter getFilter() {
+    return filter;
+  }
+
+  public void setFilter(VrQuotaAvailNotificationsFilter filter) {
+    this.filter = filter;
+  }
+
+  public VrQuotaAvailSubscription callbackUri(String callbackUri) {
+    this.callbackUri = callbackUri;
+    return this;
+  }
+
+  /**
+   * Get callbackUri
+   * @return callbackUri
+   **/
+  @Schema(required = true, description = "")
+      @NotNull
+
+    public String getCallbackUri() {
+    return callbackUri;
+  }
+
+  public void setCallbackUri(String callbackUri) {
+    this.callbackUri = callbackUri;
+  }
+
+  public VrQuotaAvailSubscription _links(VrQuotaAvailSubscriptionLinks _links) {
+    this._links = _links;
+    return this;
+  }
+
+  /**
+   * Get _links
+   * @return _links
+   **/
+  @Schema(required = true, description = "")
+      @NotNull
+
+    @Valid
+    public VrQuotaAvailSubscriptionLinks getLinks() {
+    return _links;
+  }
+
+  public void setLinks(VrQuotaAvailSubscriptionLinks _links) {
+    this._links = _links;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    VrQuotaAvailSubscription vrQuotaAvailSubscription = (VrQuotaAvailSubscription) o;
+    return Objects.equals(this.id, vrQuotaAvailSubscription.id) &&
+        Objects.equals(this.filter, vrQuotaAvailSubscription.filter) &&
+        Objects.equals(this.callbackUri, vrQuotaAvailSubscription.callbackUri) &&
+        Objects.equals(this._links, vrQuotaAvailSubscription._links);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, filter, callbackUri, _links);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class VrQuotaAvailSubscription {\n");
+    
+    sb.append("    id: ").append(toIndentedString(id)).append("\n");
+    sb.append("    filter: ").append(toIndentedString(filter)).append("\n");
+    sb.append("    callbackUri: ").append(toIndentedString(callbackUri)).append("\n");
+    sb.append("    _links: ").append(toIndentedString(_links)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
---
+++ MANO-API/etsi-models/3.5.1/model/src/main/java/com/ubiqube/etsi/mano/vnfm/v351/model/vrqan/VrQuotaAvailSubscriptionRequest.java
@@ -0,0 +1,132 @@
+package com.ubiqube.etsi.mano.vnfm.v351.model.vrqan;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.ubiqube.etsi.mano.vnfm.v351.model.vrqan.SubscriptionAuthentication;
+import com.ubiqube.etsi.mano.vnfm.v351.model.vrqan.VrQuotaAvailNotificationsFilter;
+import io.swagger.v3.oas.annotations.media.Schema;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * This type represents a subscription request related to notifications related to the availability of the virtualised resources quotas. 
+ */
+@Schema(description = "This type represents a subscription request related to notifications related to the availability of the virtualised resources quotas. ")
+@Validated
+
+
+public class VrQuotaAvailSubscriptionRequest   {
+  @JsonProperty("filter")
+  private VrQuotaAvailNotificationsFilter filter = null;
+
+  @JsonProperty("callbackUri")
+  private String callbackUri = null;
+
+  @JsonProperty("authentication")
+  private SubscriptionAuthentication authentication = null;
+
+  public VrQuotaAvailSubscriptionRequest filter(VrQuotaAvailNotificationsFilter filter) {
+    this.filter = filter;
+    return this;
+  }
+
+  /**
+   * Get filter
+   * @return filter
+   **/
+  @Schema(description = "")
+  
+    @Valid
+    public VrQuotaAvailNotificationsFilter getFilter() {
+    return filter;
+  }
+
+  public void setFilter(VrQuotaAvailNotificationsFilter filter) {
+    this.filter = filter;
+  }
+
+  public VrQuotaAvailSubscriptionRequest callbackUri(String callbackUri) {
+    this.callbackUri = callbackUri;
+    return this;
+  }
+
+  /**
+   * Get callbackUri
+   * @return callbackUri
+   **/
+  @Schema(required = true, description = "")
+      @NotNull
+
+    public String getCallbackUri() {
+    return callbackUri;
+  }
+
+  public void setCallbackUri(String callbackUri) {
+    this.callbackUri = callbackUri;
+  }
+
+  public VrQuotaAvailSubscriptionRequest authentication(SubscriptionAuthentication authentication) {
+    this.authentication = authentication;
+    return this;
+  }
+
+  /**
+   * Get authentication
+   * @return authentication
+   **/
+  @Schema(description = "")
+  
+    @Valid
+    public SubscriptionAuthentication getAuthentication() {
+    return authentication;
+  }
+
+  public void setAuthentication(SubscriptionAuthentication authentication) {
+    this.authentication = authentication;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    VrQuotaAvailSubscriptionRequest vrQuotaAvailSubscriptionRequest = (VrQuotaAvailSubscriptionRequest) o;
+    return Objects.equals(this.filter, vrQuotaAvailSubscriptionRequest.filter) &&
+        Objects.equals(this.callbackUri, vrQuotaAvailSubscriptionRequest.callbackUri) &&
+        Objects.equals(this.authentication, vrQuotaAvailSubscriptionRequest.authentication);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(filter, callbackUri, authentication);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class VrQuotaAvailSubscriptionRequest {\n");
+    
+    sb.append("    filter: ").append(toIndentedString(filter)).append("\n");
+    sb.append("    callbackUri: ").append(toIndentedString(callbackUri)).append("\n");
+    sb.append("    authentication: ").append(toIndentedString(authentication)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
