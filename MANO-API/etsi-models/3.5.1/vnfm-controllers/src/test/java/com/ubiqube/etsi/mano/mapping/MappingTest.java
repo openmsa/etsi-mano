a5ca6ea3bc6701300bc7d3bb4701aaace197da2c
Olivier Vignaud <ovi@ubiqube.com>
2021-10-31 16:41:27 +0100

    Add test for 2.5.1.

---
+++ MANO-API/etsi-models/3.5.1/vnfm-controllers/src/test/java/com/ubiqube/etsi/mano/mapping/MappingTest.java
@@ -0,0 +1,60 @@
+package com.ubiqube.etsi.mano.mapping;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+
+import com.ubiqube.etsi.mano.dao.mano.GrantResponse;
+import com.ubiqube.etsi.mano.dao.mano.VnfPackage;
+import com.ubiqube.etsi.mano.dao.mano.dto.VnfLcmOpOccs;
+import com.ubiqube.etsi.mano.nfvo.v351.model.vnf.VnfPkgInfo;
+import com.ubiqube.etsi.mano.test.TestHelper;
+import com.ubiqube.etsi.mano.vnfm.v351.OrikaMapperVnfm351;
+import com.ubiqube.etsi.mano.vnfm.v351.model.grant.GrantRequest;
+import com.ubiqube.etsi.mano.vnfm.v351.model.vnflcm.VnfInstance;
+import com.ubiqube.etsi.mano.vnfm.v351.model.vnflcm.VnfLcmOpOcc;
+
+/**
+ *
+ * @author Olivier Vignaud <ovi@ubiqube.com>
+ *
+ */
+public class MappingTest extends TestHelper {
+
+	public MappingTest() {
+		super(new OrikaMapperVnfm351());
+	}
+
+	@Test
+	void testVnfPkgInfo() throws Exception {
+		final Set<String> ignore = new HashSet<>();
+		ignore.add("getSoftwareImages");
+		ignore.add("getLinks");
+		ignore.add("getChecksum");
+		doTest(VnfPkgInfo.class, VnfPackage.class, ignore);
+	}
+
+	@Test
+	void testVnfInstance() throws Exception {
+		final Set<String> ignore = new HashSet<>();
+		ignore.add("getLinks");
+		ignore.add("getExtLinkPortId");
+		doTest(VnfInstance.class, com.ubiqube.etsi.mano.dao.mano.VnfInstance.class, ignore);
+	}
+
+	@Test
+	void testLcmOpOccs() throws Exception {
+		final Set<String> ignore = new HashSet<>();
+		ignore.add("getLinks");
+		doTest(VnfLcmOpOcc.class, VnfLcmOpOccs.class, ignore);
+	}
+
+	@Test
+	void testGrant() throws Exception {
+		final Set<String> ignore = new HashSet<>();
+		ignore.add("getLinks");
+		doTest(GrantRequest.class, GrantResponse.class, ignore);
+	}
+
+}
