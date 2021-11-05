101ae9b60f37bf20c644c42ddd42f1ee376876a0
Olivier Vignaud <ovi@ubiqube.com>
2021-11-05 16:40:26 +0100

    3.5.1 fix missing import.

--- MANO-API/etsi-models/3.5.1/model/src/main/java/com/ubiqube/etsi/mano/vnfm/v351/model/vnf/VnfPkgInfo.java
+++ MANO-API/etsi-models/3.5.1/model/src/main/java/com/ubiqube/etsi/mano/vnfm/v351/model/vnf/VnfPkgInfo.java
@@ -29,6 +29,7 @@ import org.springframework.validation.annotation.Validated;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonValue;
+import com.ubiqube.etsi.mano.nfvo.v351.model.vnf.Checksum;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
