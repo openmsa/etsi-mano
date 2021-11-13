8b9d1f9aa9c338a02b657fc21126e8a2772c207b
Olivier Vignaud <ovi@ubiqube.com>
2021-11-13 17:27:22 +0100

    Minor typo in open stack vim.

--- MANO-API/mano-vim/mano-vim-openstack/src/main/java/com/ubiqube/etsi/mano/service/vim/OpenStackVim.java
+++ MANO-API/mano-vim/mano-vim-openstack/src/main/java/com/ubiqube/etsi/mano/service/vim/OpenStackVim.java
@@ -355,7 +355,7 @@ public class OpenStackVim implements Vim {
 	@Override
 	public String createServerGroup(final VimConnectionInformation vimConnectionInformation, final AffinityRule ar) {
 		final OSClientV3 os = this.getClient(vimConnectionInformation);
-		final org.openstack4j.model.compute.ServerGroup res = os.compute().serverGroups().create(ar.getId().toString(), ar.getScope().toString());
+		final org.openstack4j.model.compute.ServerGroup res = os.compute().serverGroups().create(ar.getId().toString(), ar.getScope());
 		return res.getId();
 	}
 
