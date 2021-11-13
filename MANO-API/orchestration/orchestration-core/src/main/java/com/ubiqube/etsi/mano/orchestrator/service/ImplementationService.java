441e9e88412fe6d30023651b0ad1c911498ccc54
Olivier Vignaud <ovi@ubiqube.com>
2021-11-13 17:28:45 +0100

    Better message on vim error.

--- MANO-API/orchestration/orchestration-core/src/main/java/com/ubiqube/etsi/mano/orchestrator/service/ImplementationService.java
+++ MANO-API/orchestration/orchestration-core/src/main/java/com/ubiqube/etsi/mano/orchestrator/service/ImplementationService.java
@@ -51,7 +51,7 @@ public class ImplementationService {
 	public SystemBuilder getTaretSystem(final VirtualTask<?> virtualTask) {
 		final String connectionId = virtualTask.getVimConnectionId();
 		if (null == connectionId) {
-			throw new OrchestrationException("Unable to find VimId: " + virtualTask.getVimConnectionId());
+			throw new OrchestrationException("Unable to find VimId: " + virtualTask.getVimConnectionId() + ", for task: " + virtualTask.getName());
 		}
 		final SystemConnections vim = vimManager.findVimByVimIdAndProviderId(connectionId, virtualTask.getProviderId());
 		final System sys = systems.get(vim.getVimType());
