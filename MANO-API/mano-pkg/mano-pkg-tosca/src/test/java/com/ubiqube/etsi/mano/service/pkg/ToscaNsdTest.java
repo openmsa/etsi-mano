cd494577ae8ec52232c7aaf96e4bf9b9c4072459
Olivier Vignaud <ovi@ubiqube.com>
2021-11-15 12:35:26 +0100

    Fix test unit in package.

--- MANO-API/mano-pkg/mano-pkg-tosca/src/test/java/com/ubiqube/etsi/mano/service/pkg/ToscaNsdTest.java
+++ MANO-API/mano-pkg/mano-pkg-tosca/src/test/java/com/ubiqube/etsi/mano/service/pkg/ToscaNsdTest.java
@@ -19,6 +19,8 @@ package com.ubiqube.etsi.mano.service.pkg;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Set;
 
@@ -29,13 +31,24 @@ import com.ubiqube.etsi.mano.dao.mano.v2.nfvo.NsVirtualLink;
 import com.ubiqube.etsi.mano.service.pkg.bean.NsInformations;
 import com.ubiqube.etsi.mano.service.pkg.bean.SecurityGroupAdapter;
 import com.ubiqube.etsi.mano.service.pkg.tosca.ns.ToscaNsPackageProvider;
-import com.ubiqube.etsi.mano.test.TestTools;
+import com.ubiqube.etsi.mano.test.ZipUtil;
+import com.ubiqube.etsi.mano.test.ZipUtil.Entry;
+
+import ma.glasnost.orika.OrikaSystemProperties;
+import ma.glasnost.orika.impl.generator.EclipseJdtCompilerStrategy;
 
 public class ToscaNsdTest {
 	private final ToscaNsPackageProvider tpp;
 
 	public ToscaNsdTest() throws IOException {
-		final byte[] data = TestTools.readFile("/ubi-nsd-tosca.csar");
+		System.setProperty(OrikaSystemProperties.COMPILER_STRATEGY, EclipseJdtCompilerStrategy.class.getName());
+		System.setProperty(OrikaSystemProperties.WRITE_SOURCE_FILES, "true");
+		System.setProperty(OrikaSystemProperties.WRITE_SOURCE_FILES_TO_PATH, "/tmp/orika-test");
+		ZipUtil.makeToscaZip("/tmp/ubi-nsd-tosca.csar", Entry.of("ubi-nsd-tosca/Definitions/nsd_ubi.yaml", "Definitions/nsd_ubi.yaml"),
+				Entry.of("ubi-nsd-tosca/Definitions/etsi_nfv_sol001_nsd_types.yaml", "Definitions/etsi_nfv_sol001_nsd_types.yaml"),
+				Entry.of("ubi-nsd-tosca/Definitions/fix_ubiqube.yaml", "Definitions/fix_ubiqube.yaml"),
+				Entry.of("ubi-nsd-tosca/TOSCA-Metadata/TOSCA.meta", "TOSCA-Metadata/TOSCA.meta"));
+		final byte[] data = Files.readAllBytes(Path.of("/tmp/ubi-nsd-tosca.csar"));
 		tpp = new ToscaNsPackageProvider(data);
 	}
 
---
+++ MANO-API/mano-pkg/mano-pkg-tosca/src/test/resources/ubi-nsd-tosca/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm ../ubi-nsd-tosca.csar
+zip -r ../ubi-nsd-tosca.csar .
