0a7c1c3e9b8efb7e1586d6f946836af1c2a0f2a7
Olivier Vignaud <ovi@ubiqube.com>
2021-11-24 09:56:50 +0100

    mano-model: Add builder on PmJob.

--- MANO-API/mano-model/src/main/java/com/ubiqube/etsi/mano/dao/mano/pm/PmJob.java
+++ MANO-API/mano-model/src/main/java/com/ubiqube/etsi/mano/dao/mano/pm/PmJob.java
@@ -41,12 +41,23 @@ import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
 import com.ubiqube.etsi.mano.dao.mano.AuthentificationInformations;
 import com.ubiqube.etsi.mano.dao.mano.VimConnectionInformation;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/**
+ *
+ * @author Olivier Vignaud <ovi@ubiqube.com>
+ *
+ */
 @Getter
 @Setter
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Indexed
 public class PmJob {
 	@Id
@@ -55,20 +66,28 @@ public class PmJob {
 	private UUID id;
 
 	/**
-	 * Type of the measured object. The applicable measured object type for a measurement is defined in clause 7.2 of ETSI GS NFV-IFA 027 [5].
+	 * Type of the measured object. The applicable measured object type for a
+	 * measurement is defined in clause 7.2 of ETSI GS NFV-IFA 027 [5].
 	 */
 	@Enumerated(EnumType.STRING)
 	private PmType objectType;
 
 	/**
-	 * Identifiers of the measured object instances for which performance information is collected.
+	 * Identifiers of the measured object instances for which performance
+	 * information is collected.
 	 */
 	@ElementCollection(fetch = FetchType.EAGER)
 	private List<String> objectInstanceIds;
 
 	/**
-	 * Identifiers of the sub-object instances of the measured object instance for which performance information is requested to be collected. May be present if a sub-object is defined in clause 6.2 of ETSI GS NFV-IFA 027 [5] for the related measured object type If this attribute is present, the cardinality of the "objectInstanceIds" attribute shall be 1. If this attribute is absent and a sub-object is defined in clause 6.2 of ETSI GS NFV-IFA 027 [5] for the related measured object type,
-	 * measurements will be taken for all sub-object instances of the measured object instance.
+	 * Identifiers of the sub-object instances of the measured object instance for
+	 * which performance information is requested to be collected. May be present if
+	 * a sub-object is defined in clause 6.2 of ETSI GS NFV-IFA 027 [5] for the
+	 * related measured object type If this attribute is present, the cardinality of
+	 * the "objectInstanceIds" attribute shall be 1. If this attribute is absent and
+	 * a sub-object is defined in clause 6.2 of ETSI GS NFV-IFA 027 [5] for the
+	 * related measured object type, measurements will be taken for all sub-object
+	 * instances of the measured object instance.
 	 */
 	@ElementCollection // (fetch = FetchType.EAGER)
 	private List<String> subObjectInstanceIds;
