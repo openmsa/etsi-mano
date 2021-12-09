668971127f3134c86ec7c91052da1d4b18235ed2
Olivier Vignaud <ovi@ubiqube.com>
2021-12-09 09:11:31 +0100

    mano-model: Add missing files.

---
+++ MANO-API/mano-model/src/main/java/com/ubiqube/etsi/mano/dao/mano/v2/nfvo/NsSfcTask.java
@@ -0,0 +1,71 @@
+/**
+ *     Copyright (C) 2019-2020 Ubiqube.
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.ubiqube.etsi.mano.dao.mano.v2.nfvo;
+
+import java.util.UUID;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
+
+import com.ubiqube.etsi.mano.dao.mano.AuditListener;
+import com.ubiqube.etsi.mano.dao.mano.ResourceTypeEnum;
+import com.ubiqube.etsi.mano.dao.mano.ScaleInfo;
+import com.ubiqube.etsi.mano.dao.mano.nsd.VnffgDescriptor;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author Olivier Vignaud <ovi@ubiqube.com>
+ *
+ */
+@Entity
+@Getter
+@Setter
+@EntityListeners(AuditListener.class)
+@Indexed
+public class NsSfcTask extends NsTask {
+
+	/** Serial. */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private UUID id;
+
+	@ManyToOne(cascade = CascadeType.DETACH, fetch = FetchType.EAGER)
+	private VnffgDescriptor vnffg;
+
+	@Override
+	public ResourceTypeEnum getType() {
+		return ResourceTypeEnum.VNFFG;
+	}
+
+	@Override
+	public ScaleInfo getScaleInfo() {
+		return null;
+	}
+}
