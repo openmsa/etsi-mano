919235bb4272dd8134bb4b14050cb4addf79ca15
Olivier Vignaud <ovi@ubiqube.com>
2021-12-20 08:48:57 +0100

    mano-model: Add missing field in Vnf link port.

--- MANO-API/mano-model/src/main/java/com/ubiqube/etsi/mano/dao/mano/VnfLinkPort.java
+++ MANO-API/mano-model/src/main/java/com/ubiqube/etsi/mano/dao/mano/VnfLinkPort.java
@@ -28,8 +28,13 @@ import javax.persistence.ManyToOne;
 
 import com.ubiqube.etsi.mano.utils.ToStringIgnore;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
 @EntityListeners(AuditListener.class)
+@Getter
+@Setter
 public class VnfLinkPort implements BaseEntity, Auditable {
 	/** Serial. */
 	private static final long serialVersionUID = 1L;
@@ -50,86 +55,15 @@ public class VnfLinkPort implements BaseEntity, Auditable {
 
 	private int interfaceOrder;
 
+	private Integer bitrateRequirement;
+
+	private String vnicType;
+
 	@ManyToOne
 	@ToStringIgnore
 	private VnfPackage vnfPackage;
 
 	@Embedded
 	private Audit audit;
-
-	@Override
-	public UUID getId() {
-		return id;
-	}
-
-	public void setId(final UUID id) {
-		this.id = id;
-	}
-
-	public String getToscaId() {
-		return toscaId;
-	}
-
-	public void setToscaId(final String toscaId) {
-		this.toscaId = toscaId;
-	}
-
-	public String getToscaName() {
-		return toscaName;
-	}
-
-	public void setToscaName(final String toscaName) {
-		this.toscaName = toscaName;
-	}
-
-	public String getState() {
-		return state;
-	}
-
-	public void setState(final String state) {
-		this.state = state;
-	}
-
-	public String getVirtualLink() {
-		return virtualLink;
-	}
-
-	public void setVirtualLink(final String virtualLink) {
-		this.virtualLink = virtualLink;
-	}
-
-	public String getVirtualBinding() {
-		return virtualBinding;
-	}
-
-	public void setVirtualBinding(final String virtualBinding) {
-		this.virtualBinding = virtualBinding;
-	}
-
-	public int getInterfaceOrder() {
-		return interfaceOrder;
-	}
-
-	public void setInterfaceOrder(final int interfaceOrder) {
-		this.interfaceOrder = interfaceOrder;
-	}
-
-	public VnfPackage getVnfPackage() {
-		return vnfPackage;
-	}
-
-	public void setVnfPackage(final VnfPackage vnfPackage) {
-		this.vnfPackage = vnfPackage;
-	}
-
-	@Override
-	public Audit getAudit() {
-		return audit;
-	}
-
-	@Override
-	public void setAudit(final Audit audit) {
-		this.audit = audit;
-	}
 
 }
