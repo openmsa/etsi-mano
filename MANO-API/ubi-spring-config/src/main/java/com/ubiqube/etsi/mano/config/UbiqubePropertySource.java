e7112b9441e0c506255a25960f80824815d22d00
Olivier Vignaud <ovi@ubiqube.com>
2021-12-22 16:42:11 +0100

    Remove duplicated license.

--- MANO-API/ubi-spring-config/src/main/java/com/ubiqube/etsi/mano/config/UbiqubePropertySource.java
+++ MANO-API/ubi-spring-config/src/main/java/com/ubiqube/etsi/mano/config/UbiqubePropertySource.java
@@ -14,20 +14,6 @@
  *     You should have received a copy of the GNU General Public License
  *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-/**
- * This copy of Woodstox XML processor is licensed under the
- * Apache (Software) License, version 2.0 ("the License").
- * See the License for details about distribution rights, and the
- * specific rights regarding derivate works.
- *
- * You may obtain a copy of the License at:
- *
- * http://www.apache.org/licenses/
- *
- * A copy is also included in the downloadable source code package
- * containing Woodstox, in file "ASL2.0", under the same directory
- * as this file.
- */
 package com.ubiqube.etsi.mano.config;
 
 import java.util.Map;
@@ -67,10 +53,7 @@ public class UbiqubePropertySource extends EnumerablePropertySource<Map<String, 
 		if (this == obj) {
 			return true;
 		}
-		if (!super.equals(obj)) {
-			return false;
-		}
-		if (getClass() != obj.getClass()) {
+		if (!super.equals(obj) || (getClass() != obj.getClass())) {
 			return false;
 		}
 		final UbiqubePropertySource other = (UbiqubePropertySource) obj;
