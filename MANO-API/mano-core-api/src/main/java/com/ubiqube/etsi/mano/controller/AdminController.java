281189ab19696662f9cb756f4bc974504851c11d
Olivier Vignaud <ovi@ubiqube.com>
2022-01-19 11:29:36 +0100

    mano-core: Add patch on vim connections.

--- MANO-API/mano-core-api/src/main/java/com/ubiqube/etsi/mano/controller/AdminController.java
+++ MANO-API/mano-core-api/src/main/java/com/ubiqube/etsi/mano/controller/AdminController.java
@@ -19,18 +19,25 @@ package com.ubiqube.etsi.mano.controller;
 import java.util.Map;
 import java.util.UUID;
 
+import javax.annotation.Nullable;
+
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ubiqube.etsi.mano.dao.mano.VimConnectionInformation;
 import com.ubiqube.etsi.mano.exception.NotFoundException;
+import com.ubiqube.etsi.mano.exception.PreConditionException;
 import com.ubiqube.etsi.mano.jpa.VimConnectionInformationJpa;
+import com.ubiqube.etsi.mano.service.Patcher;
 import com.ubiqube.etsi.mano.service.SystemService;
 import com.ubiqube.etsi.mano.service.event.EventManager;
 import com.ubiqube.etsi.mano.service.event.NotificationEvent;
@@ -47,14 +54,17 @@ public class AdminController {
 	private final VimManager vimManager;
 	private final SystemService systemService;
 	private final EventManager eventManager;
+	private final Patcher patcher;
 
-	public AdminController(final VimConnectionInformationJpa vciJpa, final MapperFacade mapper, final VimManager vimManager, final SystemService systemService, final EventManager eventManager) {
+	public AdminController(final VimConnectionInformationJpa vciJpa, final MapperFacade mapper, final VimManager vimManager, final SystemService systemService, final EventManager eventManager,
+			final Patcher patcher) {
 		super();
 		this.vciJpa = vciJpa;
 		this.mapper = mapper;
 		this.vimManager = vimManager;
 		this.systemService = systemService;
 		this.eventManager = eventManager;
+		this.patcher = patcher;
 	}
 
 	@PostMapping(value = "/vim/register")
@@ -70,6 +80,18 @@ public class AdminController {
 	public ResponseEntity<Void> deleteVim(@PathVariable("id") final String id) {
 		vimManager.deleteVim(UUID.fromString(id));
 		return ResponseEntity.noContent().build();
+	}
+
+	@PatchMapping(value = "/vim/{id}")
+	public ResponseEntity<VimConnectionInformation> patchVim(@PathVariable("id") final UUID id, @Nullable @RequestBody final String body,
+			@RequestHeader(name = HttpHeaders.IF_MATCH, required = false) final String ifMatch) {
+		final VimConnectionInformation vim = vimManager.findVimById(id);
+		if (ifMatch != null && !ifMatch.equals(vim.getVersion() + "")) {
+			throw new PreConditionException(ifMatch + " does not match " + vim.getVersion());
+		}
+		patcher.patch(body, vim);
+		final VimConnectionInformation newVim = vimManager.save(vim);
+		return ResponseEntity.ok(newVim);
 	}
 
 	@GetMapping(value = "/vim/{id}/connect")
