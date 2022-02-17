f0216063bb7c1d0e7d0b82985b2e6b0a3cccabf6
Olivier Vignaud <ovi@ubiqube.com>
2022-02-17 09:59:00 +0100

    2.6.1: Change vnfVirtualLinkDescId to String type.

--- MANO-API/mano-model/src/main/java/com/ubiqube/etsi/mano/dao/mano/ExtCpInfo.java
+++ MANO-API/mano-model/src/main/java/com/ubiqube/etsi/mano/dao/mano/ExtCpInfo.java
@@ -47,33 +47,81 @@ public class ExtCpInfo implements Serializable {
 	/** Serial. */
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * IdentifierInVnf: Identifier of the external CP instance and the related
+	 * information instance.
+	 */
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private UUID id = null;
-
-	private String cpdId = null;
-	// 3.3.1
-	private String cpConfigId;
-	// 3.3.1
-	private String vnfdId;
-
-	@OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-	private Set<CpProtocolDataEntity> cpProtocolInfo = null;
+	private UUID id;
 
 	/**
+	 * IdentifierInVnfd: Identifier of the external CPD, VnfExtCpd, in the VNFD.
+	 */
+	private String cpdId;
+
+	/**
+	 * IdentifierInVnf: Identifier that references the applied "VnfExtCpConfig"
+	 * entry in the "cpConfig" map of the "currentVnfExtCpData" in the
+	 * "ExtVirtualLinkInfo" structure.
+	 *
+	 * @since 3.3.1
+	 */
+	private String cpConfigId;
+	/**
+	 * Identifier: Identifier of the VNFD. Shall be present in case the value
+	 * differs from the vnfdId attribute of the VnfInstance (e.g. during a "Change
+	 * current VNF package" operation or due to its final failure).
+	 *
+	 * @since 3.3.1
+	 */
+	private String vnfdId;
+
+	/**
+	 * Network protocol information for this CP.
+	 */
+	@OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	private Set<CpProtocolDataEntity> cpProtocolInfo;
+
+	/**
+	 * Identifier of the "ExtLinkPortInfo" structure inside the "ExtVirtualLinkInfo"
+	 * structure. Shall be present if the CP is associated to a link port. See note
+	 * 2.
+	 *
 	 * @deprecated Duplicate with 271.
 	 */
 	@Deprecated(forRemoval = false)
 	@OneToOne
-	private CpProtocolDataEntity extLinkPortId = null;
+	private CpProtocolDataEntity extLinkPortId;
 	// 2.7.1 Type change
 	private CpProtocolDataEntity extLinkPortId271;
 
 	@ElementCollection
 	private Map<String, String> metadata = new HashMap<>();
 
-	private String associatedVnfcCpId = null;
+	/**
+	 * IdentifierInVnf: Identifier of the "vnfcCpInfo" structure in
+	 * "VnfcResourceInfo" structure that represents the VNFC CP which is exposed by
+	 * this external CP instance, either directly or via a floating IP address.
+	 * Shall be present in case this CP instance maps to a VNFC CP. See note 1.
+	 */
+	private String associatedVnfcCpId;
 
-	private String associatedVnfVirtualLinkId = null;
+	/**
+	 * IdentifierInVnf: Identifier of the VIP CP instance that is exposed as this
+	 * VnfExtCp instance, either directly or via a floating IP address, and the
+	 * related "VipCpInfo" structure in "VnfInstance". Shall be present if the cpdId
+	 * of this VnfExtCp has a vipCpd attribute.
+	 */
+	private String associatedVipCpId;
+
+	/**
+	 * IdentifierInVnf: Identifier of the "VnfVirtualLinkResourceInfo" structure
+	 * that represents the internal VL or of the "ExtManagedVirtualLinkInfo"
+	 * structure that represents the externally-managed internal VL which is exposed
+	 * by this external CP instance. Shall be present in case this CP instance maps
+	 * to an internal VL (including externally-managed internal VL).
+	 */
+	private String associatedVnfVirtualLinkId;
 
 }
