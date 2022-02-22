b762c0b7a850069e59916b7f39fdde6dc13acff8
Olivier Vignaud <ovi@ubiqube.com>
2022-02-22 16:46:54 +0100

    2.7.1: Vnfm mapping.

--- MANO-API/etsi-models/2.7.1/vnfm-controllers/src/main/java/com/ubiqube/etsi/mano/vnfm/v271/OrikaMapperVnfm271.java
+++ MANO-API/etsi-models/2.7.1/vnfm-controllers/src/main/java/com/ubiqube/etsi/mano/vnfm/v271/OrikaMapperVnfm271.java
@@ -232,8 +232,8 @@ public class OrikaMapperVnfm271 implements OrikaMapperFactoryConfigurer {
 				.byDefault()
 				.register();
 		orikaMapperFactory.classMap(ResourceDefinition.class, GrantInformationExt.class)
-				.exclude("id")
-				.field("id", "resourceDefinitionId")
+				.field("id", "id")
+				// .field("id", "resourceDefinitionId")
 				.field("type", "type")
 				.field("vduId", "vduId")
 				.field("resource.vimConnectionId", "vimConnectionId")
